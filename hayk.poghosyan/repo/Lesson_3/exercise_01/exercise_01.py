f3aa60ece6061268293bcb43ec8fa9f0a2106007
hpoghosyan <hayk.m.poghosyan@gmail.com>
2021-08-05 22:25:57 +0400

    Exercise_01 Version1

---
+++ hayk.poghosyan/repo/Lesson_3/exercise_01/exercise_01.py
@@ -0,0 +1,17 @@
+# 1-a
+x = "*"
+y = 19
+print(x*y + "\n" + x*y + "\n" + x*y + "\n" + x*y)
+
+# 1-b
+x = "*"
+y = 19
+print("\n",x*y,"\n",x*y,"\n",x*y,"\n",x*y)
+
+# 1-c
+x = "*"
+y = 19
+print(x*y)
+print(x*y)
+print(x*y)
+print(x*y)
