d66f7e31285cd219e1fdaadaed1c8a6874a04f42
hpoghosyan <hayk.m.poghosyan@gmail.com>
2021-08-05 23:32:51 +0400

    Exercise_10 Version1

---
+++ hayk.poghoyan/repo/Lesson_3/exercise_10/exercise_10.py
@@ -0,0 +1,5 @@
+# 10
+width_room = float(input("Enter the width of the room:" ))
+length_room = float(input("Enter the length of the room:" ))
+area_room = width_room*length_room
+print(area_room,"m^2")
