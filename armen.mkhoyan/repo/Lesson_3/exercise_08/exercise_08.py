a03a95f69a3d77284c722f9ddfa6f482459ea15c
Armen M <amkhoyanh@gmail.com>
2021-08-05 14:53:08 -0400

    Exercise_08 Version1

---
+++ armen.mkhoyan/repo/Lesson_3/exercise_08/exercise_08.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+number1 = float(input("Enter a first number: "))
+number2 = float(input("Enter a second number: "))
+number3 = float(input("Enter a thirt number : "))
+total = number1+number2+number3
+average = total/3
+print("Total: ", total,"\nAverage: ", average)
