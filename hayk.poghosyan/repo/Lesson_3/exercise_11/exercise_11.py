65a0c6a0213197b56b7b72339dad041f1c24b833
hpoghosyan <hayk.m.poghosyan@gmail.com>
2021-08-05 23:35:37 +0400

    Exercise_11 Version1

---
+++ hayk.poghosyan/repo/Lesson_3/exercise_11/exercise_11.py
@@ -0,0 +1,7 @@
+# 11
+width_field = float(input("Enter the width of the field:" ))
+length_field = float(input("Enter the length of the field:" ))
+one_acre = 43560
+area_room = width_field*length_field
+total_acres = area_room/one_acre
+print(total_acres,"acre")
