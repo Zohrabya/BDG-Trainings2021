d19ca9a210fea62ca8b5c80bdebc27bcab4bb6b7
Armen M <amkhoyanh@gmail.com>
2021-08-05 14:33:52 -0400

    Exercise_03 Version1

---
+++ armen.mkhoyan/repo/Lesson_3/exercise_03/exercise_3.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+#a
+print("*","\n**","\n***","\n****")
+
+print("---------------")
+
+#b
+x = "*"
+y = "\n**"
+z = "\n***"
+print(x,y,z,"\n****")
+
+print("---------------")
+
+#c
+a = "*"
+b = "\n**"
+c = "\n***"
+d = "\n****"
+print(a)
+print(b)
+print(c)
+print(d)
