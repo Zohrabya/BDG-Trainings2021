b9f9ac1b20e2f4e8644b65569c8242814776d82b
Armen M <amkhoyanh@gmail.com>
2021-08-05 13:58:24 -0400

    Exercise_01 Version1

---
+++ armen.mkhoyan/repo/Lesson_3/exercise_01/exercise_01.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+#a.	Using one statement with one print function
+
+print("***********","\n***********","\n***********","\n***********",)
+
+print("----------")
+
+#b.	Using four statement with one print function.
+x = "***********"
+y = "\n***********"
+print(x,3*y)
+
+print("----------")
+
+#c.	Using four statement with four print function.
+a = "***********"
+b = "\n***********"
+c = "\n***********"
+d = "\n***********"
+print(a)
+print(b)
+print(c)
+print(d)
+
