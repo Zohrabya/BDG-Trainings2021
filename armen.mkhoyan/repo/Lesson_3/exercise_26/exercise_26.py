01e0d887744cb6b49f3ad1006a6862e22b9effdb
Armen M <amkhoyanh@gmail.com>
2021-08-05 16:11:11 -0400

    Exercise_26 Version1

---
+++ armen.mkhoyan/repo/Lesson_3/exercise_26/exercise_26.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+passport_name = str(input("Enter a First Name: "))
+passport_last_name = str(input("Enter a Last Name: "))
+
+print(passport_name.capitalize(), passport_last_name.capitalize())
