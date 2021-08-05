4af29d87d12a19f02333a303008bb7455afebadc
hpoghosyan <hayk.m.poghosyan@gmail.com>
2021-08-05 23:05:28 +0400

    Exercise_02 Version1

---
+++ hayk.poghosyan/repo/Lesson_3/exercise_02/exercise_02.py
@@ -0,0 +1,20 @@
+# 2-a
+x = "*"
+y = 17
+z = 19
+print(x*z+"\n"+ x + " "*y + x + "\n" + x + " "*y + x + "\n" + x*z)
+
+# 2-b
+x = "*"
+y = 17
+z = 19
+print("\n",x*z,"\n",x + " "*y + x,"\n",x + " "*y + x,"\n",x*z)
+
+# 2-c
+x = "*"
+y = 17
+z = 19
+print(x*z)
+print(x + " "*y + x)
+print(x + " "*y + x)
+print(x*z)
