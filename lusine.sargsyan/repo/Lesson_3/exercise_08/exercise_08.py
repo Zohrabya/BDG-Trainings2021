5d3fd79754adafdb489c2fbeb07cb58da37e8e75
LusineSar <lusinesargsyan85@yahoo.com>
2021-08-06 02:45:31 +0400

    Exercise_08 Version1

---
+++ lusine.sargsyan/repo/Lesson_3/exercise_08/exercise_08.py
@@ -0,0 +1,28 @@
+#!/usr/bin /env python3
+
+
+a=input("enter a number1:")
+b=input("enter a number2:")
+c=input("enter a number3:")
+
+a=int(a)
+b=int(b)
+c=int(c)
+
+lst=[a,b,c]
+
+
+total=sum(lst)
+qnt=len(lst)
+average=total/qnt
+
+print(average)
+
+
+
+ 
+
+
+
+
+
