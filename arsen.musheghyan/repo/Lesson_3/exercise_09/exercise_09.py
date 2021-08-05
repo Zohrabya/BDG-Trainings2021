7a2469db6d43feb9d266de9fc6c806a0de0aff17
Arsen1978 <jobforars@gmail.com>
2021-08-05 20:13:14 +0400

    Exercise_09 Version1

---
+++ arsen.musheghyan/repo/Lesson_3/exercise_09/exercise_09.py
@@ -0,0 +1,6 @@
+print ("Exercise N 9")
+a= int(input("Write price of the meal:"))
+b= int(input("How many percent tip do you want to leave:"))
+c = a*b/100
+print ("Your price for tip",c)
+print ("Total amount",c+a)
