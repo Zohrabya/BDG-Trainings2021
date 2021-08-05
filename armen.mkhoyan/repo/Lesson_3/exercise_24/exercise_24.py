232f2114db38f24ae0916695bc33e2f081852adf
Armen M <amkhoyanh@gmail.com>
2021-08-05 16:07:25 -0400

    Exercise_24 Version1

---
+++ armen.mkhoyan/repo/Lesson_3/exercise_24/exercise_24.py
@@ -0,0 +1,59 @@
+#!/usr/bin/evn python3
+
+#int / #square / #cube
+a = 0
+a_square = a**2
+a_cube = a**3
+
+b = 1
+b_square = b**2
+b_cube = b**3
+
+c = 2
+c_square = c**2
+c_cube = c**3
+
+d = 3
+d_square = d**2
+d_cube = d**3
+
+e = 4
+e_square = e**2
+e_cube = e**3
+
+f = 5
+f_square = f**2
+f_cube = f**3
+
+g = 6
+g_square = g**2
+g_cube = g**3
+
+h = 7
+h_square = h**2
+h_cube = h**3
+
+j = 8
+j_square = j**2
+j_cube = j**3
+
+k = 9
+k_square = k**2
+k_cube = k**3
+
+l = 10
+l_square = l**2
+l_cube = l**3
+
+print("integer","square","cube")
+print(a,"     ", a_square,"    ", a_cube)
+print(b,"     ", b_square,"    ", b_cube)
+print(c,"     ", c_square,"    ", c_cube)
+print(d,"     ", d_square,"    ", d_cube)
+print(e,"     ", e_square,"   ", e_cube)
+print(f,"     ", f_square,"   ", f_cube)
+print(g,"     ", g_square,"   ", g_cube)
+print(h,"     ", h_square,"   ", h_cube)
+print(j,"     ", j_square,"   ", j_cube)
+print(k,"     ", k_square,"   ", k_cube)
+print(l,"    ", l_square,"  ", l_cube)
