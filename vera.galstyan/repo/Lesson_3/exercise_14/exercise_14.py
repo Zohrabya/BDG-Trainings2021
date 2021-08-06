668bd39812795545f1f39712e01bee164ad12475
ver-galstyan <vera.galstyan@inbox.ru>
2021-08-06 12:16:42 +0400

    Exercise_14 Version1

---
+++ vera.galstyan/repo/Lesson_3/exercise_14/exercise_14.py
@@ -0,0 +1,14 @@
+#! usr/bin/env/python3
+
+print("	Given the algebraic equation ax3 + 7, which of the following, if any, are correct Python statements for this equation?")
+
+print("a.	y = a * x * x * x + 7")
+
+print("b.	y = a * x * x * (x * 7)")
+
+print("c.	y = (a * x) * x * (x +7)")
+
+
+print("\n")
+
+print("Right answer is: ", "ax3+7", "=", "a.")
