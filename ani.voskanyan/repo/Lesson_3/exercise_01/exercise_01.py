75a8a410c7acdfd561adad71713e66a81a15efe6
annievoskanyan <you@example.com>
2021-08-06 11:26:45 +0400

    Exercise_01 Version2

---
+++ ani.voskanyan/repo/Lesson_3/exercise_01/exercise_01.py
@@ -0,0 +1,23 @@
+#a. Using one statement with one print function
+line_a = "*" * 19 + "\n" 
+print(line_a * 4)
+
+#b. Using four statement with one print function
+line_b_1 = "*" * 19
+line_b_2 = "*" * 19
+line_b_3 = "*" * 19
+line_b_4 = "*" * 19
+print(f'''{line_b_1} 
+{line_b_2}
+{line_b_3}
+{line_b_4}''')
+
+#c. Using four statement with four print function
+line_c_1 = "*" * 19
+line_c_2 = "*" * 19
+line_c_3 = "*" * 19
+line_c_4 = "*" * 19
+print(line_c_1)
+print(line_c_2)
+print(line_c_3)
+print(line_c_4)
