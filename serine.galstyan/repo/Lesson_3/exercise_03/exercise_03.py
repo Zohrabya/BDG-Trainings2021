40ae0281cd30b4965f9546facd055eb2bb39be22
serine-galstyan <serine.galstyan98@gmail.com>
2021-08-06 09:40:12 +0400

    Exercise_03 Version1

---
+++ serine.galstyan/repo/Lesson_3/exercise_03/exercise_03.py
@@ -0,0 +1,10 @@
+#!/usr/bin/evn python3
+# a point
+print("*"+"\n"+"*"*2+"\n"+"*"*3+"\n"+"*"*4)
+# b point
+print(" "+"*"+"\n", "*"*2+"\n", "*"*3+"\n", "*"*4)
+# c point
+print("*")
+print("*"*2)
+print("*"*3)
+print("*"*4)
