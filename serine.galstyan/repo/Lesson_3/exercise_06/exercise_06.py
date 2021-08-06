bd157d1574d48700e591b55d576b8b094b3acb48
serine-galstyan <serine.galstyan98@gmail.com>
2021-08-06 09:51:09 +0400

    Exercise_06 Version1

---
+++ serine.galstyan/repo/Lesson_3/exercise_06/exercise_06.py
@@ -0,0 +1,5 @@
+#!/usr/bin/evn python3
+x = input("Enter how much kilograms should convert:")
+num_in_float = float(x)
+count = 2.2*num_in_float
+print("There are", count, "pounds in", num_in_float, "kg.", sep=" ")
