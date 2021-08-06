d2942e2d2adf8be648bc30f6577b969eaacf74c4
annievoskanyan <you@example.com>
2021-08-06 11:48:34 +0400

    Exercise_08 Version1

---
+++ ani.voskanyan/repo/Lesson_3/exercise_08/exercise_08.py
@@ -0,0 +1,6 @@
+number_1 = int(input("Please enter the first number: "))
+number_2 = int(input("Please enter the second number: "))
+number_3 = int(input("Please enter the third number: "))
+total = number_1 + number_2 + number_3
+average = total / 2
+print(f'The total of numbers is {total}. The average of numbers is {average}')
