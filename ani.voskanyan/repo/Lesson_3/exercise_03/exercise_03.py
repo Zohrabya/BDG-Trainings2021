cd61f5a647694fcec26f8296df2883a220e234aa
annievoskanyan <you@example.com>
2021-08-06 11:28:54 +0400

    Exercise_03 Version1

---
+++ ani.voskanyan/repo/Lesson_3/exercise_03/exercise_03.py
@@ -0,0 +1,20 @@
+#a.Using one statement with one print function
+symbols_a = "*\n" + "*" * 2 + "\n" + "*" * 3 + "\n" + "*" * 4
+print(symbols_a)
+
+#b.Using four statement with one print function
+line_b_1 = "*\n" 
+line_b_2 = "*" * 2 + "\n"
+line_b_3 = "*" * 3 + "\n"
+line_b_4 = "*" * 4 + "\n"
+print(line_b_1 + line_b_2 + line_b_3 + line_b_4)
+
+#c.Using four statement with four print function
+line_c_1 = "*"
+line_c_2 = "*" * 2
+line_c_3 = "*" * 3
+line_c_4 = "*" * 4
+print(line_c_1)
+print(line_c_2)
+print(line_c_3)
+print(line_c_4)
