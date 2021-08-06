f953a1730cce2b5747ed974d5c2dab42bf83d9bb
annievoskanyan <you@example.com>
2021-08-06 15:48:17 +0400

    Exercise_13 Version2

--- ani.voskanyan/repo/Lesson_3/exercise_13/exercise_13.py
+++ ani.voskanyan/repo/Lesson_3/exercise_13/exercise_13.py
@@ -3,4 +3,4 @@ cost_tax = meal_cost * 0.20
 tip = meal_cost * 0.18
 total_cost = meal_cost + cost_tax + tip
 
-print(f'Cost of the meal: {meal_cost}$. Tax: {cost_tax:.2f}$. Tip: {tip:.2f}. Total: {total_cost:.2f}$')
+print(f'Tax: {cost_tax:.2f}$. Tip: {tip:.2f}. Total: {total_cost:.2f}$')
