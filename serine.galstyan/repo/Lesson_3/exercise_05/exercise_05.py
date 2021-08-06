2a7acaa3da96b2d4dc4c764ecbfe78bbf05146b3
serine-galstyan <serine.galstyan98@gmail.com>
2021-08-06 14:33:09 +0400

    Exercise_05 Version2

--- serine.galstyan/repo/Lesson_3/exercise_05/exercise_05.py
+++ serine.galstyan/repo/Lesson_3/exercise_05/exercise_05.py
@@ -1,4 +1,4 @@
 #!/usr/bin/evn python3
-x = input("Enter a number: ")
-entered_num = int(x)
-print("The square of", entered_num, "is", str(entered_num**2)+".", sep=" ")
+num_as_str = input("Enter a number: ")
+entered_num = int(num_as_str)
+print("The square of", entered_num, "is", str(entered_num ** 2) + ".", sep=" ")
