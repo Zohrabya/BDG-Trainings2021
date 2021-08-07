6134226e303449cb1b9cbd3ac0b1867e671675b8
diananavasardyan <diana.navasardyan@gmail.com>
2021-08-08 03:25:51 +0400

    Exercise_01 Version1

---
+++ diana.navasardyan/repo/Lesson_3/exercise_01/exercise_01.py
@@ -0,0 +1,23 @@
+#a
+
+chart_1 ='*'*19+'\n'+'*'*19+'\n'+'*'*19+'\n'+'*'*19
+print(chart_1)
+
+#b
+
+chart_2_1 = 19*"*"+'\n'
+chart_2_2 = chart_2_1
+chart_2_3 = chart_2_2
+chart_2_4 = 19*"*"
+print(chart_2_1,chart_2_2,chart_2_3,chart_2_4)
+
+#c
+chart_3_1 = 19*"*"
+chart_3_2 = chart_3_1
+chart_3_3 = chart_3_2
+chart_3_4 = chart_3_2
+print(chart_3_1)
+print(chart_3_2)
+print(chart_3_3)
+print(chart_3_4)
+
