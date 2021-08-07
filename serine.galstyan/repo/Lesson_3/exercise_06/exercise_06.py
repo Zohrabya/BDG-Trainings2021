04f1baf8df274718b3476e4f18f1cbb9441e8793
serine-galstyan <serine.galstyan98@gmail.com>
2021-08-07 18:11:46 +0400

    Exercise_06  Version4

--- serine.galstyan/repo/Lesson_3/exercise_06/exercise_06.py
+++ serine.galstyan/repo/Lesson_3/exercise_06/exercise_06.py
@@ -1,3 +1,4 @@
 #!/usr/bin/evn python3
-entered_num = int(input("Enter a number: "))
-print(entered_num, 2 * entered_num, 3 * entered_num, 4 * entered_num, 5 * entered_num, sep="---")
+
+entered_num = input("Enter a number: ")
+print(entered_num, 2 * int(entered_num), 3 * int(entered_num), 4 * int(entered_num), 5 * int(entered_num), sep="---")
