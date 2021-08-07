a1cd4386cd6bc2529736df2f6afb14cbb4c60369
serine-galstyan <serine.galstyan98@gmail.com>
2021-08-07 14:27:45 +0400

    Exercise_01 Version3

--- serine.galstyan/repo/Lesson_3/exercise_01/exercise_01.py
+++ serine.galstyan/repo/Lesson_3/exercise_01/exercise_01.py
@@ -1,12 +1,9 @@
 #!/usr/bin/evn python3
 # a point
-print("a point of exercise:\n")
 print(("*" * 19) + "\n" + ("*" * 19) + "\n" + ("*" * 19) + "\n" + ("*" * 19))
 # b point
-print("\n b point of exercise:")
 print("\n " + "*" * 19 + "\n", "*" * 19 + "\n", "*" * 19 + "\n", "*" * 19 + "\n")
 # c point
-print("c point of exercise:\n")
 print("*" * 19)
 print("*" * 19)
 print("*" * 19)
