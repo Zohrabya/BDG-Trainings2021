7c26e3c883c5e8b8e1b3a8dfeb65f0b3867bac04
ani2025 <ANIHARUTYUNYAN61@GMAIL.COM>
2021-08-09 00:42:35 +0400

    Exercise_18_version1

---
+++ ani.ter-markosyan/repo/Lesson_3/exercise_18/exercise_18.py
@@ -0,0 +1,3 @@
+radius = int(input("Enter a radius of a circle: "))
+pi = float(3.14159)
+print("circle's diameter is", 2 * radius,"circle's circumference is",2 * pi * radius,"circle's area is",pi * radius * radius)
