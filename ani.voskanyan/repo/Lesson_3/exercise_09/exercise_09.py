aa3bc66576851239e3bf727daccd29323a843f9c
diananavasardyan <diana.navasardyan@gmail.com>
2021-08-08 04:37:12 +0400

    Exercise_02 Version1

--- ani.voskanyan/repo/Lesson_3/exercise_09/exercise_09.py
+++
@@ -1,5 +0,0 @@
-meal_price = float(input("Please, tell price of the meal: "))
-percent_tip = int(input("Percent tip: "))
-tip_amount = meal_price * percent_tip / 100
-total = meal_price + tip_amount
-print(f"Tip amount is {tip_amount}. Your total bill is equal to {total}")
---
+++ diana.navasardyan/repo/Lesson_3/exercise_02/exercise_02.py
@@ -0,0 +1,17 @@
+#a
+picture_1 = 19*"*" + "\n" + "*" + 17* " " + "*" "\n" + "*"+ 17* " " + "*" "\n" + 19*"*"
+print(picture_1)
+
+#b
+picture_2_1 = 19*"*" + "\n"
+picture_2_2 = "*" + 17* " " + "*" "\n"
+picture_2_3 = picture_2_2
+picture_2_4 = 19*"*"
+print(picture_2_1, picture_2_2, picture_2_3, picture_2_4)
+
+#c
+picture_3_1 = picture_2_1
+picture_3_2 = picture_2_2
+picture_3_3 = picture_2_3
+picture_3_4 = picture_2_4
+print(picture_3_1, picture_3_2, picture_3_3, picture_3_4)
