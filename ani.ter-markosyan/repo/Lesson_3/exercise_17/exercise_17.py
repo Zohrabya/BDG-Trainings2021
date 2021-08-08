afd91114ec193c16de5e2b6caa7fe1356470c7fd
ani2025 <ANIHARUTYUNYAN61@GMAIL.COM>
2021-08-08 23:48:12 +0400

    Exercise_17_version1

---
+++ ani.ter-markosyan/repo/Lesson_3/exercise_17/exercise_17.py
@@ -0,0 +1,17 @@
+number1, number2, number3 = map(int,input("Input three different numbers: "). split())
+sum = number1 + number2 + number3
+average = (number1 + number2 + number3) / 3
+product = number1 * number2 * number3
+if (number1 > number2) and (number1 > number3):
+    largest = number1
+elif (number2 > number1) and (number2 > number3):
+    largest = number2
+else:
+    largest = number3
+if (number1 < number2) and (number1 < number3):
+    smallest = number1
+elif (number2 < number1) and (number2 < number3):
+    smallest = number2
+else:
+    smallest = number3
+print("Sum is",sum), print("Average is",average),print("Product is",product),print("Smallest is", smallest),print("Largest is",largest)
