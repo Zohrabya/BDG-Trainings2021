d9aa799f5dae91830a596aa4eaf3ab32a100a611
lilit.sargsyan <sargsyan695@gmail.com>
2021-08-08 14:16:29 +0400

    Exercise_01 version2

--- lilit.sargsyan/repo/Lesson_3/exercise_01/exercise_01.py
+++ lilit.sargsyan/repo/Lesson_3/exercise_01/exercise_01.py
@@ -1,18 +1,15 @@
-a)
-line_1_bdg='*'*12+'\n'+'*'*12+'\n'+'*'*12+'\n'+'*'*12
-print(line_1_bdg)
-b)
-line_1_lesson_3="*"*12 +'\n'
-line_2_lesson_3="*"*12 + "\n"
-line_3_lesson_3=line_1_lesson_3
-line_4_lesson_3=line_1_lesson_3
+print('*' *12 + '\n' + '*'*12 + '\n'+ '*'*12 + '\n' + '*'*12)
+
+line_1_lesson_3= "*"*12 +'\n'
+line_2_lesson_3= "*"*12 + "\n"
+line_3_lesson_3= line_1_lesson_3
+line_4_lesson_3= line_1_lesson_3
 print(line_1_lesson_3,line_2_lesson_3,line_3_lesson_3,line_4_lesson_3)
 
-c)
-line_1_lesson_3='*'*12
-line_2_lesson_3='*'*12
-line_3_lesson_3='*'*12
-line_4_lesson_3='*'*12
+line_1_lesson_3= '*'*12
+line_2_lesson_3= '*'*12
+line_3_lesson_3= '*'*12
+line_4_lesson_3= '*'*12
 print(line_1_lesson_3)
 print(line_2_lesson_3)
 print(line_3_lesson_3)
