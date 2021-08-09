91aa4dee6ff0180d974b29760cf38dbfeed9e204
annievoskanyan <you@example.com>
2021-08-10 00:35:09 +0400

    Exercise_14 Version2

--- ani.voskanyan/repo/Lesson_3/exercise_14/exercise_14.py
+++ ani.voskanyan/repo/Lesson_3/exercise_14/exercise_14.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-a. y = a * x * x * x + 7 # The most correct as doesn't have odd brackets
+a. y = a * x * x * x + 7 # The most correct as doesn't contain odd brackets
 
 d. y = (a*x) * x * x + 7 
 
 e. y = a * (x * x * x) + 7
+s
