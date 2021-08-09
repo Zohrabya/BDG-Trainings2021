2fd800726c41f8f52c0c01861f5a87a35721aecb
annievoskanyan <you@example.com>
2021-08-09 23:29:47 +0400

    Exercise_13 Version3

--- ani.voskanyan/repo/Lesson_3/exercise_13/exercise_13.py
+++ ani.voskanyan/repo/Lesson_3/exercise_13/exercise_13.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python3
 meal_cost = float(input("Please, enter the cost of the meal: "))
-cost_tax = meal_cost * 0.20
-tip = meal_cost * 0.18
+TAX_PERCENTAGE = 0.20
+cost_tax = meal_cost * TAX_PERCENTAGE
+TIP_PERCENTAGE = 0.18
+tip = meal_cost * TIP_PERCENTAGE
 total_cost = meal_cost + cost_tax + tip
 
 print(f'Tax: {cost_tax:.2f}$. Tip: {tip:.2f}. Total: {total_cost:.2f}$')
