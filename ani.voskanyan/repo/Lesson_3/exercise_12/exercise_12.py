e657b6944b6741679f678c6bb730f6d25e860d02
annievoskanyan <you@example.com>
2021-08-09 23:56:23 +0400

    Exercise_12 Version6

--- ani.voskanyan/repo/Lesson_3/exercise_12/exercise_12.py
+++ ani.voskanyan/repo/Lesson_3/exercise_12/exercise_12.py
@@ -1,14 +1,5 @@
-container_size = float(input("Please, enter size of the drink container (liter): "))
-container_quantity = int(input("Please, enter quantity of drink containers which have that size:  "))
-total_refund = 0
-
-if 0 < container_size <= 1:
-    deposit = 0.10
-    total_refund += (deposit * container_quantity)
-    print(f'Refund: {deposit:.2f}$. Your total refund is {total_refund:.2f}$')
-elif container_size > 1:
-    deposit = 0.25
-    total_refund += (deposit * container_quantity)
-    print(f'Refund: {deposit:.2f}$. Your total refund is {total_refund:.2f}$')
-else:
-    print("Something went wrong.")
+#!/usr/bin/env python3
+container_quantity_less = int(input("Please, enter quantity of drink containers holding one liter and less : "))
+container_quantity_more = int(input("Please, enter quantity of drink containers holding more than one liter:  "))
+total_refund = 0.10 * container_quantity_less + 0.25 * container_quantity_more
+print("Your total refund:", total_refund)
