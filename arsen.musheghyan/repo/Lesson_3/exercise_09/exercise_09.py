f2b891f19cdace709f7fd8b17bb65ea017fbae81
Arsen1978 <jobforars@gmail.com>
2021-08-09 12:47:10 +0400

    Exercise_09 Version2

--- arsen.musheghyan/repo/Lesson_3/exercise_09/exercise_09.py
+++ arsen.musheghyan/repo/Lesson_3/exercise_09/exercise_09.py
@@ -1,6 +1,5 @@
-print ("Exercise N 9")
-a= int(input("Write price of the meal:"))
-b= int(input("How many percent tip do you want to leave:"))
-c = a*b/100
-print ("Your price for tip",c)
-print ("Total amount",c+a)
+meal = int(input("Write price of the meal: "))
+percent  = int(input("How many percent tip do you want to leave: "))
+tip = meal * percent / 100
+print ("Your price for tip: ", tip)
+print ("Total amount: ", meal + tip)
