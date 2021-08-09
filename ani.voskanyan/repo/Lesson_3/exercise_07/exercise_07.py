552298158833605aff0bf91aae0ab779db9a110e
annievoskanyan <you@example.com>
2021-08-09 23:06:52 +0400

    Exercise_7 Version2

--- ani.voskanyan/repo/Lesson_3/exercise_07/exercise_07.py
+++ ani.voskanyan/repo/Lesson_3/exercise_07/exercise_07.py
@@ -1,3 +1,3 @@
+#!/usr/bin/env python3
 weight_input_kg = float(input("Please enter your wwight in kg : "))
-weight_in_pounds = weight_input_kg * 2.2
-print("Your weight in pounds is equal to", weight_in_pounds)
+print("Your weight in pounds is equal to", weight_input_kg * 2.2)
