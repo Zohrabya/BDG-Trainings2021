e4831bea9e4953d73c165d28e257444f718eb2a3
Ani Sadaghyan <anisadaghyan@gmail.com>
2021-08-09 17:04:06 +0400

    ani.sadaghyan_EXE_16 version3

--- ani.sadaghyan/repo/Lesson_3/exercise_16/exercise_16.py
+++ ani.sadaghyan/repo/Lesson_3/exercise_16/exercise_16.py
@@ -1,7 +1,7 @@
 first = int(input("Insert first number \n"))
 second = int(input("Insert second number \n"))
 if first > second:
-    print("{first, is larger ")
+    print("first, is larger ")
 elif first < second:
     print("second,  is larger")
 else:
