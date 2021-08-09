29cf117a78d1b472ab8a0fa1260ff430474191fe
lilit.sargsyan <sargsyan695@gmail.com>
2021-08-09 16:02:10 +0400

    Exercise_08 Version1

---
+++ lilit.sargsyan/repo/Lesson_3/exercise_08/exercise_08.py
@@ -0,0 +1,6 @@
+first_num = int(input('Enter first number:' ))
+second_num = int(input('Enter second number:' ))
+third_num = int(input('Enter third number:'))
+total = first_num + second_num + third_num
+average = total / 3
+print('Total:', total, '\nAverage:', (average))
