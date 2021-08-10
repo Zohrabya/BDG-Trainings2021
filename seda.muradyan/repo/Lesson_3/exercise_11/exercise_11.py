3f88832107837e8fb0a53c307c2051da86410f5e
sedamur-qa <seda.hrachovna@gmail.com>
2021-08-10 21:38:41 +0400

    Exercise_11 Version 1

---
+++ seda.muradyan/repo/Lesson_3/exercise_11/exercise_11.py
@@ -0,0 +1,10 @@
+#home work Lesson 3 exe_11
+
+width = float(input("Enter your field width: "))
+lenght = float(input("Enter your field lenght: "))
+acresisequal = 43560
+
+output = "The area of field from acres" + str(width * lenght/acresisequal)
+
+
+print(output)
