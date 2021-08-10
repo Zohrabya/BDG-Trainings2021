da5a0c8c3ad85ee20134a8e5e58a5af8be539f7f
Ani Sadaghyan <anisadaghyan@gmail.com>
2021-08-10 11:27:02 +0400

    ani.sadaghyan_EXE_06 version2

--- ani.sadaghyan/repo/Lesson_3/exercise_06/exercise6.py
+++ ani.sadaghyan/repo/Lesson_3/exercise_06/exercise6.py
@@ -1,2 +1,2 @@
-number=int(input("Enter a number"));
-print("{0}---{1}---{2}---{3}---{4}".format(number, number * 2, number * 3, number * 4, number * 5));
+number=int(input("Enter a number:"))
+print("{0}---{1}---{2}---{3}---{4}".format(number, number * 2, number * 3, number * 4, number * 5))
