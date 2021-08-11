01dae65fcc050812a0379c2ec861473a89440253
ani2025 <ANIHARUTYUNYAN61@GMAIL.COM>
2021-08-11 11:11:51 +0400

    Exercise_10_version2

--- ani.ter-markosyan/repo/Lesson_3/Exercise_10/exercise_10.py
+++ ani.ter-markosyan/repo/Lesson_3/Exercise_10/exercise_10.py
@@ -1,3 +1,3 @@
 width = float(input("Enter a width of a room (in meter): "))
 lenght = float(input("Enter a lenght of a room (in meter): "))
-print("The area of a room is",width * lenght, "square meter")
+print("The area of a room is", round(width * lenght, 2), "square meter", end = ".")
