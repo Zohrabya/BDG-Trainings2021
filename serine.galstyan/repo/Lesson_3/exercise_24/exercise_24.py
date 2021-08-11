897566cb5984fd0261dd52858f19357e14d9af65
serine-galstyan <serine.galstyan98@gmail.com>
2021-08-11 11:53:18 +0400

    Exercise_24 Version1

---
+++ serine.galstyan/repo/Lesson_3/exercise_24/exercise_24.py
@@ -0,0 +1,25 @@
+#!/usr/bin/evn python3
+
+print("integer  square     cube")
+integer = 0
+print(integer, integer ** 2, integer ** 3, sep="          ")
+integer = 1
+print(integer, integer ** 2, integer ** 3, sep="          ")
+integer = 2
+print(integer, integer ** 2, integer ** 3, sep="          ")
+integer = 3
+print(integer, integer ** 2, integer ** 3, sep="          ")
+integer = 4
+print(integer, integer ** 2, integer ** 3, sep="         ")
+integer = 5
+print(integer, integer ** 2, integer ** 3, sep="         ")
+integer = 6
+print(integer, integer ** 2, integer ** 3, sep="         ")
+integer = 7
+print(integer, integer ** 2, integer ** 3, sep="         ")
+integer = 8
+print(integer, integer ** 2, integer ** 3, sep="         ")
+integer = 9
+print(integer, integer ** 2, integer ** 3, sep="         ")
+integer = 10
+print(integer, integer ** 2, integer ** 3, sep="        ")
