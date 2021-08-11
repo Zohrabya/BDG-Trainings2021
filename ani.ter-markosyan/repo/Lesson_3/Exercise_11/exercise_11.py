f67905c73e797af573ec8984363eaa90b253c313
ani2025 <ANIHARUTYUNYAN61@GMAIL.COM>
2021-08-11 11:26:05 +0400

    Exercise_11_version2

--- ani.ter-markosyan/repo/Lesson_3/Exercise_11/exercise_11.py
+++ ani.ter-markosyan/repo/Lesson_3/Exercise_11/exercise_11.py
@@ -1,3 +1,4 @@
 width = float(input("Enter a widht of a field (in feet): "))
 lenght = float(input("Enter a lenght of a field (in feet): "))
-print("The area of a field is", round(width * lenght / 43560,2),"acres.")
+ACRE = 43560 #square feet
+print("The area of a field is", round(width * lenght / ACRE, 2), "acres.")
