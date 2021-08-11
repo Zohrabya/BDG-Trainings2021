7239261cf924aee5c9553c168b183a5a1db9b70b
serine-galstyan <serine.galstyan98@gmail.com>
2021-08-11 12:04:52 +0400

    Exercise_27 Version1

---
+++ serine.galstyan/repo/Lesson_3/exercise_27/exercise_27.py
@@ -0,0 +1,37 @@
+#!/usr/bin/evn python3
+
+print("Decimal Octal Hexadecimal  Binary")
+integer = 1
+print(integer, str(oct(integer))[2], str(hex(integer))[2], str(bin(integer))[2], sep="         ")
+integer = 2
+print(integer, str(oct(integer))[2], str(hex(integer))[2], str(bin(integer))[2:4], sep="         ")
+integer = 3
+print(integer, str(oct(integer))[2], str(hex(integer))[2], str(bin(integer))[2:4], sep="         ")
+integer = 4
+print(integer, str(oct(integer))[2], str(hex(integer))[2], str(bin(integer))[2:5], sep="         ")
+integer = 5
+print(integer, str(oct(integer))[2], str(hex(integer))[2], str(bin(integer))[2:5], sep="         ")
+integer = 6
+print(integer, str(oct(integer))[2], str(hex(integer))[2], str(bin(integer))[2:5], sep="         ")
+integer = 7
+print(integer, str(oct(integer))[2], str(hex(integer))[2], str(bin(integer))[2:5], sep="         ")
+integer = 8
+print(integer, str(oct(integer))[2:5], str(hex(integer))[2], str(bin(integer))[2:6], sep="        ")
+integer = 9
+print(integer, str(oct(integer))[2:5], str(hex(integer))[2], str(bin(integer))[2:6], sep="        ")
+integer = 10
+print(integer, str(oct(integer))[2:5], str(hex(integer))[2].upper(), str(bin(integer))[2:6], sep="        ")
+integer = 11
+print(integer, str(oct(integer))[2:5], str(hex(integer))[2].upper(), str(bin(integer))[2:6], sep="        ")
+integer = 12
+print(integer, str(oct(integer))[2:5], str(hex(integer))[2].upper(), str(bin(integer))[2:6], sep="        ")
+integer = 13
+print(integer, str(oct(integer))[2:5], str(hex(integer))[2].upper(), str(bin(integer))[2:6], sep="        ")
+integer = 14
+print(integer, str(oct(integer))[2:5], str(hex(integer))[2].upper(), str(bin(integer))[2:6], sep="        ")
+integer = 15
+print(integer, str(oct(integer))[2:5], str(hex(integer))[2].upper(), str(bin(integer))[2:6], sep="        ")
+integer = 16
+print(integer, str(oct(integer))[2:5], str(hex(integer))[2:5], str(bin(integer))[2:7], sep="        ")
+integer = 17
+print(integer, str(oct(integer))[2:5], str(hex(integer))[2:5], str(bin(integer))[2:7], sep="        ")
