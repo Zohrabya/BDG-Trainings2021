089f46dea71916ba87cee08c67f6ba03fd52f0a9
ani2025 <ANIHARUTYUNYAN61@GMAIL.COM>
2021-08-11 11:05:52 +0400

    Exercise_9_version2

--- ani.ter-markosyan/repo/Lesson_3/exercise_09/exercise_09.py
+++ ani.ter-markosyan/repo/Lesson_3/exercise_09/exercise_09.py
@@ -1,3 +1,3 @@
 price = int(input("Enter the price of meal: "))
-tip = int (input("Enter the percent tip you want to leave: "))
-print(price * tip / 100, price + price * tip / 100)
+tip = int(input("Enter the percent tip you want to leave: "))
+print("The tip amount is", price * tip / 100,), print("Total amount is", price + price * tip / 100)
