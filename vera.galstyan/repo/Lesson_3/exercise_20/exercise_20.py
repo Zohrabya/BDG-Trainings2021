c0806626abfd802a70c0b766ebb286f98ea82332
ver-galstyan <vera.galstyan@inbox.ru>
2021-08-11 18:51:13 +0400

    Exercise_20 Version1

---
+++ vera.galstyan/repo/Lesson_3/exercise_20/exercise_20.py
@@ -0,0 +1,11 @@
+#!usr/bin/env python3
+#1
+
+number1, number2, number3, number4, number5 = map(int,input("Enter five different numbers: ").split())
+#2
+
+largest = max(number1, number2, number3, number4, number5)
+smallest = min(number1, number2, number3, number4, number5)
+
+#3
+print("Largest is", largest,"Smallest is", smallest)
