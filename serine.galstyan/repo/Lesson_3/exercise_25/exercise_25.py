f113848006975d282efdf3cc8538bf8f3fd23049
serine-galstyan <serine.galstyan98@gmail.com>
2021-08-11 11:55:39 +0400

    Exercise_25 Version1

---
+++ serine.galstyan/repo/Lesson_3/exercise_25/exercise_25.py
@@ -0,0 +1,5 @@
+#!/usr/bin/evn python3
+
+integer = input("Enter a number(four-digit):")
+sum_of_digits = int(integer[0]) + int(integer[1]) + int(integer[2]) + int(integer[3])
+print(integer[0] + "+" + integer[1] + "+" + integer[2] + "+" + integer[3] + "=" + str(sum_of_digits))
