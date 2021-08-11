6d655ff9c5c10d2e90a3834d542550d045bdc97a
Ani Sadaghyan <anisadaghyan@gmail.com>
2021-08-11 12:55:28 +0400

    exercise_10 version1

---
+++ ani.sadaghyan/repo/Lesson_3/exercise_10/exercise_10.py
@@ -0,0 +1,5 @@
+length = float(input("Enter the length of the room: "))
+width = float(input("Enter the width of the room: "))
+unit = input("Select the unit you are more comfortable working with (meters or feet):  ")
+area = length * width
+print("The area is", area, unit)
