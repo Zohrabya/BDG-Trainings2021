124ac6351d8c63d6b8eea7a1ee72d6176ae85540
ani2025 <ANIHARUTYUNYAN61@GMAIL.COM>
2021-08-12 00:36:39 +0400

    Exercise_24_version1

---
+++ ani.ter-markosyan/repo/Lesson_3/Exercise_24/exercise_24.py
@@ -0,0 +1,16 @@
+#!usr/bin/evn python3
+
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+squared_numbers = [number ** 2 for number in numbers]
+cubed_numbers = [number ** 3 for number in numbers]
+print("integer" + "\t", "square" + "\t", "cube")
+print(numbers[0], "\t", squared_numbers[0], "\t", cubed_numbers[0])
+print(numbers[1], "\t", squared_numbers[1], "\t", cubed_numbers[1])
+print(numbers[2], "\t", squared_numbers[2], "\t", cubed_numbers[2])
+print(numbers[3], "\t", squared_numbers[3], "\t", cubed_numbers[3])
+print(numbers[4], "\t", squared_numbers[4], "\t", cubed_numbers[4])
+print(numbers[5], "\t", squared_numbers[5], "\t", cubed_numbers[5])
+print(numbers[6], "\t", squared_numbers[6], "\t", cubed_numbers[6])
+print(numbers[7], "\t", squared_numbers[7], "\t", cubed_numbers[7])
+print(numbers[8], "\t", squared_numbers[8], "\t", cubed_numbers[8])
+print(numbers[9], "\t", squared_numbers[9], "\t", cubed_numbers[9])
