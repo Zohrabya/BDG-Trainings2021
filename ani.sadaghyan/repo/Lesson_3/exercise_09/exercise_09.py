ac3844442194588092fcf2fc6b11460360c6a278
Ani Sadaghyan <anisadaghyan@gmail.com>
2021-08-11 12:45:38 +0400

    exercise_09 version1

---
+++ ani.sadaghyan/repo/Lesson_3/exercise_09/exercise_09.py
@@ -0,0 +1,5 @@
+meal = int(input("Write price of the meal: "))
+percent  = int(input("Percent tip: "))
+tip = meal * percent / 100
+print ("Your price for tip: ", tip)
+print ("Total amount: ", meal + tip)
