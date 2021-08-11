4bfc20100f57bf200e595728e43ae1460a3a972a
ani2025 <ANIHARUTYUNYAN61@GMAIL.COM>
2021-08-11 10:50:37 +0400

    Exercise_7_version3

--- ani.ter-markosyan/repo/Lesson_3/exercise_07/exercisr_07.py
+++ ani.ter-markosyan/repo/Lesson_3/exercise_07/exercisr_07.py
@@ -1,2 +1,2 @@
-weight = int(input("Enter your weight in kg: "))
-print(round(weight * 2.2,2),"pounds")
+weight = float(input("Enter your weight in kg: "))
+print("Your weight in pounds is", round(weight * 2.2,2), end = ".")
