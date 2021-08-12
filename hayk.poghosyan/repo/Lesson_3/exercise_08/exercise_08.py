1e8afa8785ab747a4977abcfb8b4f6739ab5e889
hpoghosyan <hayk.m.poghosyan@gmail.com>
2021-08-12 22:22:07 +0400

    Exercise_08 Version2

--- hayk.poghosyan/repo/Lesson_3/exercise_08/exercise_08.py
+++ hayk.poghosyan/repo/Lesson_3/exercise_08/exercise_08.py
@@ -1,7 +1,7 @@
 # 8
-number_1 = int(input("Enter a number"))
-number_2 = int(input("Enter a number"))
-number_3 = int(input("Enter a number"))
+number_1 = int(input("Enter a number: "))
+number_2 = int(input("Enter a number: "))
+number_3 = int(input("Enter a number: "))
 total = number_1 + number_2 + number_3
 average = total/3
-print(total,"\n",average)
+print(" The sum of numbers are: " + str(total) +'\n',"The average of numbers are: " + str(average))
