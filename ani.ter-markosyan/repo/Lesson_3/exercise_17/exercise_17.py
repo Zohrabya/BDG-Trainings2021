5fc91b3d16e5e1fabbca9399fa41b3de02e54956
ani2025 <ANIHARUTYUNYAN61@GMAIL.COM>
2021-08-12 17:45:44 +0400

    Exercise_17_version2

--- ani.ter-markosyan/repo/Lesson_3/exercise_17/exercise_17.py
+++ ani.ter-markosyan/repo/Lesson_3/exercise_17/exercise_17.py
@@ -1,17 +1,21 @@
-number1, number2, number3 = map(int,input("Input three different numbers: ").split())
+#!usr/bin/evn python3
+#number1, number2, number3 = map(int,input("Input three different numbers: ").split())
+
+number1, number2, number3 = input("Input three different integers: ").split()
+number1, number2, number3 = [int(number1), int(number2), int(number3)]
 sum = number1 + number2 + number3
-average = (number1 + number2 + number3) / 3
+average = round(number1 + number2 + number3 / 3, 2)
 product = number1 * number2 * number3
-if (number1 > number2) and (number1 > number3):
+if number1 > number2 and number1 > number3:
     largest = number1
-elif (number2 > number1) and (number2 > number3):
+elif number2 > number1 and number2 > number3:
     largest = number2
 else:
     largest = number3
-if (number1 < number2) and (number1 < number3):
+if number1 < number2 and number1 < number3:
     smallest = number1
-elif (number2 < number1) and (number2 < number3):
+elif number2 < number1 and number2 < number3:
     smallest = number2
 else:
     smallest = number3
-print("Sum is",sum),print("Average is",average),print("Product is",product),print("Smallest is", smallest),print("Largest is",largest)
+print("Sum is", sum), print("Average is", average), print("Product is", product), print("Smallest is", smallest), print("Largest is", largest)
