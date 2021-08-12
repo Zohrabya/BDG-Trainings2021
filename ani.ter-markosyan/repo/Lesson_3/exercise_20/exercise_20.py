809a4de4ec2ed4a3300ea7a76c6387fd55dff69a
ani2025 <ANIHARUTYUNYAN61@GMAIL.COM>
2021-08-12 23:35:36 +0400

    Exercise_20_version2

--- ani.ter-markosyan/repo/Lesson_3/exercise_20/exercise_20.py
+++ ani.ter-markosyan/repo/Lesson_3/exercise_20/exercise_20.py
@@ -1,4 +1,10 @@
-num1, num2, num3, num4, num5 = map(int,input("Enter five different numbers: ").split())
+#!usr/bin/evn python3
+# num1, num2, num3, num4, num5 = map(int,input("Enter five different numbers: ").split())
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+num4 = int(input("Enter fourth number: "))
+num5 = int(input("Enter fifth number: "))
 largest = max(num1, num2, num3, num4, num5)
 smallest = min(num1, num2, num3, num4, num5)
-print("Largest is", largest,"Smallest is", smallest)
+print("Largest is", largest, "\n", "Smallest is", smallest)
