098705e62e4790b2da5876bfbc26c0086bed8509
ani2025 <ANIHARUTYUNYAN61@GMAIL.COM>
2021-08-12 17:00:38 +0400

    Exercise_16_version2

--- ani.ter-markosyan/repo/Lesson_3/exercise_16/Exercise_16.py
+++ ani.ter-markosyan/repo/Lesson_3/exercise_16/Exercise_16.py
@@ -1,8 +1,9 @@
+#!usr/bin/evn python3
 first_number = int(input("Enter a number: "))
 second_number = int(input("Enter another number: "))
 if (first_number > second_number):
-    print(first_number,"is larger")
-if (first_number < second_number):
-    print(second_number,"is larger")  
+    print(first_number, "is larger.")
+elif (second_number > first_number):
+    print(second_number, "is larger.")  
 else:
     print("These numbers are equal.")  
