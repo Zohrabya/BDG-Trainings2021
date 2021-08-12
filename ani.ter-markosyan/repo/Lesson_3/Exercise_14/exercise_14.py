adeac2e0b97fc7cc6e53c95b667dcc894387d04d
ani2025 <ANIHARUTYUNYAN61@GMAIL.COM>
2021-08-12 11:45:34 +0400

    Exercise_14_version3

--- ani.ter-markosyan/repo/Lesson_3/Exercise_14/exercise_14.py
+++ ani.ter-markosyan/repo/Lesson_3/Exercise_14/exercise_14.py
@@ -1,2 +1,4 @@
+#!usr/bin/evn python3
+# a. y = a * x * x * x + 7
+# d. y = (a * x) * x * x + 7
 # e. y = a * (x * x * x) + 7
-# a, d, and e because there is no difference which product you will first do you will get the same result.
