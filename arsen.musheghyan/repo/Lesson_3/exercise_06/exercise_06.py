e0da0faf26b3813a8352b2955fc7bf13ea1ccc5b
Arsen1978 <jobforars@gmail.com>
2021-08-13 10:26:47 +0400

    Exercise_06 Version3

--- arsen.musheghyan/repo/Lesson_3/exercise_06/exercise_06.py
+++ arsen.musheghyan/repo/Lesson_3/exercise_06/exercise_06.py
@@ -1,2 +1,2 @@
 number = int(input("Enter a number: "))
-print (number,'---',2 * number,'---',3 * number,'---',4 * number,'---',number*5)
+print (number,'---',2 * number,'---',3 * number,'---',4 * number,'---',number * 5)
