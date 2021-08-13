426af50eae9a3eb4543fb43e02eb3d4c65fa1702
lilit.sargsyan <sargsyan695@gmail.com>
2021-08-13 12:04:16 +0400

    Exercise_18 version1

---
+++ lilit.sargsyan/repo/Lesson_3/exercise_18/exercise_18.py
@@ -0,0 +1,3 @@
+radius_circle = int(input("Enter radius:"))
+pi = 3.14159
+print("Circle's Diameter:", 2 * radius_circle, " Circumference:", round(2 * pi * radius_circle), " Area:", round(2 * pi * radius_circle ** 2), ".", sep=" ")
