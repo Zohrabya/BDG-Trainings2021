d507b49441151dff5f4a0e9e72e0563914241dbc
Arsen1978 <jobforars@gmail.com>
2021-08-13 12:02:13 +0400

    Exercise_10 Version1

---
+++ arsen.musheghyan/repo/Lesson_3/exercise_10/Exercise_10.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+width = float(input("Please input width number of room in meters: "))
+leght = float(input("Please input legth number of room in meters: "))
+print ("Area of the room is: ", round(width * leght))
