092ba32e83632578d331194d2a57e116982eaed2
lilit.sargsyan <sargsyan695@gmail.com>
2021-08-13 15:30:03 +0400

    Exercise_21 version_1

---
+++ lilit.sargsyan/repo/Lesson_3/exercise_21/exercise_21.py
@@ -0,0 +1,5 @@
+num_for_calculation = int(input("Enter a number: "))
+if (num_for_calculation % 2) == 0:
+   print("{0} is Even". format(num_for_calculation))
+else:
+   print("{0} is Odd". format(num_for_calculation))
