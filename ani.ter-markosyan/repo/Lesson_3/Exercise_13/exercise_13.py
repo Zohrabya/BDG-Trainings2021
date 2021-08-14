4de522740dfe0688dd373d651a9356b0e53f6278
ani2025 <ANIHARUTYUNYAN61@GMAIL.COM>
2021-08-14 16:57:00 +0400

    Exercise_13_version3

--- ani.ter-markosyan/repo/Lesson_3/Exercise_13/exercise_13.py
+++ ani.ter-markosyan/repo/Lesson_3/Exercise_13/exercise_13.py
@@ -1,9 +1,8 @@
+#!usr/bin/evn python
 cost = int(input("Enter the cost of your meal: "))
 TAX_RATE = 5.2
+TIP_RATE = 18
 tax = (round(TAX_RATE * cost  / 100,2))
-tip = (round(18 * cost / 100,2))
+tip = (round( TIP_RATE * cost / 100,2))
 total = (round(tax + tip + cost, 2))
-# print("Tax amount is: ", tax, "tip amount is: ", tip, "grand total is: ", total)
-#I think the following variant is more beautiful and readable 
 print("Tax amount is: ", tax, end = "\n"), print("Tip amount is: ", tip, end = "\n"), print("Grand total is: ", total)
-
