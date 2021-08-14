f16c57359bd52c9c4b1e4966aae691e53f44f32b
LusineSar <lusinesargsyan85@yahoo.com>
2021-08-14 18:45:51 +0400

    Exercise_01 Version1

---
+++ lusine.sargsyan/repo/Lesson3/Exercise_01/Exercise_01.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+#a
+
+print(4 * (19 * "*" + "\n"))
+
+
+#b
+
+print(19 * "*", 19 * "*", 19 * "*", 19 * "*", sep = "\n" )
+
+
+#c
+print(19 * "*")
+print(19 * "*")
+print(19 * "*")
+print(19 * "*")
