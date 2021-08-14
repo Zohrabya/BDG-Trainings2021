5374c65f0150204de89e1bdcdd4f5ada9fdedffe
ani2025 <ANIHARUTYUNYAN61@GMAIL.COM>
2021-08-14 18:24:30 +0400

    Exercise_12_version3

--- ani.ter-markosyan/repo/Lesson_3/exercise_02/Exercise_12/exercise_12.py
+++
@@ -1,6 +0,0 @@
-size = float(input("Enter a size of container (liter): "))
-number = float(input("Enter the number of containers: "))
-if (size > 1):
-    print("Your deposit is",round(number * 0.25,2),"$")
-else:
-    print("Your deposit is",round(number * 0.10,2), "$")    
