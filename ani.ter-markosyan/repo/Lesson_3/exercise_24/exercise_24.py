5bdf2f3e60ada025d7856b746aba49d2bee65630
ani2025 <ANIHARUTYUNYAN61@GMAIL.COM>
2021-08-15 00:35:23 +0400

    exercise_24_version1

---
+++ ani.ter-markosyan/repo/Lesson_3/exercise_24/exercise_24.py
@@ -0,0 +1,23 @@
+#!usr/bin/evn python3
+
+print("integer" + "\t", "square" + "\t", "cube")
+number1 = 0
+print(number1, "\t", number1 ** 2, "\t", number1 ** 3)
+number2 = 1
+print(number2, "\t", number2 ** 2, "\t", number2 ** 3)
+number3 = 2
+print(number3, "\t", number3 ** 2, "\t", number3 ** 3)
+number4 = 3
+print(number4, "\t", number4 ** 2, "\t", number4 ** 3)
+number5 = 4
+print(number5, "\t", number5 ** 2, "\t", number5 ** 3)
+number6 = 5
+print(number6, "\t", number6 ** 2, "\t", number6 ** 3)
+number7 = 6
+print(number7, "\t", number7 ** 2, "\t", number7 ** 3)
+number8 = 7
+print(number8, "\t", number8 ** 2, "\t", number8 ** 3)
+number9 = 8
+print(number9, "\t", number9 ** 2, "\t", number9 ** 3)
+number10 = 9
+print(number10, "\t", number10 ** 2, "\t", number10 ** 3)
