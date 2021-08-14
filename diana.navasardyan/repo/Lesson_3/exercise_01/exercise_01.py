2478f853d386c309db42aba42784198ca6748a48
diananavasardyan <diana.navasardyan@gmail.com>
2021-08-14 16:09:33 +0400

    Exercise_ 01 Version2

--- diana.navasardyan/repo/Lesson_3/exercise_01/exercise_01.py
+++ diana.navasardyan/repo/Lesson_3/exercise_01/exercise_01.py
@@ -1,23 +1,15 @@
 #a
 
-chart_1 ='*'*19+'\n'+'*'*19+'\n'+'*'*19+'\n'+'*'*19
-print(chart_1)
-
+print('*'*19+'\n'+'*'*19+'\n'+'*'*19+'\n'+'*'*19)
+print ('\n')
 #b
 
-chart_2_1 = 19*"*"+'\n'
-chart_2_2 = chart_2_1
-chart_2_3 = chart_2_2
-chart_2_4 = 19*"*"
-print(chart_2_1,chart_2_2,chart_2_3,chart_2_4)
-
+print(19*"*"+'\n', 19*"*"+'\n', 19*"*"+'\n', 19*"*" )
+print('\n')
 #c
-chart_3_1 = 19*"*"
-chart_3_2 = chart_3_1
-chart_3_3 = chart_3_2
-chart_3_4 = chart_3_2
-print(chart_3_1)
-print(chart_3_2)
-print(chart_3_3)
-print(chart_3_4)
+
+print(19*"*")
+print(19*"*")
+print(19*"*")
+print(19*"*")
 
