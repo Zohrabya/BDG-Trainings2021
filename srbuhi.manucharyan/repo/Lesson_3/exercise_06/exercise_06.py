74b0e898fc82552d4909c7b6108fb31b49760bfa
Srbuhi1 <srbuhi.manucharyan@gmail.com>
2021-08-14 18:25:30 +0400

    Exercise_06 Version2

--- srbuhi.manucharyan/repo/Lesson_3/exercise_06/exercise_06.py
+++ srbuhi.manucharyan/repo/Lesson_3/exercise_06/exercise_06.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 
-x= int (input ("Enter a number: "))
-print (str (x) +"---"+ str (2*x)+"---"+ str (3*x)+ "---"+ str (4*x)+ "---"+ str (5*x))
+x= int(input("Enter a number: "))
+print(str(x) + "---" + str(2 * x) + "---" + str (3*x) + "---" + str(4*x) + "---" + str(5*x))
