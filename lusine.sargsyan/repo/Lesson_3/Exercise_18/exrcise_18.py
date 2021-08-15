cb24a03c4e0a15136f9c9df192bef102201ab27e
LusineSar <lusinesargsyan85@yahoo.com>
2021-08-16 01:19:29 +0400

    Exercise_18 Version1

---
+++ lusine.sargsyan/repo/Lesson_3/Exercise_18/exrcise_18.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python3
+
+Pi = 3.14159
+radius = int(input("Enter Radius of the cicle: "))
+
+print("Diameter is: ", radius * 2)
+print("Area is: ", Pi * radius**2)
+print("Circumference is: ", 2 * Pi * radius)
