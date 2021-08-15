87f52a303aab2bdd05e6092963437f0cbcd8fdfe
LusineSar <lusinesargsyan85@yahoo.com>
2021-08-15 17:00:15 +0400

    Exercise_03 Version1

---
+++ lusine.sargsyan/repo/Lesson_3/Exercise_03/exercise_03.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+#a 
+print("*"+"\n" + 2 * "*" + "\n" + 3 * "*" + "\n" + 4 * "*")
+
+#or 
+print("*\n**\n***\n****")
+
+
+#b
+
+print("*", "**","***","****", sep="\n")
+
+#c
+
+print("*")
+print("**")
+print("***")
+print("****")
+
