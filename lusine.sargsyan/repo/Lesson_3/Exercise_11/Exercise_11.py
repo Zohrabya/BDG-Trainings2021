7d682c36011575b3e2edb75556318e0f0f3b0726
LusineSar <lusinesargsyan85@yahoo.com>
2021-08-15 21:21:45 +0400

    Exercise_11 Version1

---
+++ lusine.sargsyan/repo/Lesson_3/Exercise_11/Exercise_11.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python3 
+
+lenght = int(input("Enter the lenght of the field (feets): "))
+width = int(input("Enter the width of the field (feets): "))
+
+print("area is: ", lenght * width / 43560, "acre")
+
