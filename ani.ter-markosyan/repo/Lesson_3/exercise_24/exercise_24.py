c2143be4e2cf84abf39feb7761a93a2533450976
ani2025 <ANIHARUTYUNYAN61@GMAIL.COM>
2021-08-15 14:57:49 +0400

    exercise_24_version2

--- ani.ter-markosyan/repo/Lesson_3/exercise_24/exercise_24.py
+++ ani.ter-markosyan/repo/Lesson_3/exercise_24/exercise_24.py
@@ -1,23 +1,14 @@
 #!usr/bin/evn python3
 
 print("integer" + "\t", "square" + "\t", "cube")
-number1 = 0
-print(number1, "\t", number1 ** 2, "\t", number1 ** 3)
-number2 = 1
-print(number2, "\t", number2 ** 2, "\t", number2 ** 3)
-number3 = 2
-print(number3, "\t", number3 ** 2, "\t", number3 ** 3)
-number4 = 3
-print(number4, "\t", number4 ** 2, "\t", number4 ** 3)
-number5 = 4
-print(number5, "\t", number5 ** 2, "\t", number5 ** 3)
-number6 = 5
-print(number6, "\t", number6 ** 2, "\t", number6 ** 3)
-number7 = 6
-print(number7, "\t", number7 ** 2, "\t", number7 ** 3)
-number8 = 7
-print(number8, "\t", number8 ** 2, "\t", number8 ** 3)
-number9 = 8
-print(number9, "\t", number9 ** 2, "\t", number9 ** 3)
-number10 = 9
-print(number10, "\t", number10 ** 2, "\t", number10 ** 3)
+print(0, "\t", 0 ** 2, "\t", 0 ** 3)
+print(1, "\t", 1 ** 2, "\t", 1 ** 3)
+print(2, "\t", 2 ** 2, "\t", 2 ** 3)
+print(3, "\t", 3 ** 2, "\t", 3 ** 3)
+print(4, "\t", 4 ** 2, "\t", 4 ** 3)
+print(5, "\t", 5 ** 2, "\t", 5 ** 3)
+print(6, "\t", 6 ** 2, "\t", 6 ** 3)
+print(7, "\t", 7 ** 2, "\t", 7 ** 3)
+print(8, "\t", 8 ** 2, "\t", 8 ** 3)
+print(9, "\t", 9 ** 2, "\t", 9 ** 3)
+print(10, "\t", 10 ** 2, "\t", 10 ** 3)
