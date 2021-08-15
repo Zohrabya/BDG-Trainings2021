0cae847fe61ab8d8b0348160576929f7a6b32759
LusineSar <lusinesargsyan85@yahoo.com>
2021-08-16 00:54:58 +0400

    Exercise_17 Version1

---
+++ lusine.sargsyan/repo/Lesson_3/Exercise_17/exercise_17.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python3 
+
+number1, number2, number3 = input("enter three numbers: ") .split()
+
+print("Sum is: ", int(number1) + int(number2) + int(number3))
+
+print("Average is: ", round((int(number1) + int(number2) + int(number3)) / 3))
+
+print("Product is: ", int(number1) * int(number2) * int(number3))
+
+
+#the smallest 
+
+if number1 < number2:
+    if  number1 < number3: 
+        print("Smallest is: ", number1)
+elif number2 < number3: 
+    print("Smallest is: ", number2)
+else: 
+    print("Smallest is: ", number3)  
+
+#the largest
+
+if number1 > number2: 
+    if number1 > number3: 
+        print("Largest is: ", number1)
+elif number2 > number3:
+    print("Largest is: ", number2)
+else: 
+    print("Largest is: ", number3)
+
+
+
+
