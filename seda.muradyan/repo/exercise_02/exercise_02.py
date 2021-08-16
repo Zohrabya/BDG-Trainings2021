a8107f949ad855728949a57d266ab2bf451ab4bf
sedamur-qa <seda.hrachovna@gmail.com>
2021-08-16 12:15:03 +0400

    Exercise_02 Version 1

---
+++ seda.muradyan/repo/exercise_02/exercise_02.py
@@ -0,0 +1,15 @@
+#a
+print(19 * "*" + "\n" + "*"+ 17 * " "+ "*" +"\n"+ "*"+ 17 * " "+ "*" +"\n"+ 19 * "*" + "\n")
+
+#b 
+my_string1 = 19 * "*"
+my_string2 = "*"+ 17 * " "+ "*"
+print(my_string1, my_string2, my_string2, my_string1, sep= "\n")
+
+#c 
+my_string1 = 19 * "*"
+my_string2 = "*"+ 17 * " "+ "*"
+print(my_string1)
+print(my_string2)
+print(my_string2)
+print(my_string1)
