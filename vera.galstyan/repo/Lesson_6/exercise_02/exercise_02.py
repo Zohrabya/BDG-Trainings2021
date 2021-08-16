6bd071dc1a107cb60d884a38a2c713a665d30b53
ver-galstyan <vera.galstyan@inbox.ru>
2021-08-16 14:21:06 +0400

    Exercise_02_Lesson_6_Version1

---
+++ vera.galstyan/repo/Lesson_6/exercise_02/exercise_02.py
@@ -0,0 +1,17 @@
+#!usr/bin/evn python3
+
+#1
+set_box = 5
+for set_box in range(1):
+
+    print('*'*15)
+
+#2
+print("*", "*", sep= ((" ")*13))
+print("*", "*", sep= ((" ")*13))
+
+#3
+set_box = 5
+for set_box in range(1):
+
+    print('*'*15)
