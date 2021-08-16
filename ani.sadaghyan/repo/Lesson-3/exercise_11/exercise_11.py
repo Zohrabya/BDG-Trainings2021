424cd4b33991e864607f36a7286c2ff90691ea2b
Ani Sadaghyan <anisadaghyan@gmail.com>
2021-08-16 19:24:20 +0400

    exercise_11 version3

--- ani.sadaghyan/repo/Lesson-3/exercise_11/exercise_11.py
+++ ani.sadaghyan/repo/Lesson-3/exercise_11/exercise_11.py
@@ -1,5 +1,5 @@
-length = int(input("Enter the length (in feet):"))
-width = int(input("Enter the width (in feet):"))
-ACRES = 43560
-AREA = (length + width) / ACRES
-print("The area of the field is", "AREA", "ACRES:" )
+length = int(input("Enter the length (in feet): "))
+width = int(input("Enter the width (in feet): "))
+ACRE = 43560
+AREA = (length + width) / ACRE
+print("The area of the field is", "AREA", "acres: " )
---
+++ ani.sadaghyan/repo/Lesson_3/exercise_12/exercise_12.py
@@ -0,0 +1,3 @@
+liter_or_less = int(input("Enter the number of containers"))
+more_then_liter = int(input("Enter the number of  containers"))
+ 
