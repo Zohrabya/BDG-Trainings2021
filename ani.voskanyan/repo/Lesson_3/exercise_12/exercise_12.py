44e269bc127b384e678c6425479e82459aff585f
annievoskanyan <you@example.com>
2021-08-17 18:42:10 +0400

    Exercise_12 Version7

--- ani.voskanyan/repo/Lesson_3/exercise_12/exercise_12.py
+++ ani.voskanyan/repo/Lesson_3/exercise_12/exercise_12.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
+
 container_quantity_less = int(input("Please, enter quantity of drink containers holding one liter and less : "))
 container_quantity_more = int(input("Please, enter quantity of drink containers holding more than one liter:  "))
-total_refund = 0.10 * container_quantity_less + 0.25 * container_quantity_more
+DEPOSIT_1 = 0.10
+DEPOSIT_2 = 0.25
+total_refund = DEPOSIT_1 * container_quantity_less + DEPOSIT_2 * container_quantity_more
 print("Your total refund:", total_refund)
