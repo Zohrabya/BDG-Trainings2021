dc373ee252f80185f53cfe7f32ff723642a7a56c
annievoskanyan <you@example.com>
2021-08-17 18:38:31 +0400

    Exercise_11 Version3

--- ani.voskanyan/repo/Lesson_3/exercise_11/exercise_11.py
+++ ani.voskanyan/repo/Lesson_3/exercise_11/exercise_11.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+
 field_length = float(input("Please enter the length of the field: "))
 field_width = float(input("Please enter the width of the field: "))
 FEET_IN_ACRE = 43560
 field_square_in_feet = field_length * field_width
-field_square_in_acre = FEET_IN_ACRE * field_square_in_feet
+field_square_in_acre = field_square_in_feet / FEET_IN_ACRE
 
 print(f'The square of the field is {field_square_in_acre} acres')
