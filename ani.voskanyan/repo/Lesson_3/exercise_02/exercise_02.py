032ba3918d3e8e0db9c802aaf8d31f4dfd42596a
annievoskanyan <you@example.com>
2021-08-17 18:23:54 +0400

    Exercise_2 Version3

--- ani.voskanyan/repo/Lesson_3/exercise_02/exercise_02.py
+++ ani.voskanyan/repo/Lesson_3/exercise_02/exercise_02.py
@@ -1,19 +1,13 @@
+#!/usr/bin/env python3
+
 #a.Using one statement with one print function
 print(("*" * 19) + "\n" + "*" + (" " * 17) + "*\n" + "*" + (" " * 17) + "*\n" + ("*" * 19))
 
 #b.Using four statement with one print function
-line_b_1 = "*" * 19 +"\n"
-line_b_2 = "*" + " " * 17 + "*\n"
-line_b_3 = line_b_2
-line_b_4 = line_b_1
-print(line_b_1 + line_b_2 + line_b_3 + line_b_4)
+print(("*" * 19 +"\n") + ("*" + " " * 17 + "*\n") * 2 +  ("*" * 19 +"\n"))
 
 #c.Using four statement with four print function
-line_c_1 = "*" * 19
-line_c_2 = "*" + " " * 17 + "*"
-line_c_3 = line_c_2
-line_c_4 = line_c_1
-print(line_c_1)
-print(line_c_2)
-print(line_c_3)
-print(line_c_4)
+print("*" * 19)
+print("*" + " " * 17 + "*")
+print("*" + " " * 17 + "*")
+print("*" * 19)
