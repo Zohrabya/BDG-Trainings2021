fb9d1ee2570589f7b65dd9d96c987b6ee81027d5
annievoskanyan <you@example.com>
2021-08-17 18:52:30 +0400

    Exercise_22 Version2

--- ani.voskanyan/repo/Lesson_3/exercise_22/exercise_22.py
+++ ani.voskanyan/repo/Lesson_3/exercise_22/exercise_22.py
@@ -1,3 +1,5 @@
+#!/usr/bin/evn python3
+
 integer_1 = int(input("Please enter the first integer:"))
 integer_2 = int(input("Please enter the second integer: "))
 
@@ -5,4 +7,3 @@ if integer_1 % integer_2 == 0:
     print(f' {integer_1} is a multiple of {integer_2}')
 else:
     print(f" {integer_1} isn't a multiple of {integer_2}")
-    
