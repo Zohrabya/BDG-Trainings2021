03e4bd576bacd9cc675a72e36807cc254239e5fd
ani2025 <ANIHARUTYUNYAN61@GMAIL.COM>
2021-08-17 15:49:52 +0400

    exercise_17_version4

--- ani.ter-markosyan/repo/Lesson_3/exercise_17/exercise_17.py
+++ ani.ter-markosyan/repo/Lesson_3/exercise_17/exercise_17.py
@@ -1,12 +1,9 @@
 #!usr/bin/evn python3
 
-number1 = int(input("Input the first number: "))
-number2 = int(input("Input second number: "))
-number3 = int(input("input third number: "))
-print("Input three different integers:", number1, number2, number3)
-sum = number1 + number2 + number3
-average = round(number1 + number2 + number3 / 3, 2)
-product = number1 * number2 * number3
+number1, number2, number3 = (input("Input three different integers: ").split())
+sum = int(number1) + int (number2) + int(number3)
+average = round(int (number1) + int(number2) + int(number3) / 3, 2)
+product = int(number1) * int(number2) * int(number3)
 if number1 > number2 and number1 > number3:
     largest = number1
 elif number2 > number1 and number2 > number3:
