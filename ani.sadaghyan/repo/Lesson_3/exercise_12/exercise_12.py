8cd5df13084f5572c2fc56b1975146d0d188ea55
Ani Sadaghyan <anisadaghyan@gmail.com>
2021-08-17 18:49:18 +0400

    exercise_12 version2

--- ani.sadaghyan/repo/Lesson_3/exercise_12/exercise_12.py
+++ ani.sadaghyan/repo/Lesson_3/exercise_12/exercise_12.py
@@ -1,4 +1,4 @@
 #!usr/bin/evn python3
-liter_or_less = int(input("Enter the number of containers"))
-more_then_liter = int(input("Enter the number of  containers"))
+liter_or_less = int(input("Enter the number of containers: "))
+more_then_liter = int(input("Enter the number of  containers: "))
 print("the number for containers is", round(liter_or_less * 0.10, 2) + round(more_then_liter * 0.25, 2), "$")
