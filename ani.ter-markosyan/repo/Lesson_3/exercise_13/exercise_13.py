136b613612c3e2a8681d6d43a72d83a2922b2622
ani2025 <ANIHARUTYUNYAN61@GMAIL.COM>
2021-08-17 16:02:53 +0400

    exercise_13_version3

--- ani.ter-markosyan/repo/Lesson_3/exercise_13/exercise_13.py
+++ ani.ter-markosyan/repo/Lesson_3/exercise_13/exercise_13.py
@@ -3,7 +3,7 @@
 cost = int(input("Enter the cost of your meal: "))
 TAX_RATE = 5.2
 TIP_RATE = 18
-tax = (round(TAX_RATE * cost  / 100, 2))
-tip = (round( TIP_RATE * cost / 100, 2))
-total = (round(tax + tip + cost, 2))
-print("Tax amount is: ", tax, "\nTip amount is: ", tip, "\nGrand total is: ", total)
+tax = TAX_RATE * cost  / 100
+tip = TIP_RATE * cost / 100
+total = tax + tip + cost
+print("Tax amount is: ", round(tax, 2), "\nTip amount is: ", round(tip, 2), "\nGrand total is: ", round(total, 2))
