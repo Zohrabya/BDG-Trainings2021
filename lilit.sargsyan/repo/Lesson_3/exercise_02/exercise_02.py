a43b79bbbf2e3894981239fb372a9f06dd7a5023
lilit.sargsyan <sargsyan695@gmail.com>
2021-08-18 10:46:08 +0400

    Exercise_02 Version3

--- lilit.sargsyan/repo/Lesson_3/exercise_02/exercise_02.py
+++ lilit.sargsyan/repo/Lesson_3/exercise_02/exercise_02.py
@@ -1,17 +1,12 @@
-
- print(('*'*12) + '\n' + "*" + (" "*10) + '*\n' + '*' + (' '*10) + '*\n' + ('*'*12))
+print(('*'*19) + '\n' + "*" + (" "*17) + '*\n' + '*' + (' '*17) + '*\n' + ('*'*19))
  
- lesson_03_line_1= '*'*12 + '\n'
- lesson_03_line_2= '*' + ' '*10 + '*\n'
- lesson_03_line_3= '*' + ' '*10 + '*\n'
- lesson_03_line_4= '*'*12
- print(lesson_03_line_1,lesson_03_line_2,lesson_03_line_3,lesson_03_line_4)
+print(('*'*19) + '\n' + "*" + (" "*17) + '*\n' + '*' + (' '*17) + '*\n' + ('*'*19))
  
- lesson_03_line_1= '*'*12
- lesson_03_line_2= '*' + ' '*10 + '*'
- lesson_03_line_3= '*' + ' '*10 + '*'
- lesson_03_line_4= '*'*12
- print(lesson_03_line_1)
- print(lesson_03_line_2)
- print(lesson_03_line_3)
- print(lesson_03_line_4)
+lesson_03_line_1= '*' *19
+lesson_03_line_2= '*' + ' '*17 + '*'
+lesson_03_line_3= '*' + ' '*17 + '*'
+lesson_03_line_4= '*' *19
+print(lesson_03_line_1)
+print(lesson_03_line_2)
+print(lesson_03_line_3)
+print(lesson_03_line_4)
