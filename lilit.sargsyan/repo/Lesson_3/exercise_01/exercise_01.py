4fc5510f411389355bea424811a49507ed5aeeca
lilit.sargsyan <sargsyan695@gmail.com>
2021-08-18 09:30:01 +0400

    exercise_01 Version3

--- lilit.sargsyan/repo/Lesson_3/exercise_01/exercise_01.py
+++ lilit.sargsyan/repo/Lesson_3/exercise_01/exercise_01.py
@@ -1,15 +1,12 @@
-print('*' *12 + '\n' + '*'*12 + '\n'+ '*'*12 + '\n' + '*'*12)
+print('*' *19 + '\n' + '*' *19 + '\n'+ '*' *19 + '\n' + '*' *19)
 
-line_1_lesson_3= "*"*12 +'\n'
-line_2_lesson_3= "*"*12 + "\n"
-line_3_lesson_3= line_1_lesson_3
-line_4_lesson_3= line_1_lesson_3
-print(line_1_lesson_3,line_2_lesson_3,line_3_lesson_3,line_4_lesson_3)
 
-line_1_lesson_3= '*'*12
-line_2_lesson_3= '*'*12
-line_3_lesson_3= '*'*12
-line_4_lesson_3= '*'*12
+print('*' *19 + '\n' + '*' *19 + '\n'+ '*' *19 + '\n' + '*' *19)
+
+line_1_lesson_3= '*' *19
+line_2_lesson_3= '*' *19
+line_3_lesson_3= '*' *19
+line_4_lesson_3= '*' *19
 print(line_1_lesson_3)
 print(line_2_lesson_3)
 print(line_3_lesson_3)
