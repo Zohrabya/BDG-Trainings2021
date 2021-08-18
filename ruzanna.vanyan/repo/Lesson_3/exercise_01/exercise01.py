d97fa34e91e9ef2ac7fb4da5f5a9dfd805b53d81
Ruzanna Vanyan <ruzikvanyan@gmail.com>
2021-08-18 18:26:03 +0400

    Exercise_01 Version1

---
+++ ruzanna.vanyan/repo/Lesson_3/exercise_01/exercise01.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+print("*" *10 + "\n" + "*" *10 + "\n" + "*" *10 + "\n" + "*" *10 + "\n"  )
+
+
+shape_1 = "\n" + "*"*10
+shape_2 = shape_1
+shape_3 = shape_1
+shape_4 = shape_1
+print(shape_1,shape_2,shape_3,shape_4)
+
+
+
+
+line_1 = "*"*10
+line_2 = line_1
+line_3 = line_1
+line_4 = line_1
+
+print(line_1)
+print(line_2)
+print(line_3)
+print(line_4)
+
+
+
+
+
+
+
+
+
+
+
+
