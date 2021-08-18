ecd34d3b0d0585784cfd61063def14412f1aba54
ani2025 <ANIHARUTYUNYAN61@GMAIL.COM>
2021-08-18 17:49:48 +0400

    exercise_01_version2

--- ani.ter-markosyan/repo/Lesson_6/exercise_01/exercise_01.py
+++ ani.ter-markosyan/repo/Lesson_6/exercise_01/exercise_01.py
@@ -3,6 +3,4 @@
 rows = int(input("Enter the number of rows of the box: "))
 columns = int(input("Enter the number of columns of the box: "))
 for i in range(rows):
-    for j in range(columns):
-        print("*", end= "")
-    print()
+        print("*" * columns)
