186fcf4524373542990b08695f6229c1d20124e6
lilit.sargsyan <sargsyan695@gmail.com>
2021-08-18 15:45:00 +0400

    Exercise_09 Version 2

--- lilit.sargsyan/repo/Lesson_3/exercise_09/exercise_09.py
+++ lilit.sargsyan/repo/Lesson_3/exercise_09/exercise_09.py
@@ -1,5 +1,5 @@
-price = int(input('Enter the price of the meal:' ))
-percent = int(input('Enter the percent of the tip:' ))
+price = int(input('Enter the price of the meal: ' ))
+percent = int(input('Enter the percent of the tip: ' ))
 tip = price * percent / 100
 bill_of_the_meal = price + tip
 print('Tip amount is:', tip, '\nBill(the tip is included) is:', bill_of_the_meal, sep= ' ')
