dfea8ea21f548f457495b7dc1a0166a208b97319
ani2025 <ANIHARUTYUNYAN61@GMAIL.COM>
2021-08-19 00:36:24 +0400

    exercise_05_version1

---
+++ ani.ter-markosyan/repo/Lesson_6/exercise_05/exercise_05.py
@@ -0,0 +1,13 @@
+#!usr/bin/evn python3
+
+height = int(input("Enter the height of a diamond: "))
+for i in range(0, height):
+    if i == 0:
+        print (" " * (height - 1) + "*")
+    else:
+         print (" " * (height - 1 - i) + "*" * (i+1) + "*" * i)
+for i in range (0, height-1):
+    if i == (height -1):
+        print(" " * (i + 1) + "*" * (height - i - 1))
+    else:
+        print(" " * (i + 1) + "*" * (height - i - 1) + "*" * (height - i - 2))
