2707a9e23bc1f3f11b72d8cbc6fb3078e4c6322f
lilit.sargsyan <sargsyan695@gmail.com>
2021-08-18 15:42:58 +0400

    Exercise_08 Version2

--- lilit.sargsyan/repo/Lesson_3/exercise_08/exercise_08.py
+++ lilit.sargsyan/repo/Lesson_3/exercise_08/exercise_08.py
@@ -1,6 +1,7 @@
-first_num = int(input('Enter first number:' ))
-second_num = int(input('Enter second number:' ))
-third_num = int(input('Enter third number:'))
+first_num = int(input('Enter first number: ' ))
+second_num = int(input('Enter second number: ' ))
+third_num = int(input('Enter third number: '))
 total = first_num + second_num + third_num
 average = total / 3
-print('Total:', total, '\nAverage:', (average))
+average_format = '{:.2f}' .format(average)
+print('Total:', total, '\nAverage:', (average_format))
