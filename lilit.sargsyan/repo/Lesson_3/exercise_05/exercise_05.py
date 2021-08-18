9e23257debe7ba84c3a3d13ff848a7c9dbe58206
lilit.sargsyan <sargsyan695@gmail.com>
2021-08-18 11:31:36 +0400

    exercise_05 version2

--- lilit.sargsyan/repo/Lesson_3/exercise_05/exercise_05.py
+++ lilit.sargsyan/repo/Lesson_3/exercise_05/exercise_05.py
@@ -1,2 +1,2 @@
-num = eval(input('Enter a number:5 '))
-print('The square of 5 is ' , num*num , '.', sep='')
+num_ex_5 = int(input('Enter a number: '))
+print('The square of ', (num_ex_5), ' is ', num_ex_5 * num_ex_5 , '.', sep='')
