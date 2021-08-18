1869a95573ce2bc757f44ccbce00310a8f8c51b9
lilit.sargsyan <sargsyan695@gmail.com>
2021-08-18 20:37:15 +0400

    Exercise_16 version2

--- lilit.sargsyan/repo/Lesson_3/Exercise_16/exercise_16.py
+++ lilit.sargsyan/repo/Lesson_3/Exercise_16/exercise_16.py
@@ -1,5 +1,5 @@
-first_num = int(input("Enter the first integer number:"))
-second_num = int(input("Enter the second integer number:"))
+first_num = int(input("Enter the first integer number: "))
+second_num = int(input("Enter the second integer number: "))
 if first_num < second_num:
     print(second_num, 'is larger')
 if first_num > second_num:
