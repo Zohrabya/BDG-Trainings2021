de9cc69d92dce95a40bc73ac9dc6c202ea56cbec
Ruzanna Vanyan <ruzikvanyan@gmail.com>
2021-08-19 20:47:54 +0400

    Exercise_02 Version1

--- ruzanna.vanyan/repo/Lesson_3/exercise_02.py
+++ ruzanna.vanyan/repo/Lesson_3/exercise_02.py
@@ -5,8 +5,9 @@ print(19*"*" + "\n" + "*" + 17*" " + "*" + "\n" + "*" + 17*" " + "*" + "\n" + 19
 
 shape_1 = "\n" + 19*"*"
 shape_2 = "\n" + "*" + 17*" " + "*"
-shape_3 = "\n" + shape_2
-shape_4 = "\n" + shape_1
+shape_3 =  shape_2
+shape_4 =  shape_1
+print(shape_1,shape_2,shape_3,shape_4)
 
 line_1 = 19*"*"
 line_2 = "*" + 17*" " + "*"
