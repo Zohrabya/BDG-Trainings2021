113ca5271e8cd578ed652d45037cc55026d27f06
annievoskanyan <you@example.com>
2021-08-19 20:08:33 +0400

    Exercise_27 Version1

---
+++ ani.voskanyan/repo/Lesson_3/exercise_27/exercise_27.py
@@ -0,0 +1,52 @@
+#!usr/bin/evn python3
+
+number = 1
+print(number, oct(number), hex(number).upper(), bin(number))
+
+number = 2
+print(number, oct(number), hex(number).upper(), bin(number))
+
+number = 3
+print(number, oct(number), hex(number).upper(), bin(number))
+
+number = 4
+print(number, oct(number), hex(number).upper(), bin(number))
+
+number = 5
+print(number, oct(number), hex(number).upper(), bin(number))
+
+number = 6
+print(number, oct(number), hex(number).upper(), bin(number))
+
+number = 7
+print(number, oct(number), hex(number).upper(), bin(number))
+
+number = 8
+print(number, oct(number), hex(number).upper(), bin(number))
+
+number = 9
+print(number, oct(number), hex(number).upper(), bin(number))
+
+number = 10
+print(number, oct(number), hex(number).upper(), bin(number))
+
+number = 11
+print(number, oct(number), hex(number).upper(), bin(number))
+
+number = 12
+print(number, oct(number), hex(number).upper(), bin(number))
+
+number = 13
+print(number, oct(number), hex(number).upper(), bin(number))
+
+number = 14
+print(number, oct(number), hex(number).upper(), bin(number))
+
+number = 15
+print(number, oct(number), hex(number).upper(), bin(number))
+
+number = 16
+print(number, oct(number), hex(number).upper(), bin(number))
+
+number = 17
+print(number, oct(number), hex(number).upper(), bin(number))
