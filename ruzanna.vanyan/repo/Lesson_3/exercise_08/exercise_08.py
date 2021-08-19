44d4f5bf19ad3cd55b822bec806e3aec05a4842f
Ruzanna Vanyan <ruzikvanyan@gmail.com>
2021-08-19 23:47:39 +0400

    Exercise_08 Version1

---
+++ ruzanna.vanyan/repo/Lesson_3/exercise_08/exercise_08.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+
+num1 = eval(input('Enter the first number: '))
+num2 = eval(input('Enter the second number: '))
+num3 = eval(input('Enter the third number: '))
+print('The average of the numbers you entered is',(num1+num2+num3)/2)
+print('The total of the numbers you entered is' , num1+num2+num3)
