0d0d4ca40a74ee4a710b30caa311374be49bf547
lilit.sargsyan <sargsyan695@gmail.com>
2021-08-19 14:44:44 +0400

    Exercise_22 version2

--- lilit.sargsyan/repo/Lesson_3/exercise_22/exercise_22.py
+++ lilit.sargsyan/repo/Lesson_3/exercise_22/exercise_22.py
@@ -1,4 +1,4 @@
-num_first = int(input('Enter the first number'))
-num_second = int(input('Enter the second number'))
+num_first = int(input('Enter the first number: '))
+num_second = int(input('Enter the second number: '))
 if (num_first % num_second) == 0:
     print('First is a multiple of second.')
