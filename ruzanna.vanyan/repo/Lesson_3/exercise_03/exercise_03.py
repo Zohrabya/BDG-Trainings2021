fab8d1d4009c48fff403e6790ce7bfba5084f5c8
Ruzanna Vanyan <ruzikvanyan@gmail.com>
2021-08-19 21:22:10 +0400

    Exercise_03 Version1

---
+++ ruzanna.vanyan/repo/Lesson_3/exercise_03/exercise_03.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+print("*" + "\n" + "*"*2 + "\n" + "*"*3 + "\n" + "*"*4 + "\n")
+
+shape_1 = "\n" + "*"
+shape_2 = "\n" + "*"*2
+shape_3 = "\n" + "*"*3
+shape_4 = "\n" + "*"*4
+print(shape_1,shape_2,shape_3,shape_4)
+
+line_1 =  "*"
+line_2 = "*"*2
+line_3 = "*"*3
+line_4 = "*"*4
+print(line_1)
+print(line_2)
+print(line_3)
+print(line_4)
