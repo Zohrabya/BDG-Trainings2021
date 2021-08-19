0fa0e39810dd40bf678252c62531e99a6cb73771
annievoskanyan <you@example.com>
2021-08-19 20:26:38 +0400

    Exercise_1 Version_1 Lesson_6

---
+++ ani.voskanyan/repo/Lesson_6/exercise_01/exercise_01.py
@@ -0,0 +1,8 @@
+#!usr/bin/env python3
+
+height = int(input("Height of the box: "))
+width = int(input("Width of the box: "))
+
+for asterisk in range(0, height):
+        print("*" * width)
+        
