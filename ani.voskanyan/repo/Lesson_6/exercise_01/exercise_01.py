2037570efec8de67f5f83f805b953d68da7a318a
annievoskanyan <you@example.com>
2021-08-19 20:28:13 +0400

    Exercise_1 Version_2 Lesson_6

--- ani.voskanyan/repo/Lesson_6/exercise_01/exercise_01.py
+++ ani.voskanyan/repo/Lesson_6/exercise_01/exercise_01.py
@@ -4,5 +4,4 @@ height = int(input("Height of the box: "))
 width = int(input("Width of the box: "))
 
 for asterisk in range(0, height):
-        print("*" * width)
-        
+    print("*" * width)
