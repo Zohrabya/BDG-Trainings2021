c2d90d95655f2c3aa927db175617f48732927732
LusineSar <lusinesargsyan85@yahoo.com>
2021-08-20 20:26:27 +0400

    Exercise_02 Version1

---
+++ lusine.sargsyan/repo/Lesson_3/exercise_02/exercise_02.py
@@ -0,0 +1,25 @@
+#! usr/bin/env python3
+
+
+#a 
+
+print(19 * "*", "\n*"+ 17 * " "+ "*", "\n*"+ 17 * " "+ "*", "\n" + 19 * "*")
+
+
+#b 
+
+my_string1 = 19 * "*"
+my_string2 = "*"+ 17 * " "+ "*"
+
+print(my_string1, my_string2, my_string2, my_string1, sep= "\n")
+
+
+
+#c 
+
+my_string1 = 19 * "*"
+my_string2 = "*"+ 17 * " "+ "*"
+print(my_string1)
+print(my_string2)
+print(my_string2)
+print(my_string1)
