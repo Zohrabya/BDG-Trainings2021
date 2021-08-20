20801c35083dda645bba7a9336c42a92ec74ddce
Ruzanna Vanyan <ruzikvanyan@gmail.com>
2021-08-20 18:13:57 +0400

    Exercise_11 Version1

---
+++ ruzanna.vanyan/repo/Lesson_3/exercise_11/exercise_11.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+width = float(input('Enter your fields width in feet: ' ))
+length = float(input('Enter your fields length in feet: '))
+print('The area of the field in acres is',round(width * length / 43560,4))
+
+
+
