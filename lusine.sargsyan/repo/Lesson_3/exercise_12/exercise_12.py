59bffd6e274821922e80f21a81caef46ed86386b
LusineSar <lusinesargsyan85@yahoo.com>
2021-08-21 00:52:30 +0400

    Exercise_12 Version2

--- lusine.sargsyan/repo/Lesson_3/exercise_12/exercise_12.py
+++ lusine.sargsyan/repo/Lesson_3/exercise_12/exercise_12.py
@@ -1,25 +1,9 @@
 #! /usr/bin/env python3
 
-#version1
-index1 = 0.10
-index2 = 0.25
-count = int(input("please enter the count of conteiners: "))
-liter = int(input("please enter the litrage of conteiners: "))
-if (liter <= 1):
-    result = count * index1
-else:
-    result = count * index2
-print ("deposite is: $ {0:1.2f}".format(result))
 
-#version2
-index1 = 0.10
-index2 = 0.25
-count1 = int(input("please enter the count of conteiners having more than 1l litrage: "))
-count2 = int(input("please enter the count of conteiners having less than 1l litrage: "))
-result = count1 * index2 + count2 * index1
+deposite_1 = 0.10
+deposite_2 = 0.25
+count1 = int(input("please enter the count of conteiners with litrage 1l or less: "))
+count2 = int(input("please enter the count of conteiners with litrage more than 1l: "))
 
-print ("deposite is: $ {0:1.2f}".format(result))
-
-
-
-
+print ("Deposite is: $ {0:1.2f}".format(count1 * deposite_1 + count2 * deposite_2))
