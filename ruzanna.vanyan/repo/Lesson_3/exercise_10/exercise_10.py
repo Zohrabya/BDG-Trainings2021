2661a745d80c05b8dbd7fad9dd5285909087261a
Ruzanna Vanyan <ruzikvanyan@gmail.com>
2021-08-20 17:42:09 +0400

    Exercise_10 Version1

---
+++ ruzanna.vanyan/repo/Lesson_3/exercise_10/exercise_10.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+width = float((input('Enter the width of the room: ')))
+length = float((input('Enter the length of the room: ')))
+print('The area of the room is: ', round(width*length,2), 'square meter',)
+
+
+
