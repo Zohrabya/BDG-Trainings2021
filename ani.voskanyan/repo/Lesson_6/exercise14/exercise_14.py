db30132f83b615c5956c81fe66898ef78a32a224
annievoskanyan <you@example.com>
2021-08-21 00:39:17 +0400

    Exercise_14 Version_2 Lesson_6

--- ani.voskanyan/repo/Lesson_6/exercise14/exercise_14.py
+++ ani.voskanyan/repo/Lesson_6/exercise14/exercise_14.py
@@ -8,12 +8,15 @@ n = 1
 while n <= 100:
     n_list.append(n)
     n += 1
-print(n_list)
 
 for n in n_list:
-    divisor = n 
+    divisor = n - 1
     while divisor > 0:
         if n % divisor == 0:
             divisor_list.append(divisor)
         divisor -= 1
-print(f'{n}  {divisor_list}')
+    if sum(divisor_list) == n:
+        perfect_numbers.append(n)
+    divisor_list.clear()
+
+print(perfect_numbers)
