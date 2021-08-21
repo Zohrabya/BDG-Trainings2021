dd0fb2371bfea61837885d9dc180b3b37be0f625
ver-galstyan <vera.galstyan@inbox.ru>
2021-08-21 11:49:08 +0400

    Exercise_Lesson_3 Version1

---
+++ vera.galstyan/repo/Lesson_3/exercise_18/exercise_18.py
@@ -0,0 +1,17 @@
+#!usr\bin env python 3
+#!/usr/bin/env python3
+ #1
+radius = int(input("Enter radius"))
+
+#2
+diameter = 2*radius
+print("Diameter is: ", diameter)
+
+#3
+p = 3.14159
+circumference = 2*p*radius
+print("Circumference is: ", circumference)
+
+#4
+area = p*radius*radius
+print("Area is: ", area)
