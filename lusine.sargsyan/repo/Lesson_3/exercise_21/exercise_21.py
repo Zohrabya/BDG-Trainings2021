54b4e05fbded8e5fff94a1c794990ea4b8769e87
LusineSar <lusinesargsyan85@yahoo.com>
2021-08-21 12:35:53 +0400

    Exercise_21 Version1

---
+++ lusine.sargsyan/repo/Lesson_3/exercise_21/exercise_21.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env Python3
+
+your_integer = int(input("please enter the number: "))
+
+if your_integer % 2 == 0: 
+    print(your_integer, "is even.")
+else: 
+    print(your_integer, "is odd.")
+        
