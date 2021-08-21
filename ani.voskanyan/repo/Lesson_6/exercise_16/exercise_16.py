dda2e0932ca309c75e0fb5b6bc2906a8efece308
annievoskanyan <you@example.com>
2021-08-21 18:01:15 +0400

    Exercise_16 Version_1 Lesson_6

---
+++ ani.voskanyan/repo/Lesson_6/exercise_16/exercise_16.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python3
+
+text_input = input("Add a text to transform into Pig Latin: ").lower().split(" ")
+consonants = ["b", "c", "d", "f","g", "h", "j", "k", "l", "m", "n", "p","q", "r", "s","t", "v", "x", "w", "y", "z"]
+vowels = ["a", "e", "i", "o", "u"]
+
+print(text_input)
+
+
+def transform_vowel(word):
+  word.append('way')
+  return word
+
+
+def transform_consonant(word):
+  done = True
+  
+  while done:
+    if word[0] in consonants:
+        word.append(word[0])
+        del word[0]
+    else: 
+        done = False
+  
+  word.append('ay')
+  return word
+
+
+for word in text_input:
+    word = list(word)
+
+    if word[0] in consonants:
+        result = transform_consonant(word)
+    else:
+        result = transform_vowel(word)
+
+print(''.join(result))
