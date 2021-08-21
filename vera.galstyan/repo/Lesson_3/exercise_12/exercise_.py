f85fc33a536b7c99fd40e099d76393d017deefd9
ver-galstyan <vera.galstyan@inbox.ru>
2021-08-21 10:41:03 +0400

    Vera.Galstyan_EXE_12 Version1

---
+++ vera.galstyan/repo/Lesson_3/exercise_12/exercise_.py
@@ -0,0 +1,11 @@
+#!usr\bin\env python 3
+
+
+def main():
+    set_1  = input("Enter the namber  of containers ")
+    infile = open(set_1, )
+    numList = input('Enter any numbers(seperated by a space): ')
+    print(numList, file=infile)
+
+main()
+
