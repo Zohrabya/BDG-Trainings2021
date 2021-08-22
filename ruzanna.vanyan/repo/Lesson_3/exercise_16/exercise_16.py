0a615aea01f7e7687bea45d89d6f651972cef012
Ruzanna Vanyan <ruzikvanyan@gmail.com>
2021-08-22 22:32:21 +0400

    Exercise_16 Version1

---
+++ ruzanna.vanyan/repo/Lesson_3/exercise_16/exercise_16.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+if(num1>num2):
+    print(num1, "Is larger")
+elif(num1<num2):
+    print(num2, "Is larger")
+else:
+ print("These numbers are equal")
