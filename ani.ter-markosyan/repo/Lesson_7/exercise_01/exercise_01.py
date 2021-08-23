3bbf46ad98af051ee41f3bedf133ef18d6265ccb
ani2025 <ANIHARUTYUNYAN61@GMAIL.COM>
2021-08-24 00:11:11 +0400

    exercise_01_lesson7_version2

--- ani.ter-markosyan/repo/Lesson_7/exercise_01/exercise_01.py
+++ ani.ter-markosyan/repo/Lesson_7/exercise_01/exercise_01.py
@@ -2,8 +2,10 @@
 
 short_side1 = int(input("Enter the lenght of triangle's 1 side: "))
 short_side2 = int(input("Enter the lenght of trangle's 2 sede: "))
-hypotenuse = (short_side1 ** 2 + short_side2 ** 2) ** 0.5
-if short_side1 == 0 or short_side2 == 0:
-   raise ValueError("The lenght of triangle's side can not be zero")
-print ("Hypotenuse is", hypotenuse)
+def triangle_hyp(short_side1, short_side2):
+    if short_side1 == 0 or short_side2 == 0:
+        raise ValueError("The lenght of triangle's side can not be zero.")
+    else:
+         return (short_side1 ** 2 + short_side2 ** 2) ** 0.5
+print(f"Triangle's hypotenus is {triangle_hyp(short_side1, short_side2)}.")
 
