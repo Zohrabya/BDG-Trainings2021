7d4bcaf9e7eeacf15187b60365413747f520d61e
annievoskanyan <you@example.com>
2021-08-25 16:51:04 +0400

    Exercise_20 Version_3

--- ani.voskanyan/repo/Lesson_3/exercise_20/exercise_20.py
+++ ani.voskanyan/repo/Lesson_3/exercise_20/exercise_20.py
@@ -6,26 +6,32 @@ integer_3 = int(input("Enter the third integer: "))
 integer_4 = int (input("Enter the fourth integer: "))
 integer_5 = int(input("Enter the fifth integer: "))
 
-if integer_1 < integer_2 and integer_1 < integer_3 and integer_1 < integer_4 and integer_1 < integer_5:
-    smallest_integer = integer_1
-elif integer_2 < integer_1 and integer_2 < integer_3 and integer_2 < integer_4 and integer_2 < integer_5:
-     smallest_integer = integer_2
-elif integer_3 < integer_1 and integer_3 < integer_2 and integer_3 < integer_4 and integer_3 < integer_5:
-     smallest_integer = integer_3
-elif integer_4 < integer_1 and integer_4 < integer_3 and integer_4 < integer_2 and integer_4 < integer_5:
-     smallest_integer = integer_4
+if integer_1 < integer_2:
+     larger_1 = integer_2
+     smaller_1 = integer_1
 else:
-    smallest_integer = integer_5 
+     larger_1 = integer_1
+     smaller_1 = integer_2
 
-if integer_1 > integer_2 and integer_1 > integer_3 and integer_1 > integer_4 and integer_1 > integer_5:
-    largest_integer = integer_1
-elif integer_2 > integer_1 and integer_2 > integer_3 and integer_2 > integer_4 and integer_2 > integer_5:
-     largest_integer = integer_2
-elif integer_3 > integer_1 and integer_3 > integer_2 and integer_3 > integer_4 and integer_3 > integer_5:
-     largest_integer = integer_3
-elif integer_4 > integer_1 and integer_4 > integer_3 and integer_4 > integer_2 and integer_4 > integer_5:
-     largest_integer = integer_4
+if integer_3 < integer_4:
+     larger_2 = integer_4
+     smaller_2 = integer_3
 else:
-    largest_integer = integer_5 
+     larger_2 = integer_3
+     smaller_2 = integer_4
+
+if larger_1 > larger_2:
+     larger_3 = larger_1
+else:
+     larger_3 = larger_2
+
+if smaller_1 > smaller_2:
+     smaller_3 = smaller_1
+else:
+     smaller_3 = smaller_2
+
+largest_integer = larger_3 if larger_3 > integer_5 else integer_5
+smallest_integer = smaller_3 if smaller_3 < integer_5 else integer_5
+
 
 print(f'Smallest integer is {smallest_integer}. Largest integer is {largest_integer}')
