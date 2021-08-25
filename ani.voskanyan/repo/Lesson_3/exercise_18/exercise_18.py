4e37cb5701afb962d9553bf8b29e01366f3b3ab3
annievoskanyan <you@example.com>
2021-08-25 07:35:14 +0400

    Exercise_18 Version_3

--- ani.voskanyan/repo/Lesson_3/exercise_18/exercise_18.py
+++ ani.voskanyan/repo/Lesson_3/exercise_18/exercise_18.py
@@ -3,6 +3,4 @@
 radius = float(input("Enter the radius of circle: "))
 PI = 3.14159
 
-print(f""" Diameter is {radius *2}
-Circumference is {radius * 2 * PI}
-Area is {radius ** 2 * PI}""")
+print(f" Diameter: {radius *2}. Circumference: {radius * 2 * PI}. Area: {radius ** 2 * PI}")
