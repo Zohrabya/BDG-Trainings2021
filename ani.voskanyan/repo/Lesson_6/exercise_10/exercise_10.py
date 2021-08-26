7a82a68727c242a899caa207ca514e6b5d744458
annievoskanyan <you@example.com>
2021-08-26 15:51:02 +0400

    Exercise_10 Version_2 Lesson_6

--- ani.voskanyan/repo/Lesson_6/exercise_10/exercise_10.py
+++ ani.voskanyan/repo/Lesson_6/exercise_10/exercise_10.py
@@ -2,28 +2,21 @@
 
 points = []
 i, j = 0, 0
-coordinate_x = input()
+coordinate_x = input("Enter the x part of the coordinate: ")
 while coordinate_x != '':
     points.append([])
     points[i].append(int(coordinate_x))
-    coordinate_y = int(input())
+    coordinate_y = int(input("Enter the y part of the coordinate: "))
     points[i].append(coordinate_y)
-    coordinate_x = input()
+    coordinate_x = input("Enter the x part of the coordinate: (blank to quit): ")
     i += 1
-
-print(points)
 
 distances = []
 
-
-def calculate_perimeter (*list):
-    for k in range(0, len(list)-1):
-        dist = ((list[k + 1][0] - list[k][0]) ** 2 + (list[k + 1][1] - list[k][1]) ** 2) ** (1 / 2)
-        distances.append(dist)
-    dist = ((list[len(list) - 1][0] - list[0][0]) ** 2 + (list[len(list) - 1][1] - list[0][1]) ** 2) ** (1 / 2)
+for k in range(0, len(points)-1):
+    dist = ((points[k + 1][0] - points[k][0]) ** 2 + (points[k + 1][1] - points[k][1]) ** 2) ** (1 / 2)
     distances.append(dist)
-    poly_perimeter = sum(distances)
-    return poly_perimeter
+dist = ((points[len(points) - 1][0] - points[0][0]) ** 2 + (points[len(points) - 1][1] - points[0][1]) ** 2) ** (1 / 2)
+distances.append(dist)
 
-
-print(calculate_perimeter(*points))
+print("The perimeter of that polygon is:", sum(distances))
