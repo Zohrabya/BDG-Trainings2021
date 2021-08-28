d20aef2cac82b819c4316976750663571a039340
annievoskanyan <you@example.com>
2021-08-28 11:10:22 +0400

    Exercise_02 Version_1

---
+++ ani.voskanyan/repo/Lesson_7/exercise_02/exercise_02.py
@@ -0,0 +1,22 @@
+#!usr/bin/env python3
+
+args_list = []
+
+def arguments_input ():
+    while len(args_list) < 3:
+        arg_input = float(input(f"Please enter a value N{len(args_list) + 1}: "))
+        args_list.append(arg_input)
+        
+
+def calculate_median(*list):
+    sorted_list = sorted(list)
+    print(sorted_list[1])
+
+
+try:
+   arguments_input()
+except ValueError: 
+    print("You can enter only numbers!")
+    arguments_input()
+
+calculate_median(*args_list) 
