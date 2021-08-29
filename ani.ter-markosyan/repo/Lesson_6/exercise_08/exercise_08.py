ba12e9fd68b735fc2d494b63ed259ac7bd52c7b1
ani2025 <ANIHARUTYUNYAN61@GMAIL.COM>
2021-08-29 20:17:49 +0400

    exercise_08_lesson6_version2

--- ani.ter-markosyan/repo/Lesson_6/exercise_08/exercise_08.py
+++ ani.ter-markosyan/repo/Lesson_6/exercise_08/exercise_08.py
@@ -5,5 +5,5 @@ discount = 0.6
 list_1 = [4.95, 9.95, 14.95, 19.95, 24.95]
 for item in (list_1):
     discount_list = discount * item
-    new_prices = discount * item + item
-    print (item, 3 * "\t", round(discount_list, 2), 3 * "\t", round(new_prices, 2))
+    new_prices = item - discount * item
+    print (item, 3 * "\t",format(discount_list, ".2f"), 3 * "\t", format(new_prices, ".2f"))
