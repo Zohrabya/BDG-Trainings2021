1ab92f3bf1320fc3fb55769d8385fa0bbeb5a196
ani2025 <ANIHARUTYUNYAN61@GMAIL.COM>
2021-08-29 20:56:28 +0400

    exercise_07_lesson6_version2

--- ani.ter-markosyan/repo/Lesson_6/exercise_07/exercise_07.py
+++ ani.ter-markosyan/repo/Lesson_6/exercise_07/exercise_07.py
@@ -1,11 +1,11 @@
 #!usr/bin/evn python3
 
-count = 0 
+count = -1 
 sum = 0
 values = int
 while values != 0:
     values = int(input("Enter a value (enter 0 to exit): "))
     sum = sum + values
-    count += 1
-average = sum / (count-1)
-print(f"Average of the values is {average}")
+    count += 1   
+average = sum / count
+print(f"Average of the values is {round(average, 2)}")
