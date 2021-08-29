c307a6e7f5f48b9ab2783fec5803fbb9199e9190
ani2025 <ANIHARUTYUNYAN61@GMAIL.COM>
2021-08-30 00:54:09 +0400

    exercise_09_lesson6_version3

--- ani.ter-markosyan/repo/Lesson_6/exercise_09/exercise_09.py
+++ ani.ter-markosyan/repo/Lesson_6/exercise_09/exercise_09.py
@@ -1,8 +1,6 @@
 #!usr/bin/evn python3
 
 print ("Celsius", "\t", "Fahrenheit")
-for i in range (0, 101):
-    if i % 10 == 0:
-        celsius = i
-        fahrenheit = (i * 10 * 9 / 5) + 32
-        print(celsius, "C", 2 * "\t", fahrenheit, "F")
+for i in range (0, 101, 10):
+        fahrenheit = i * 9 / 5 + 32
+        print(i, "C", 2 * "\t", fahrenheit, "F")
