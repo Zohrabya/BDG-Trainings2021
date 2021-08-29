ba6b02b3cf9c05d15555d457ca68d25d498f9fb2
ani2025 <ANIHARUTYUNYAN61@GMAIL.COM>
2021-08-29 21:14:31 +0400

    exercise_04_lesson6_version2

--- ani.ter-markosyan/repo/Lesson_6/exercise_04/exercise_04.py
+++ ani.ter-markosyan/repo/Lesson_6/exercise_04/exercise_04.py
@@ -1,5 +1,5 @@
 #!usr/bin/evn python3
 
 rows = int(input("Enter the number of rows: "))
-for i in range(rows):
-        print("*" * (rows - i) + " " * i)
+for i in range(-rows, 0):
+        print ("*" * (-i))
