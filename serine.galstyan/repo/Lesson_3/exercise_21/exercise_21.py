93758269fa7905805ef5983fca194da875bfbb6a
serine-galstyan <serine.galstyan98@gmail.com>
2021-08-31 05:59:14 +0400

    Exercise_21 Version2

--- serine.galstyan/repo/Lesson_3/exercise_21/exercise_21.py
+++ serine.galstyan/repo/Lesson_3/exercise_21/exercise_21.py
@@ -1,6 +1,6 @@
 #!/usr/bin/evn python3
 
-entered_number = int(input("Enter a number:"))
+entered_number = int(input("Enter a number: "))
 if entered_number % 2 == 0:
     print("The number you entered is even")
 else:
