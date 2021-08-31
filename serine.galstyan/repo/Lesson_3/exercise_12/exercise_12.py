dcf046f517aa309c66630d02d82dc0513eb352e0
serine-galstyan <serine.galstyan98@gmail.com>
2021-08-31 04:53:24 +0400

    Exercise_12 Version2

--- serine.galstyan/repo/Lesson_3/exercise_12/exercise_12.py
+++ serine.galstyan/repo/Lesson_3/exercise_12/exercise_12.py
@@ -1,7 +1,8 @@
 #!/usr/bin/evn python3
 
-liter_or_less = int(input("Enter the number of containers that holding 1 liter or less:"))
-more_than_liter = int(input("Enter the number of containers that holding more than 1 liter:"))
-total = (0.10 * liter_or_less) + (0.25 * more_than_liter)
-final = round(total, 2)
-print("The refund for returning containers is $" + str(final) + ".")
+liter_or_less = int(input("Enter the number of containers that holding 1 liter or less: "))
+more_than_liter = int(input("Enter the number of containers that holding more than 1 liter: "))
+lightweight_container_deposit = 0.10
+heavy_container_deposit = 0.25
+total = (lightweight_container_deposit * liter_or_less) + (heavy_container_deposit * more_than_liter)
+print("The refund for returning containers is $" + str(round(total, 2)) + ".")
