f7131d95bd3dc6f0d1b99d0a456d13fe63a6f1d2
serine-galstyan <serine.galstyan98@gmail.com>
2021-08-31 04:38:19 +0400

    Exercise_08 Version2

--- serine.galstyan/repo/Lesson_3/exercise_08/exercise_08.py
+++ serine.galstyan/repo/Lesson_3/exercise_08/exercise_08.py
@@ -1,8 +1,8 @@
 #!/usr/bin/evn python3
 
-first_num = int(input("Enter first number:"))
-second_num = int(input("Enter second number:"))
-third_num = int(input("Enter third number:"))
+first_num = int(input("Enter first number: "))
+second_num = int(input("Enter second number: "))
+third_num = int(input("Enter third number: "))
 total = first_num + second_num + third_num
 average = total / 3
 print("Total:", total, "\nAverage:", round(average, 2))
