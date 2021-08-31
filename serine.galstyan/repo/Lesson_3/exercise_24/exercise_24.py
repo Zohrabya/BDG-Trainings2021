5a6d084c46f0d4afe75a3ea2837a13f512524e72
serine-galstyan <serine.galstyan98@gmail.com>
2021-08-31 06:09:34 +0400

    Exercise_24 Version2

--- serine.galstyan/repo/Lesson_3/exercise_24/exercise_24.py
+++ serine.galstyan/repo/Lesson_3/exercise_24/exercise_24.py
@@ -1,25 +1,25 @@
 #!/usr/bin/evn python3
 
-print("integer  square     cube")
+print("integer\t\tsquare\t\tcube")
 integer = 0
-print(integer, integer ** 2, integer ** 3, sep="          ")
+print(integer, integer ** 2, integer ** 3, sep="\t\t")
 integer = 1
-print(integer, integer ** 2, integer ** 3, sep="          ")
+print(integer, integer ** 2, integer ** 3, sep="\t\t")
 integer = 2
-print(integer, integer ** 2, integer ** 3, sep="          ")
+print(integer, integer ** 2, integer ** 3, sep="\t\t")
 integer = 3
-print(integer, integer ** 2, integer ** 3, sep="          ")
+print(integer, integer ** 2, integer ** 3, sep="\t\t")
 integer = 4
-print(integer, integer ** 2, integer ** 3, sep="         ")
+print(integer, integer ** 2, integer ** 3, sep="\t\t")
 integer = 5
-print(integer, integer ** 2, integer ** 3, sep="         ")
+print(integer, integer ** 2, integer ** 3, sep="\t\t")
 integer = 6
-print(integer, integer ** 2, integer ** 3, sep="         ")
+print(integer, integer ** 2, integer ** 3, sep="\t\t")
 integer = 7
-print(integer, integer ** 2, integer ** 3, sep="         ")
+print(integer, integer ** 2, integer ** 3, sep="\t\t")
 integer = 8
-print(integer, integer ** 2, integer ** 3, sep="         ")
+print(integer, integer ** 2, integer ** 3, sep="\t\t")
 integer = 9
-print(integer, integer ** 2, integer ** 3, sep="         ")
+print(integer, integer ** 2, integer ** 3, sep="\t\t")
 integer = 10
-print(integer, integer ** 2, integer ** 3, sep="        ")
+print(integer, integer ** 2, integer ** 3, sep="\t\t")
