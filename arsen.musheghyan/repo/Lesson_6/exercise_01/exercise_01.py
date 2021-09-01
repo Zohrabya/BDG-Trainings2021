1798a4db185ce76d11be60670995cac727372454
Arsen1978 <jobforars@gmail.com>
2021-09-01 13:52:54 +0400

    Version1

---
+++ arsen.musheghyan/repo/Lesson_6/exercise_01/exercise_01.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+wide = int(input("Please, input wide of the box: "))
+high = int(input("Please, input high of the box: "))
+i = 1
+for i in range (high):
+    print('*'*wide)
