0161915f211e275cad02b026dc1b5ae10cf5496d
Armen M <amkhoyanh@gmail.com>
2021-09-02 11:09:51 -0400

    Exercise_04 Version2

--- armen.mkhoyan/repo/Lesson_3/exercise_04/exercise_04.py
+++ armen.mkhoyan/repo/Lesson_3/exercise_04/exercise_04.py
@@ -4,4 +4,7 @@ a = 512-282
 b = 47*48+5
 #print(a/b)
 '''
-print((512-282)/(47*48+5))
+#should print 0.1017
+#Add Spaces before and after arithmetic operations
+
+print(f'{((512 - 282) / (47 * 48 + 5)):.4f}')
