df50032be07324fd7266188eb21dfed7e56b987e
LusineSar <lusinesargsyan85@yahoo.com>
2021-09-02 20:43:51 +0400

    Exercise_11 Version5

--- lusine.sargsyan/repo/Lesson_3/exercise_11/exercise_11.py
+++ lusine.sargsyan/repo/Lesson_3/exercise_11/exercise_11.py
@@ -4,4 +4,4 @@ lenght = float(input("Enter the lenght of the field (feets): "))
 width = float(input("Enter the width of the field (feets): "))
 #area in acre
 ACRE = 43560 
-print("Area is: ", round(lenght * width / ACRE, 2), "acres.")
+print("Area is:", round(lenght * width / ACRE, 2), "acres.")
