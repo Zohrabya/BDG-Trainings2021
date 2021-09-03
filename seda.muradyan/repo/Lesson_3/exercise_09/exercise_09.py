415dedf63141924b1a5b45a8bc1eca9c9178bbc6
sedamur-qa <seda.hrachovna@gmail.com>
2021-09-03 11:29:43 +0400

    EXercise_09 Version2

--- seda.muradyan/repo/Lesson_3/exercise_09/exercise_09.py
+++ seda.muradyan/repo/Lesson_3/exercise_09/exercise_09.py
@@ -1,10 +1,8 @@
-# Home work lesson 3/9
-
 price = float(input ("Enter price of the meal: "))
-tipPracent = float(input ("Enter tip percent: "))
+tip_pracent = float(input ("Enter tip percent: "))
 
-tipValue = price * tipPracent /100
-totalValue = price + tipValue
+tip_value = price * tip_pracent / 100
+total_value = price + tip_value
 
-print("total value is equal " + str(totalValue))
-print("tip value is equal " + str(tipValue))
+print("total value is equal " + str(total_value))
+print("tip value is equal " + str(tip_value))
