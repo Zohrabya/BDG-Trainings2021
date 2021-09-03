dae2345d3294d1103c669ef3a0b408745649dcbd
sedamur-qa <seda.hrachovna@gmail.com>
2021-09-03 09:26:36 +0400

    Exercise_4 Version3

--- seda.muradyan/repo/lesson3/exercise_4/exercise_4.py
+++ seda.muradyan/repo/lesson3/exercise_4/exercise_4.py
@@ -1,8 +1,2 @@
-value_A = 512
-value_B = 282
-value_C = 47
-value_D = 48
-value_E = 5
-
-output=(value_A - value_B)/(value_C * value_D + value_E)
+output=(512 - 282)/(47 * 48 + 5)
 print(str(output))
