9e8387eec1e4c3fbab7d8247b071a3d48a69659a
sedamur-qa <seda.hrachovna@gmail.com>
2021-09-03 12:38:16 +0400

    Exercise_10 Version2

--- seda.muradyan/repo/exercise_10/exercise_10.py
+++ seda.muradyan/repo/exercise_10/exercise_10.py
@@ -1,9 +1,3 @@
-#home work Lesson 3 exe_10
-
 width = float(input("Enter your room width: "))
 lenght = float(input("Enter your room lenght: "))
-
-
-output = "The square of your room equal to " + str(width * lenght) + "m^2" 
-
-print(output)
+print("The square of your room equal to " + str(width * lenght) + "m^2")
