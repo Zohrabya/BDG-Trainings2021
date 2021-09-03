4cc1cc5b941bd032dc61fa42d2b61ae39d9afc32
sedamur-qa <seda.hrachovna@gmail.com>
2021-09-03 18:43:30 +0400

    Exercise_24 Version2

--- seda.muradyan/repo/lesson3/exercise_24/exercise_24.py
+++ seda.muradyan/repo/lesson3/exercise_24/exercise_24.py
@@ -1,23 +1,23 @@
-print("integer  square cube")
+print("integer\t\tsquare\t\tcube")
 integer = 0
-print(integer, integer ** 2, integer ** 3, sep="")
+print(integer, integer ** 2, integer ** 3, sep="\t\t")
 integer = 1
-print(integer, integer ** 2, integer ** 3, sep="")
+print(integer, integer ** 2, integer ** 3, sep="\t\t")
 integer = 2
-print(integer, integer ** 2, integer ** 3, sep="")
+print(integer, integer ** 2, integer ** 3, sep="\t\t")
 integer = 3
-print(integer, integer ** 2, integer ** 3, sep="")
+print(integer, integer ** 2, integer ** 3, sep="\t\t")
 integer = 4
-print(integer, integer ** 2, integer ** 3, sep="")
+print(integer, integer ** 2, integer ** 3, sep="\t\t")
 integer = 5
-print(integer, integer ** 2, integer ** 3, sep="")
+print(integer, integer ** 2, integer ** 3, sep="\t\t")
 integer = 6
-print(integer, integer ** 2, integer ** 3, sep="")
+print(integer, integer ** 2, integer ** 3, sep="\t\t")
 integer = 7
-print(integer, integer ** 2, integer ** 3, sep="")
+print(integer, integer ** 2, integer ** 3, sep="\t\t")
 integer = 8
-print(integer, integer ** 2, integer ** 3, sep="")
+print(integer, integer ** 2, integer ** 3, sep="\t\t")
 integer = 9
-print(integer, integer ** 2, integer ** 3, sep="")
+print(integer, integer ** 2, integer ** 3, sep="\t\t")
 integer = 10
-print(integer, integer ** 2, integer ** 3, sep="")
+print(integer, integer ** 2, integer ** 3, sep="\t\t")
