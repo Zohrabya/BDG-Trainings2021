8fe8bb3970fb873b8b769fece872633223881051
sedamur-qa <seda.hrachovna@gmail.com>
2021-09-03 18:20:20 +0400

    Exercise_23 Version2

--- seda.muradyan/repo/Lesson_3/exercise_23/exercise_23.py
+++ seda.muradyan/repo/Lesson_3/exercise_23/exercise_23.py
@@ -1,4 +1,4 @@
-print("Enter a 5 digit whole number: ")
+print("Enter a 5_digit number: ")
 num = int(input())
 digit5 = num % 10
 num = int(num / 10)
@@ -9,5 +9,5 @@ num = int(num / 10)
 digit2 = num % 10
 num = int(num / 10)
 digit1 = num % 10
-print(digit1,"   ",digit2,"   ",digit3,"   ",digit4,"   ",digit5,"\n")
+print(digit1,digit2,digit3,digit4,digit5, sep='  ')
 
