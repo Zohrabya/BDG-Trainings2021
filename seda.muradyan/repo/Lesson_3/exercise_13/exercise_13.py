dfb7f6969515fb13429d1830b3878198f7dddc94
sedamur-qa <seda.hrachovna@gmail.com>
2021-09-03 15:15:22 +0400

    Exercise_13 Version2

--- seda.muradyan/repo/Lesson_3/exercise_13/exercise_13.py
+++ seda.muradyan/repo/Lesson_3/exercise_13/exercise_13.py
@@ -1,14 +1,10 @@
-# Home work lesson 3/13
-
 price = float(input ("Enter price of the meal: "))
+tax_Precent = 22
+tip_Precent = 18
+tip_value = price * 18 / 100
+tax_value = price * 22 / 100
+total_value = price + tip_value + tax_value
 
-taxPracent = 22
-tipPracent = 18
-
-tipValue = price * tipPracent /100
-taxValue = price * taxPracent /100
-totalValue = price + tipValue + taxValue
-
-print("total value is equal: " + "{:.2f}".format(totalValue))
-print("tip value is equal: " + "{:.2f}".format(tipValue))
-print("tax value is equal: " + "{:.2f}".format(taxValue))
+print("total value is equal: " + "{:.2f}".format(total_value))
+print("tip value is equal: " + "{:.2f}".format(tip_value))
+print("tax value is equal: " + "{:.2f}".format(tax_value))
