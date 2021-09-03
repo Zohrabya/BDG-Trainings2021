fa37ac0c009b1d2728aac9daa9b821b18b9ca01e
sedamur-qa <seda.hrachovna@gmail.com>
2021-09-03 15:48:49 +0400

    Exercise_16 Version2

--- seda.muradyan/repo/exercise_16/exercise_16.py
+++
@@ -1,9 +0,0 @@
-
-firstnumber = int(input("Enter the first number: "))
-secondnumber = int(input("Enter the second number: "))
-if firstnumber > secondnumber:
-    print(firstnumber, "is larger")
-elif firstnumber < secondnumber:
-    print(secondnumber, "is larger")
-else: 
-    print("The numbers are equal")
