e6f8296367878a642633690a82e5247bdbb5b992
ani2025 <ANIHARUTYUNYAN61@GMAIL.COM>
2021-09-03 14:45:34 +0400

    exercise_17_version4

--- ani.ter-markosyan/repo/Lesson_3/exercise_17/exercise_17.py
+++ ani.ter-markosyan/repo/Lesson_3/exercise_17/exercise_17.py
@@ -5,15 +5,21 @@ sum = int(number1) + int (number2) + int(number3)
 average = round(int (number1) + int(number2) + int(number3) / 3, 2)
 product = int(number1) * int(number2) * int(number3)
 if number1 > number2 and number1 > number3:
-    largest = number1
+    if number2 < number3:
+        largest = number1
+        smallest = number2
+    else:
+        smallest = number3
 elif number2 > number1 and number2 > number3:
-    largest = number2
+    if number1 < number3:
+        largest = number2
+        smallest = number1
+    else:
+        smallest = number3
 else:
     largest = number3
-if number1 < number2 and number1 < number3:
-    smallest = number1
-elif number2 < number1 and number2 < number3:
-    smallest = number2
-else:
-    smallest = number3
+    if number1 < number2:
+        smallest = number1
+    else:
+        smallest = number2
 print("Sum is", sum), print("Average is", average), print("Product is", product), print("Smallest is", smallest), print("Largest is", largest)
