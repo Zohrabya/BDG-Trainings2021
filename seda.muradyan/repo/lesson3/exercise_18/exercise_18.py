300a205cf67d7f446b5be13e39c50099fd52e05c
sedamur-qa <seda.hrachovna@gmail.com>
2021-09-03 16:23:04 +0400

    Exercise_18 Version2

--- seda.muradyan/repo/lesson3/exercise_18/exercise_18.py
+++ seda.muradyan/repo/lesson3/exercise_18/exercise_18.py
@@ -1,3 +1,4 @@
-pi = 3,14
-r =int(input ("Input the radius of the circle : "))
-print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
+pi = 3.14
+r = float(input ("Input the radius of the circle : "))
+print("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
+ 
