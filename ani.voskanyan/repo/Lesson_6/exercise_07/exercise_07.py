55427d6ea237ac9595dbac90d246a664e84c0d9e
annievoskanyan <you@example.com>
2021-09-04 23:37:04 +0400

    Exercise_7 Version_4 Lesson_6

--- ani.voskanyan/repo/Lesson_6/exercise_07/exercise_07.py
+++ ani.voskanyan/repo/Lesson_6/exercise_07/exercise_07.py
@@ -6,6 +6,6 @@ numbers_list = []
 while number != 0:
     numbers_list.append(number)
     number = int(input("Enter next number"))
-else:
-    numbers_average = sum(numbers_list) / len(numbers_list)
-    print(f'{numbers_average:.2f}')
+
+numbers_average = sum(numbers_list) / len(numbers_list)
+print(f'{numbers_average:.2f}')
