bd22ba10e2ca39e60f459d043a03d35c9a73823c
annievoskanyan <you@example.com>
2021-09-04 23:29:14 +0400

    Exercise_2 Version_3 Lesson_6

--- ani.voskanyan/repo/Lesson_6/exercise_02/exercise_02.py
+++ ani.voskanyan/repo/Lesson_6/exercise_02/exercise_02.py
@@ -3,7 +3,7 @@
 width = int(input("Enter the width of the box: "))
 height = int(input("Enter the height of the box: "))
 
-for i in range(0, height):
+for i in range(height):
     if i == 0 or i == height - 1:
         print("*" * width)
     else:
