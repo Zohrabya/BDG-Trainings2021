6016bf79098ad63903d0619e5d790c3807c5237c
annievoskanyan <you@example.com>
2021-09-04 22:57:36 +0400

    Exercise_06 Version_4

--- ani.voskanyan/repo/Lesson_3/exercise_06/exercise_06.py
+++ ani.voskanyan/repo/Lesson_3/exercise_06/exercise_06.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 
 number_x = int(input("Please enter a number: "))
-print(f'{number_x}---{2 * number_x}---{3 * number_x}---{4 * number_x}---{5 * number_x}')
+print(number_x, 2 * number_x, 3 * number_x, 4 * number_x, 5 * number_x, sep = "---")
