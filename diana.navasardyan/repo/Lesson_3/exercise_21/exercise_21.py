d6d37eccd20aa02d1877685f5a8ceea5b0ea5f8c
diananavasardyan <diana.navasardyan@gmail.com>
2021-09-05 01:47:01 +0400

    exercise_21 Version1

---
+++ diana.navasardyan/repo/Lesson_3/exercise_21/exercise_21.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python3
+input_number = int(input("Please enter a number: "))
+
+if input_number % 2 == 0:
+    print (f'{input_number} is even number')
+else: 
+    print(f'{input_number} is odd number')
