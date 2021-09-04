50d9600ddb7fed23c8b47c6d308f91b08ea88cc9
diananavasardyan <diana.navasardyan@gmail.com>
2021-09-05 00:01:26 +0400

    Exercise_12 Version1

---
+++ diana.navasardyan/repo/Lesson_3/exercise_12/exercise_12.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python3
+inp = float(input("Please enter the size of the bottle: "))
+
+if inp <= 1:
+    print("Your refund amount is $0.10")
+else:
+    print("Your refund amount is $0.25")
