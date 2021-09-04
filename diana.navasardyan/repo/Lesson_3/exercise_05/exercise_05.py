163818f94f4d77bb6285f904a3ec5a5690a9915e
diananavasardyan <diana.navasardyan@gmail.com>
2021-09-04 17:14:03 +0400

    Exercise_05 Version 3

--- diana.navasardyan/repo/Lesson_3/exercise_05/exercise_05.py
+++ diana.navasardyan/repo/Lesson_3/exercise_05/exercise_05.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python3
 
-number = int(input("Please enter a number"))
+number = int(input("Please enter a number  "))
 print("Enter a number",number, sep = ":")
 print("The square of", number,"is", number ** 2, ".")
