1f78f9be49dcf7f1592a0c0cf12f21528cf13170
diananavasardyan <diana.navasardyan@gmail.com>
2021-09-05 14:50:16 +0400

    Exercise_26 Version2

--- diana.navasardyan/repo/Lesson_3/exercise_26/exercise_26.py
+++ diana.navasardyan/repo/Lesson_3/exercise_26/exercise_26.py
@@ -1,5 +1,4 @@
-#! /usr/bin/env python3
-
+#!/usr/bin/env python3
 passport_name = input("Please enter your name: " )
 passport_surname = input("Please enter your surname: ")
 print(passport_name.capitalize(), passport_surname.capitalize())
