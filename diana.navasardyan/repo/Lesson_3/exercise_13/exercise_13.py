fea031f2fa7363dffc851b96dc1ed9a57d691682
diananavasardyan <diana.navasardyan@gmail.com>
2021-09-05 15:46:19 +0400

    Exercise_13 Version2

--- diana.navasardyan/repo/Lesson_3/exercise_13/exercise_13.py
+++ diana.navasardyan/repo/Lesson_3/exercise_13/exercise_13.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
+
 meal_price = float(input("Please enter meal price: ")) 
-meal_tax = meal_price * 20/100
-meal_tip = (meal_price - meal_tax) * 18/100
+tax_percentage = 0.20
+meal_tax = meal_price * tax_percentage
+tip_percentage = 0.18
+meal_tip = meal_price * tip_percentage
 meal_total_price = meal_price + meal_tax + meal_tip
 print("The meal tax is {}\nThe meal tip is {}\nThe grand total of the meal is {}".format(meal_tax,meal_tip,meal_total_price))
