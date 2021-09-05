c4335f478a7696087ce339ce941e852b6668683a
diananavasardyan <diana.navasardyan@gmail.com>
2021-09-05 14:47:15 +0400

    Exercise_25 Version2

--- diana.navasardyan/repo/Lesson_3/exercise_25/exercise_25.py
+++ diana.navasardyan/repo/Lesson_3/exercise_25/exercise_25.py
@@ -1,7 +1,5 @@
-#!usr/bin/evn python3
-
+#!/usr/bin/evn python3
 user_input  =  input("Please enter four digit number: ")
 print("+".join(user_input), "=", int(user_input[0]) + int(user_input[1]) + int(user_input[2]) + int(user_input[3]))
-
 
 
