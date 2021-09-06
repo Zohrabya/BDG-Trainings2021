2212713a5cb7caa9fd2cb9b81a961206c73ba902
LusineSar <lusinesargsyan85@yahoo.com>
2021-09-07 00:59:23 +0400

    Exercise_12 Version3

--- lusine.sargsyan/repo/Lesson_3/exercise_12/exercise_12.py
+++ lusine.sargsyan/repo/Lesson_3/exercise_12/exercise_12.py
@@ -1,9 +1,8 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
 
-
-deposite_1 = 0.10
-deposite_2 = 0.25
-count1 = int(input("please enter the count of conteiners with litrage 1l or less: "))
-count2 = int(input("please enter the count of conteiners with litrage more than 1l: "))
-
-print ("Deposite is: $ {0:1.2f}".format(count1 * deposite_1 + count2 * deposite_2))
+DEPOS_1 = 0.10
+DEPOS_2 = 0.25
+refund_1 = int(input("Please enter the count of conteiners with litrage 1l or less: "))
+refund_2 = int(input("Please enter the count of conteiners with litrage more than 1l: "))
+refund = refund_1 * DEPOS_1 + refund_2 * DEPOS_2
+print ("Deposite is: $", round(refund))
