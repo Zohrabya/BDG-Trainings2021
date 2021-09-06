ec8369ee133dc838e053f10fd1802be7da3ff0e5
LusineSar <lusinesargsyan85@yahoo.com>
2021-09-06 15:15:36 +0400

    Exercise_11 Version 1 Lesson 6

---
+++ lusine.sargsyan/repo/Lesson_6/exercise_11/exercise_11.py
@@ -0,0 +1,11 @@
+#!usr/bin/env python3
+
+words_entered = list()
+user_word = input("Please enter the word: ")
+while user_word != "":
+    if user_word not in words_entered:
+        words_entered.append(user_word)
+        continue
+    user_word = input("Please enter the word: ")
+for item in words_entered:
+    print(item)
