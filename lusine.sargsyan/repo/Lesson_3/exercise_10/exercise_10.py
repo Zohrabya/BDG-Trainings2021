4394e7c6ee3c136b9df27c28a044198e9212bac8
LusineSar <lusinesargsyan85@yahoo.com>
2021-09-07 01:17:28 +0400

    Exercise_10 Version2

--- lusine.sargsyan/repo/Lesson_3/exercise_10/exercise_10.py
+++ lusine.sargsyan/repo/Lesson_3/exercise_10/exercise_10.py
@@ -1,9 +1,7 @@
-#! /usr//bin/env python3
+#!/usr//bin/env python3
 
-unit = input("Enter the confortable for you mesuremnet unit (meter/feet): ")
+unit = str(input("Enter the confortable for you mesuremnet unit (meter/feet): "))
 width = float(input("Enter the width of the room: " ))
 lenght = float(input("Enter the lenght of the room: "))
-
 room_area = width * lenght
-
-print("The room area is: ", room_area, unit) 
+print("The room area is:", round(room_area, 2), unit) 
