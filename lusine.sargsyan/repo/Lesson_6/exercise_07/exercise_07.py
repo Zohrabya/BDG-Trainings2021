9c49fe5f570e9465afbb3e847deb8cc817a98b33
LusineSar <lusinesargsyan85@yahoo.com>
2021-09-07 01:42:12 +0400

    Exercise_07 Version2 Lesson 6

--- lusine.sargsyan/repo/Lesson_6/exercise_07/exercise_07.py
+++ lusine.sargsyan/repo/Lesson_6/exercise_07/exercise_07.py
@@ -6,5 +6,6 @@ count = 0
 while user_number != 0: 
     total += user_number
     count += 1
-    user_number  = float(input("please enter the number: "))
-print("Average is: {0:1.4f}".format(total / count))
+    user_number  = float(input("Please enter the number: "))
+average = total / count
+print("Average is:", round(average, 2))
