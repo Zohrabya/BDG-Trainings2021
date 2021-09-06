82dd8b68b5d5657774e874d07d4c541a562b9a50
LusineSar <lusinesargsyan85@yahoo.com>
2021-09-07 01:34:58 +0400

    Exrercise_11 Version 2 Lesson 6

--- lusine.sargsyan/repo/Lesson_6/exercise_11/exercise_11.py
+++ lusine.sargsyan/repo/Lesson_6/exercise_11/exercise_11.py
@@ -5,7 +5,6 @@ user_word = input("Please enter the word: ")
 while user_word != "":
     if user_word not in words_entered:
         words_entered.append(user_word)
-        continue
     user_word = input("Please enter the word: ")
 for item in words_entered:
     print(item)
