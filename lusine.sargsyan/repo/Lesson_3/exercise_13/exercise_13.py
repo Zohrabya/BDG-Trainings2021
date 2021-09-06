456b2fad7c45533ca12fffa6db4043e50837265f
LusineSar <lusinesargsyan85@yahoo.com>
2021-09-07 02:08:50 +0400

    Exercise_13 Version 4

--- lusine.sargsyan/repo/Lesson_3/exercise_13/exercise_13.py
+++ lusine.sargsyan/repo/Lesson_3/exercise_13/exercise_13.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
+#tax and tip Rates in percentage 
+TAXR = 20 
+TIPR = 18 
 price = float(input("Please neter the price of the meal: "))
-tax = price * 20 / 100
-tip = price * 18 / 100
+tax = price * TAXR / 100
+tip = price * TIPR / 100
 total = price + tip + tax 
-
-print("tip: $ {0:1.2f}".format(tip), "\ntax: $ {0:1.2f}".format(tax), "\ntotal amount: $ {0:1.2f}".format(total))
+print("Tip: $ {0:1.2f}".format(tip), "\nTax: $ {0:1.2f}".format(tax), "\nTotal amount: $ {0:1.2f}".format(total))
