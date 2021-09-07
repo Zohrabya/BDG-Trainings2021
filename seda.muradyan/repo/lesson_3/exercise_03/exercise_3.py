68fe61fff08bf42e45f76d77874b9e1d879cdd75
sedamur-qa <seda.hrachovna@gmail.com>
2021-09-07 11:56:33 +0400

    Exercise_5 Version 1 Lesson6

---
+++ seda.muradyan/repo/lesson_3/exercise_03/exercise_3.py
@@ -0,0 +1,6 @@
+h = eval(input("please enter diamond's height:"))
+
+for x in range(h):
+    print(" " * (h - x), "*" * (x * 2 + 1))
+for x in range(h - 2, -1, -1):
+    print(" " * (h - x), "*"*(x * 2 + 1))
