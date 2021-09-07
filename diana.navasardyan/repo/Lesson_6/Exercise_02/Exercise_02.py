25423733135cb16adbc347b3daf52ecbe8a1470f
diananavasardyan <diana.navasardyan@gmail.com>
2021-09-07 21:48:11 +0400

    Exercise_02_Lesson_6 Version2

--- diana.navasardyan/repo/Lesson_6/Exercise_02/Exercise_02.py
+++ diana.navasardyan/repo/Lesson_6/Exercise_02/Exercise_02.py
@@ -1,8 +1,12 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
 
 width = int(input("Enter the width of the box: "))
 height = int(input("Enter the height of the box: "))
-print("*" * width)
-for i in range(height - 2):
-    print("*", " " * (width - 2), "*", sep = "")
-print("*" * width)
+
+for i in range(height):
+    if i == 0 or i == height - 1:
+        print("*" * width)
+    else:
+        print("*", " " * (width - 2), "*", sep="")
+
+
