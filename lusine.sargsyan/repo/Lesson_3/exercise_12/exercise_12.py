2b2c88ddfc546a177384e836a7fddf3dd321cdc6
LusineSar <lusinesargsyan85@yahoo.com>
2021-09-08 21:03:08 +0400

    Exercise_12 Version5

--- lusine.sargsyan/repo/Lesson_3/exercise_12/exercise_12.py
+++ lusine.sargsyan/repo/Lesson_3/exercise_12/exercise_12.py
@@ -5,4 +5,4 @@ MORE_DEPOS = 0.25
 less_liter = int(input("Please enter the count of conteiners with litrage 1l or less: "))
 more_liter = int(input("Please enter the count of conteiners with litrage more than 1l: "))
 refund = less_liter * LESS_DEPOS + more_liter * MORE_DEPOS
-print("Deposite is: $", round(refund))
+print("Deposite is: $", round(refund, 2))
