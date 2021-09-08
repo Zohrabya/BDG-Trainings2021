1dc28c672fb3b0b4cdd8717902ddeec2a7db9fb2
Armen M <amkhoyanh@gmail.com>
2021-09-08 13:52:09 -0400

    Exercise_12 Version2

--- armen.mkhoyan/repo/Lesson_3/exercise_12/exercise_12.py
+++ armen.mkhoyan/repo/Lesson_3/exercise_12/exercise_12.py
@@ -3,8 +3,10 @@
 number_of_containers_of_one_liter_or_less = float(input("Enter number of one liter or less containers: "))
 number_of_containers_holding_more_than_one_liter = float(input("Enter number of more than one liter or containers: "))
 
-deposit_for_one_liter_or_less = 0.10
-deposit_for_more_than_one_liter = 0.25
+DEPOSIT_FOR_ONE_LITER_OR_LESS = 0.10
+DEPOSIT_FOR__MORE_THAN_ONE_LITER = 0.25
 
-print("Total refund for one liter or less containers: ","$", number_of_containers_of_one_liter_or_less*deposit_for_one_liter_or_less,)
-print("Total refund for more than one liter containers: ","$", number_of_containers_holding_more_than_one_liter*deposit_for_more_than_one_liter)
+#print("Total refund for one liter or less containers: ","$", round(number_of_containers_of_one_liter_or_less * DEPOSIT_FOR_ONE_LITER_OR_LESS, 2))
+#print("Total refund for more than one liter containers: ","$", round(number_of_containers_holding_more_than_one_liter * DEPOSIT_FOR__MORE_THAN_ONE_LITER, 2))
+
+print("Total refound is: ", "$", round((number_of_containers_of_one_liter_or_less * DEPOSIT_FOR_ONE_LITER_OR_LESS) + (number_of_containers_holding_more_than_one_liter * DEPOSIT_FOR__MORE_THAN_ONE_LITER), 2))
