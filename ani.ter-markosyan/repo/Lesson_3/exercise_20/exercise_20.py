cbae40acb1d960ceefb4a67ffe08e5999da93bc6
ani2025 <ANIHARUTYUNYAN61@GMAIL.COM>
2021-09-08 20:27:24 +0400

    exercise_20_version2

--- ani.ter-markosyan/repo/Lesson_3/exercise_20/exercise_20.py
+++ ani.ter-markosyan/repo/Lesson_3/exercise_20/exercise_20.py
@@ -1,31 +1,27 @@
-#!usr/bin/evn python3
-
-# num1, num2, num3, num4, num5 = map(int,input("Enter five different numbers: ").split())
-#largest = max(num1, num2, num3, num4, num5)
-#smallest = min(num1, num2, num3, num4, num5)
+#!usr/bin/env python3
 
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 num3 = int(input("Enter third number: "))
 num4 = int(input("Enter fourth number: "))
 num5 = int(input("Enter fifth number: "))
-if num1 > num2 and num1 > num3 and num1 >num4 and num1 > num5:
-    print("Largest is", num1)
-elif num2 > num1 and num2 > num3 and num2 > num4 and num2 > num5:
-    print("Largest is", num2)
-elif num3 > num1 and num3> num2 and num3> num4 and num3 > num5:
-    print("Largest is", num3)
-elif num4 > num1 and num4 > num2 and num4 > num3 and num4 > num5:
-    print("Largest is", num4)
-elif num5 > num1 and num5 > num2 and num5 > num3 and num5 > num4:
-    print("Largest is", num5)
-if num1 < num2 and num1 < num3 and num1 < num4 and num1 < num5:
-    print("Smallest is", num1)
-elif num2 < num1 and num2 < num3 and num2 < num4 and num2 < num5:   
-    print("Smallest is", num2)
-elif num3 < num1 and num3 < num2 and num3 < num4 and num3 < num5:
-    print("Smallest is", num3)
-elif num4 < num1 and num4 < num2 and num4 < num3 and num4 < num5:
-    print("Smallest is", num4)
-elif num5 < num1 and num5 < num2 and num5 < num3 and num5 < num4:
-    print("Smallest is", num5)
+min_num = num1
+max_num = num1
+if num2 < min_num:
+    min_num = num2
+if num3 < min_num:
+    min_num = num3
+if num4 < min_num:
+    min_num = num4
+if num5 < min_num:
+    min_num = num5
+print("Smallest integer is", min_num)
+if num2 > max_num:
+    max_num = num2
+if num3 > max_num:
+    max_num = num3
+if num4 > max_num:
+    max_num = num4
+if num5 > max_num:
+    max_num = num5  
+print("Largest integer is", max_num)
