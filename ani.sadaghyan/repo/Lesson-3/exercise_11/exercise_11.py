9f11e4e1a60037aabd3fd4b2876373b8b5f772e2
Ani Sadaghyan <anisadaghyan@gmail.com>
2021-09-08 21:13:36 +0400

    exercise_11 version7

--- ani.sadaghyan/repo/Lesson-3/exercise_11/exercise_11.py
+++ ani.sadaghyan/repo/Lesson-3/exercise_11/exercise_11.py
@@ -1,4 +1,4 @@
 length = int(input("Enter the length (in feet): "))
 width = int(input("Enter the width (in feet): "))
 ACRE = 43560
-print("The area of a field is", round(width * lenght / ACRE, 2), "acres: ")
+print("The area of a field is", round(width * length / ACRE, 2), "acres.")
