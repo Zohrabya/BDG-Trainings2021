84e87a01eee7a50e80df66071b7c73028e2627ce
ver-galstyan <vera.galstyan@inbox.ru>
2021-09-08 21:19:50 +0400

    Exercise_01 Version 5

--- Vera.Galstyan/repo/Lesson_3/exercise_01/exercise_01.py
+++ Vera.Galstyan/repo/Lesson_3/exercise_01/exercise_01.py
@@ -1,12 +1,12 @@
-#! usr/bin/env python3
+#!usr\bin\env python 3
 
-print("*" * 15 ) 
+print(("*" * 19) + "\n" + ("*" * 19) + "\n" + ("*" * 19) + "\n" + ("*" * 19) + "\n")
 
-print("*" * 15 + "\n" + "*" * 15 + "\n" + "*" * 15 +"\n")
+print("*" * 19, "*" * 19, "*" * 19, "*" * 19, sep = "\n")
 
-print("*" * 15)
-print("*" * 15)
-print("*" * 15)
-print("*" * 15)
 
+print("\n" + "*" * 19)
+print("*" * 19)
+print("*" * 19)
+print("*" * 19)
 
