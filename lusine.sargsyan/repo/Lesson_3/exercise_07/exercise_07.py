0b9ddf4767e53fcab4742692dd6235b6d7347cd6
LusineSar <lusinesargsyan85@yahoo.com>
2021-09-08 19:53:20 +0400

    Exercise_07 Version2

--- lusine.sargsyan/repo/Lesson_3/exercise_07/exercise_07.py
+++ lusine.sargsyan/repo/Lesson_3/exercise_07/exercise_07.py
@@ -1,6 +1,4 @@
-#1/usr/bin/env python3
+#!/usr/bin/env python3
 
-user_waight = int(input("Enter your waight in kg: "))
-
-print("Waight in pound is: ", 2.2 * user_waight)
-
+waight = int(input("Enter your waight in kg: "))
+print("Waight in pound is:", round(2.2 * waight, 2))
