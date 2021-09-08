727e9d5290aa81f8d10c6066945f8e6d684fdbf2
diananavasardyan <diana.navasardyan@gmail.com>
2021-09-09 00:30:24 +0400

    Exercise_02_Lesson_6 Version3

--- diana.navasardyan/repo/Lesson_6/Exercise_02/Exercise_02.py
+++ diana.navasardyan/repo/Lesson_6/Exercise_02/Exercise_02.py
@@ -7,6 +7,4 @@ for i in range(height):
     if i == 0 or i == height - 1:
         print("*" * width)
     else:
-        print("*", " " * (width - 2), "*", sep="")
-
-
+        print("*", " " * (width - 2), "*", sep = "")
