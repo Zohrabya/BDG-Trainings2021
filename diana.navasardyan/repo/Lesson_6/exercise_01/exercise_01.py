a25436bfe5983a5104161b77a2a48aabca8b8793
diananavasardyan <diana.navasardyan@gmail.com>
2021-09-09 00:27:58 +0400

    Exercise_01_Lesson_6 Version5

--- diana.navasardyan/repo/Lesson_6/exercise_01/exercise_01.py
+++ diana.navasardyan/repo/Lesson_6/exercise_01/exercise_01.py
@@ -4,7 +4,4 @@ width = int(input("Enter the width of the box: "))
 height = int(input("Enter the height of the box: "))
 
 for i in range(height):
-    print("*" * width)    
-    
-
-    
+    print("*" * width)
