3c76d3140ccf2e608de94f17d800dae07b9de3de
diananavasardyan <diana.navasardyan@gmail.com>
2021-09-08 23:40:19 +0400

    Exercise_13 Version3

--- diana.navasardyan/repo/Lesson_3/exercise_13/exercise_13.py
+++ diana.navasardyan/repo/Lesson_3/exercise_13/exercise_13.py
@@ -6,4 +6,4 @@ meal_tax = meal_price * tax_percentage
 tip_percentage = 0.18
 meal_tip = meal_price * tip_percentage
 meal_total_price = meal_price + meal_tax + meal_tip
-print("The meal tax is {}\nThe meal tip is {}\nThe grand total of the meal is {}".format(meal_tax,meal_tip,meal_total_price))
+print("The meal tax is: {}\nThe meal tip is: {}\nThe grand total of the meal is: {}".format(meal_tax,meal_tip,meal_total_price))
