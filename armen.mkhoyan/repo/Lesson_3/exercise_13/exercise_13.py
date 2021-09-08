6d3af0f1ba8ecc8acbe2f4eae313898b012c187d
Armen M <amkhoyanh@gmail.com>
2021-09-08 14:06:12 -0400

    Exercise_13 Version2

--- armen.mkhoyan/repo/Lesson_3/exercise_13/exercise_13.py
+++ armen.mkhoyan/repo/Lesson_3/exercise_13/exercise_13.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 #Order amount
-cost = float(input("Enter the Cost of the meal: "))
+cost = float(input("Enter the cost of the meal: "))
 
 #tax is 6% 
-tax_amount = cost * 0.06
+tax_amount = cost * 6/100
 
 #Tip is 18%, without the tax.
-tip_amount =cost * 0.18
+tip_amount = cost * 18/100
 
-print("Tax: ","$", tax_amount," (Tip is 6%)" )
-print("Tip: ","$", tip_amount," (Tip is 18%, without the tax)")
-print("Total Cost Is: ", "$", cost + tax_amount + tip_amount)
+print("Tax: ","$", round(tax_amount,2)," (Tip is 6%)" )
+print("Tip: ","$", round(tip_amount, 2)," (Tip is 18%, without the tax)")
+print("Total cost is: ", "$", round(cost + tax_amount + tip_amount, 2))
