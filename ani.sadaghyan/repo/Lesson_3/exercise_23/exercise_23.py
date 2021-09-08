7a1f66fd2b3bf4c36bf838d074ce8ef0121502c7
Ani Sadaghyan <anisadaghyan@gmail.com>
2021-09-08 21:23:37 +0400

    exercise_23 version3

--- ani.sadaghyan/repo/Lesson_3/exercise_23/exercise_23.py
+++ ani.sadaghyan/repo/Lesson_3/exercise_23/exercise_23.py
@@ -1,5 +1,5 @@
 num = input("Enter a five-digit number: ")
 if len(num) == 5:
-    print(num[0], num[1], num[2], num[3], num[4], sep = " "*3)
+    print(num[0], num[1], num[2], num[3], num[4], sep = " " * 3)
 else:
     print("Enter a five-digit num!")
