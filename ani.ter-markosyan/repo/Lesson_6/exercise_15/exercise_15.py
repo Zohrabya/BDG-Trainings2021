360d4da34cc78dfdfb5c13ce591b08e23bef6cc1
ani2025 <ANIHARUTYUNYAN61@GMAIL.COM>
2021-09-08 11:14:19 +0400

    exercise_15_lesson6_version1

---
+++ ani.ter-markosyan/repo/Lesson_6/exercise_15/exercise_15.py
@@ -0,0 +1,12 @@
+#!usr/bin/env python3 
+
+import random
+
+lottery_nums = []
+while len(lottery_nums) != 6:
+    random_num = random.randrange(1,50)
+    if random_num not in lottery_nums:
+        lottery_nums.append(random_num)
+lottery_nums.sort()
+for num in lottery_nums:
+    print(num)
