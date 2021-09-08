426bd67bef2649130928e862cc9443ba0912aff7
ani2025 <ANIHARUTYUNYAN61@GMAIL.COM>
2021-09-08 20:39:08 +0400

    exercise_13_lesson6_version3

--- ani.ter-markosyan/repo/Lesson_6/exercise_13/exercise_13.py
+++ ani.ter-markosyan/repo/Lesson_6/exercise_13/exercise_13.py
@@ -1,8 +1,8 @@
-#!usr/bin/evn python3
+#!usr/bin/env python3
 
 list_of_divisors = []
 integer_1 = int(input("Enter an integer: "))
-for i in range(1, int((integer_1 / 2)) + 1):
+for i in range(1, int(integer_1 / 2 + 1)):
     if integer_1 % i == 0:
         list_of_divisors.append(i)
 print (list_of_divisors)
