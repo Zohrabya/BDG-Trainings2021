d97b83f6cf1e40059b52a15bce4bf4260ce380cc
Armen M <amkhoyanh@gmail.com>
2021-09-08 12:40:43 -0400

    Exercise_02 Version2

--- armen.mkhoyan/repo/Lesson_3/exercise_02/exercise_02.py
+++ armen.mkhoyan/repo/Lesson_3/exercise_02/exercise_02.py
@@ -1,24 +1,21 @@
 #!/usr/bin/env python3
 
 #a
-print("* * * * * * ","\n*         *","\n*         *","\n* * * * * * ")
+print("* * * * * * ","\n*         *" * 2, "\n* * * * * * ")
 
-print("---------------")
+
 
 #b
 x = "* * * * * * "
 y = "\n*         *"
 print(x,2*y,"\n* * * * * * ")
 
-print("---------------")
+
 
 #c
-a = "* * * * * *"
-b = "\n*         *"
-c = "\n*         *"
-d = "\n* * * * * * "
-print(a)
-print(b)
-print(c)
-print(d)
+
+print("* * * * * *")
+print("\n*         *")
+print("\n*         *")
+print("\n* * * * * * ")
 
