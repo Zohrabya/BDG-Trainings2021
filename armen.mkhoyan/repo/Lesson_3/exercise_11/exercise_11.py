812ce09ae2891e484c86d8b40b94810a26718ecd
Armen M <amkhoyanh@gmail.com>
2021-09-08 13:23:27 -0400

    Exercise_11 Version2

--- armen.mkhoyan/repo/Lesson_3/exercise_11/exercise_11.py
+++ armen.mkhoyan/repo/Lesson_3/exercise_11/exercise_11.py
@@ -2,5 +2,5 @@
 
 width = float(input("Enter a width of farmer's field in feet: "))
 length = float(input("Enter a length of farmer's field in feet: "))
-square_feet_in_acre = 43560
-print("Total area of the field is", (width*length)/square_feet_in_acre, "acre")
+SQUARE_FEET_IN_ACRE = 43560
+print("Total area of the field is", round((width * length) / SQUARE_FEET_IN_ACRE, 2), "acre")
