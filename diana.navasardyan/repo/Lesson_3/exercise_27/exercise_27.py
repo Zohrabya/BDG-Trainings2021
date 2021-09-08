b30087aaf71c487d17972c871b73119735936f2f
diananavasardyan <diana.navasardyan@gmail.com>
2021-09-08 21:15:23 +0400

    Exercise_27 Version3

--- diana.navasardyan/repo/Lesson_3/exercise_27/exercise_27.py
+++ diana.navasardyan/repo/Lesson_3/exercise_27/exercise_27.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 
 for i in range(1, 18):
-    print(i, "\t", str(oct(i))[2:], "\t", str(hex(i))[2:], "\t ", str(bin(i))[2:])
+    print(i, "\t", str(oct(i))[2:], "\t", str(hex(i))[2:], "\t", str(bin(i))[2:])
