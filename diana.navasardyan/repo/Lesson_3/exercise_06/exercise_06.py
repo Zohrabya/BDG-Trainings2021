8650e19b487a8469e3403a6c63d2a9eb4daf0932
diananavasardyan <diana.navasardyan@gmail.com>
2021-09-08 21:50:37 +0400

    Exercise_06 Version4

--- diana.navasardyan/repo/Lesson_3/exercise_06/exercise_06.py
+++ diana.navasardyan/repo/Lesson_3/exercise_06/exercise_06.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
 
-num = int(input("Please enter a number x: "))
-x = 7
-print("Enter a number", num, sep = ":")
-print(x, 2 * x, 3 * x, 4 * x, 5 * x, sep = "---")
+num = int(input("Please enter a number: "))
+print(num, 2 * num, 3 * num, 4 * num, 5 * num, sep = "---")
