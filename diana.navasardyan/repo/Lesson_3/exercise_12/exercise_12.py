3b490d2c9e0624e79f198efa33090b73de2223b5
diananavasardyan <diana.navasardyan@gmail.com>
2021-09-08 22:43:45 +0400

    Exercise_12 Version4

--- diana.navasardyan/repo/Lesson_3/exercise_12/exercise_12.py
+++ diana.navasardyan/repo/Lesson_3/exercise_12/exercise_12.py
@@ -5,5 +5,5 @@ number_1 = float(input("Enter the number of containers holding: "))
 if number_1 <= 1:
     print("Your deposit for containers is", round(number_1 * 0.10, 2), "$")
 else:
-    print("Your deposit for containers is", round(number_1 * 0.25, 2), "$")
+   print("Your deposit for containers is", round(number_1 * 0.25, 2), "$")
 
