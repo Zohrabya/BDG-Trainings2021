543db52fd808c3ee4b87b550166972c6f0a0c490
Armen M <amkhoyanh@gmail.com>
2021-09-09 07:24:42 -0400

    Exercise_18 Version2

--- armen.mkhoyan/repo/Lesson_3/exercise_18/exercise_18.py
+++ armen.mkhoyan/repo/Lesson_3/exercise_18/exercise_18.py
@@ -3,14 +3,14 @@
 radius = int(input("Enter radius: "))
 
 #diameter
-diameter = 2*radius
+diameter = 2 * radius
 print("Diameter is: ", diameter)
 
 #circumference (π=3.14159)
-p = 3.14159
-circumference = 2*p*radius
+P_CONSTANT = 3.14159
+circumference = 2 * P_CONSTANT * radius
 print("Circumference is: ", circumference)
 
 #area (π=3.14159)
-area = p*radius*radius
+area = P_CONSTANT * radius * radius
 print("Area is: ", area)
