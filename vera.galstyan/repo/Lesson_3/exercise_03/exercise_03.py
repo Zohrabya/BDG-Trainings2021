9aad287828801ef05b735ae396c4ea38aff43817
ver-galstyan <vera.galstyan@inbox.ru>
2021-09-09 21:00:14 +0400

    Exercise_03 Version 3

--- vera.galstyan/repo/Lesson_3/exercise_03/exercise_03.py
+++ vera.galstyan/repo/Lesson_3/exercise_03/exercise_03.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python3
 
-print("*","\n**","\n***","\n*****")
+print("*\n" + "*" * 2 + "\n" + "*" * 3 + "\n" + "*" * 4)
+
+print("\n*", "*" * 2, "*" * 3, "*" * 4, sep = "\n")
+
+print("\n*")
+print("*" * 2)
+print("*" * 3)
+print("*" * 4)
