12ac50a9e3a3ded27db67450a28b383d6f971e48
Armen M <amkhoyanh@gmail.com>
2021-09-09 14:47:17 -0400

    Lesson_6_EXE_07_Version1

---
+++ armen.mkhoyan/repo/Lesson_6/exercise_07/exercise_07.py
@@ -0,0 +1,12 @@
+#!/usr/bin/evn python3
+
+user_number = int(input("Enter a number: "))
+
+list_of_numbers = []
+
+while user_number != 0:
+    list_of_numbers.append(user_number)
+    user_number = int(input("Enter next number or '0' to print the average: "))
+
+numbers_average = sum(list_of_numbers) / len(list_of_numbers)
+print(round(numbers_average, 2))
