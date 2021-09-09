82be403742b08f5447ac875676a1d010ad48ea0c
hpoghosyan <hayk.m.poghosyan@gmail.com>
2021-09-10 01:44:08 +0400

    Exercise_22 Version1

---
+++ hayk.poghosyan/repo/Lesson_3/exercise_22/exercise_22.py
@@ -0,0 +1,7 @@
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
+
+if first_number % second_number == 0:
+    print("The first number is a multiple of the second.")
+else:
+    print("The first number isn't a multiple of the second.")
