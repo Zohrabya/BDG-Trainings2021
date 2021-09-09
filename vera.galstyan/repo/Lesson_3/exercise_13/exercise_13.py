e38688bf14d9b95ceae9241378274625b2bf4e01
ver-galstyan <vera.galstyan@inbox.ru>
2021-09-09 22:00:31 +0400

    Exercise_13 Version 2

--- vera.galstyan/repo/Lesson_3/exercise_13/exercise_13.py
+++ vera.galstyan/repo/Lesson_3/exercise_13/exercise_13.py
@@ -1,9 +1,11 @@
 #!usr\bin\env python 3
 
+meal_cost = int(input("Enter the cost of the meal: "))
+tax_rate = 0.09
+tip = 0.18
+tax_amount = meal_cost * tax_rate
+tip_amount = meal_cost * tip
+grand_total = meal_cost + tax_amount + tip_amount
 
-price = float(input (" Enter the cost of a meal"))
-
-
-tipPracent = 18
-
-tipValue = price * tipPracent /100
+print("The tax amount is: ", round(tax_amount, 2), "\nThe tip amount is: ", round(tip_amount, 2), 
+"\nThe total is: ", round(grand_total, 2), sep=" ")
