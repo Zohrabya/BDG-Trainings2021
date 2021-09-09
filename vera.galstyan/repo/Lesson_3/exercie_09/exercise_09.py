9214ec78cef3327f58f403a24f8db5887c2b5161
ver-galstyan <vera.galstyan@inbox.ru>
2021-09-09 21:18:15 +0400

    Exercise_09 Version 2

---
+++ vera.galstyan/repo/Lesson_3/exercie_09/exercise_09.py
@@ -0,0 +1,8 @@
+#!\usr\bin env python 3
+
+price = int(input("Enter the price of the meal: "))
+percent = int(input("Enter the percent: "))
+tip = price * percent / 100
+total_bill = price + tip
+
+print("Tip amount:", tip, "\nBill(included tip):", total_bill, sep=" ")
