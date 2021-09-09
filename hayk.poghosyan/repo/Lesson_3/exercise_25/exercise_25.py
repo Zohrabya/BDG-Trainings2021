c8c5316509ede157dfb66304e4ad78de3e93fe38
hpoghosyan <hayk.m.poghosyan@gmail.com>
2021-09-10 01:53:10 +0400

    Exercise_25 Version1

---
+++ hayk.poghosyan/repo/Lesson_3/exercise_25/exercise_25.py
@@ -0,0 +1,4 @@
+integer = int(input("Enter four-digit integer: "))
+number = str(integer)
+result = int(number[0]) + int(number[1]) + int(number[2]) + int(number[3])
+print(number[0], "+", number[1], "+", number[2], "+", number[3], "=", result)
