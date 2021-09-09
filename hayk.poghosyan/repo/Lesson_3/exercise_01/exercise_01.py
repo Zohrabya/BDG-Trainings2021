1bfd38f88480371523a249cd6f09f322e5ea7b01
hpoghosyan <hayk.m.poghosyan@gmail.com>
2021-09-10 02:11:15 +0400

    Exercise_01 Version2

--- hayk.poghosyan/repo/Lesson_3/exercise_01/exercise_01.py
+++ hayk.poghosyan/repo/Lesson_3/exercise_01/exercise_01.py
@@ -1,17 +1,11 @@
 # 1-a
-x = "*"
-y = 19
-print(x*y + "\n" + x*y + "\n" + x*y + "\n" + x*y)
+print("*" * 19 + "\n" + "*" * 19 + "\n" + "*" * 19 + "\n" + "*" * 19)
 
 # 1-b
-x = "*"
-y = 19
-print("\n",x*y,"\n",x*y,"\n",x*y,"\n",x*y)
+print(" " + "*" * 19 +"\n", "*" * 19 + "\n", "*" * 19 + "\n", "*" * 19)
 
 # 1-c
-x = "*"
-y = 19
-print(x*y)
-print(x*y)
-print(x*y)
-print(x*y)
+print("*" * 19)
+print("*" * 19)
+print("*" * 19)
+print("*" * 19)
