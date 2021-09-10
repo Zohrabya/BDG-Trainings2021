e211ed0a23487340e9e85ab243e0446e271b6684
ver-galstyan <vera.galstyan@inbox.ru>
2021-09-10 18:41:57 +0400

    Exercise_24 Version 2

--- bdg.student/Lesson_3/vera.galstyan/repo/Lesson_3/exercise_24/exercise_24.py
+++ bdg.student/Lesson_3/vera.galstyan/repo/Lesson_3/exercise_24/exercise_24.py
@@ -1,51 +1,27 @@
-#!usr\bin\env pyt
+#!usr\bin\env python
 
-#1
-f_square = f**2
-f_cube = f*3
+print("integer\t\tsquare\t\tcube")
+integer = 0
+print(integer, integer ** 2, integer ** 3, sep="\t\t")
+integer = 1
+print(integer, integer ** 2, integer ** 3, sep="\t\t")
+integer = 2
+print(integer, integer ** 2, integer ** 3, sep="\t\t")
+integer = 3
+print(integer, integer ** 2, integer ** 3, sep="\t\t")
+integer = 4
+print(integer, integer ** 2, integer ** 3, sep="\t\t")
+integer = 5
+print(integer, integer ** 2, integer ** 3, sep="\t\t")
+integer = 6
+print(integer, integer ** 2, integer ** 3, sep="\t\t")
+integer = 7
+print(integer, integer ** 2, integer ** 3, sep="\t\t")
+integer = 8
+print(integer, integer ** 2, integer ** 3, sep="\t\t")
+integer = 9
+print(integer, integer ** 2, integer ** 3, sep="\t\t")
+integer = 10
+print(integer, integer ** 2, integer ** 3, sep="\t\t")
 
-g = 6
-g_square = g**2
-g_cube = g**3
-
-h = 7
-h_square = h**2
-h_cube = h**3
-
- j = 8
- j_square = j**2
- j_cube = j**3
-
-k = 9
-k_square = k*2
-k_cube = k*3
-
-l = 10
-l_square = l**2
-l_cube = l**3
-#!
-print("integer","square","cube")
-
-print(a,"     ", a_square,"    ", a_cube)
-
-print(b,"     ", b_square,"    ", b_cube)
-
-print(c,"     ", c_square,"    ", c_cube)
-
-print(d,"     ", d_square,"    ", d_cube)
-
-print(e,"     ", e_square,"   ", e_cube)
-
-print(f,"     ", f_square,"   ", f_cube)
-
-
-print(g,"     ", g_square,"   ", g_cube)
-
-print(h,"     ", h_square,"   ", h_cube)
-
-print(j,"     ", j_square,"", j_cube)
-
-print(k,"     ", k_square,"", k_cube)
-
-print(l,"   ", l_square,"", l_cube) 
 
