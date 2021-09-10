b1cc686a4bbaadcd683dfa612ac3b6be7ebeb92b
ver-galstyan <vera.galstyan@inbox.ru>
2021-09-10 18:29:54 +0400

    Exercise_21 Version 2

--- vera.galstyan/repo/Lesson_3/exercise_21/exercise_21.py
+++ vera.galstyan/repo/Lesson_3/exercise_21/exercise_21.py
@@ -1,9 +1,6 @@
-
-number = int(input("Enter a number: "))
-
-if (number%2):
-    print(" Even")
-
-
+#!/usr/bin/evn python3
+number = int (input("Enter a number: "))
+if number % 2 == 0:
+    print("The number you entered is even")
 else:
-    print("  Odd ")
+    print("The number you entered is odd")
