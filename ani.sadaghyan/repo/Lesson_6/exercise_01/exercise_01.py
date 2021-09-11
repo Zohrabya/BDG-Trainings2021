cb3fd6c216cfee41696c2ee7ced07c07951bd543
Ani Sadaghyan <anisadaghyan@gmail.com>
2021-09-11 23:30:31 +0400

    exercise_01 version1

---
+++ ani.sadaghyan/repo/Lesson_6/exercise_01/exercise_01.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+togh_astgh = int(input("Enter the number of togh: "))
+qanak_astgh = int(input("Enter the number of qanak: "))
+for i in range(togh_astgh):
+    print("*" * qanak_astgh)
