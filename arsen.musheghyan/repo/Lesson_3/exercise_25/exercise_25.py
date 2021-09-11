22896105a5c07100f9334c6d8bad4b139a7c6937
Arsen1978 <jobforars@gmail.com>
2021-09-11 23:42:41 +0400

    Version1

---
+++ arsen.musheghyan/repo/Lesson_3/exercise_25/exercise_25.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+list1 = str(input("Please, inputs a four-digit integer: "))
+sum = int(list1[0]) + int(list1[1]) + int(list1[2]) + int(list1[3])
+print (list1[0],"+",list1[1],"+", list1[2],"+", list1[3],"=", sum)
