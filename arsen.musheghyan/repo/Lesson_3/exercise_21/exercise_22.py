3eca0be2cf509c3616f3ea3ce83e5e7980b88d63
Arsen1978 <jobforars@gmail.com>
2021-09-11 22:51:12 +0400

    Version1

---
+++ arsen.musheghyan/repo/Lesson_3/exercise_21/exercise_22.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+number1 = int(input("Please, enter number1: "))
+number2 = int(input("Please, enter number2: "))
+if number1 % number2 == 0:
+    print ("The first number is a multiple of the second")
+else:
+    print ("The first number is NOT a multiple of the second")
