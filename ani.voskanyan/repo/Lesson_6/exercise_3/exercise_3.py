8dfe834596192ed57807b5e6fa7b4d7fd3f187bc
annievoskanyan <you@example.com>
2021-09-12 19:25:35 +0400

    Exercise_3 Version_5 Lesson_6

--- ani.voskanyan/repo/Lesson_6/exercise_3/exercise_3.py
+++ ani.voskanyan/repo/Lesson_6/exercise_3/exercise_3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-height = int(input("Enter the Height of a triangle: "))
+height = int(input("Enter the height of a triangle: "))
 
 for i in range(1, height + 1):
     print("*" * i)
