82f12b17c2a66b5e8a5f3df34a536499de798c38
Ani Sadaghyan <anisadaghyan@gmail.com>
2021-09-12 11:54:33 +0400

    exercise_11 version2

--- ani.sadaghyan/repo/Lesson_6/exercise_09/exercise_09.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-jermastitsan = ("celsius", "fahrenhit")
-for i in range(0, 101, 10):
-    celsius = (i - 32) * 5 / 9
-    fahrenhit = i * 1/8 + 32
-    print(celsius, fahrenhit)
