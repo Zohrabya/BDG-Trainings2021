8c5c89e1a908e1b176d240f0b27102d8dca9e8e3
Arsen1978 <jobforars@gmail.com>
2021-09-12 17:10:06 +0400

    Version2

--- arsen.musheghyan/repo/Lesson_3/exercise_24/exercise_24.py
+++ arsen.musheghyan/repo/Lesson_3/exercise_24/exercise_24.py
@@ -2,10 +2,7 @@
 print("integer square cube")
 for i in range (4):
     print(i, " "*5, i**2, " "*4, i**3)
-
 for i in range (5,10):
     print(i, " "*5, i**2, " "*3, i**3)
-
-
 for i in range (10,11):
     print(i, " "*4, i**2, " "*2, i**3)
