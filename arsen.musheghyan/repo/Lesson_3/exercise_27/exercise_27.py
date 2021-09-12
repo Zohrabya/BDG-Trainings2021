35490d207452e9475cc4728b44a430795125342e
Arsen1978 <jobforars@gmail.com>
2021-09-12 17:07:09 +0400

    Version2

--- arsen.musheghyan/repo/Lesson_3/exercise_27/exercise_27.py
+++ arsen.musheghyan/repo/Lesson_3/exercise_27/exercise_27.py
@@ -1,9 +1,3 @@
 #!/usr/bin/env python3
-for i in range (1,8):
+for i in range (1,18):
     print (i, " "*5, oct(i), " "*5, hex(i), " "*5, bin(i))
-for i in range (9,10):
-    print (i, " "*5, oct(i), " "*4, hex(i), " "*5, bin(i))
-for i in range (11,16):
-    print (i, " "*4, oct(i), " "*4, hex(i), " "*5, bin(i))
-for i in range (16,18):
-    print (i, " "*4, oct(i), " "*4, hex(i), " "*4, bin(i))
