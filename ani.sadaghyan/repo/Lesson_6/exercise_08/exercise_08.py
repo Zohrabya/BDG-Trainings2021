50d8de0351a53ada784704645c3a89469d98b035
Ani Sadaghyan <anisadaghyan@gmail.com>
2021-09-13 20:13:28 +0400

    exercise_08 version2

--- ani.sadaghyan/repo/Lesson_6/exercise_08/exercise_08.py
+++ ani.sadaghyan/repo/Lesson_6/exercise_08/exercise_08.py
@@ -3,6 +3,6 @@
 numbers = [4.95, 9.95, 14.95, 19.95, 24.95]
 i = 0
 tokos = 0
-for i in range(0,len(numbers)): 
+for i in range(0,len(numbers),2): 
     tokos = (numbers[i] -(numbers[i] * 0.6))
     print(tokos,  numbers[i])
