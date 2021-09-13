ad1d9aed0af37a069c08831a00777f6e5015e1f8
serine-galstyan <serine.galstyan98@gmail.com>
2021-09-13 22:16:12 +0400

    Exercise_07 Version3

--- serine.galstyan/repo/Lesson_3/exercise_07/exercise_07.py
+++ serine.galstyan/repo/Lesson_3/exercise_07/exercise_07.py
@@ -1,4 +1,4 @@
-#!/usr/bin/evn python3
+#!/usr/bin/env python3
 
 entered_number = input("Enter how many kilograms should be converted: ")
 print("There are", round(2.2 * float(entered_number), 2), "pounds in", entered_number, "kg.", sep=" ")
