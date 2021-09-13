f864f90d109e6dcedc8443752e27dfb73c51fd35
ani2025 <ANIHARUTYUNYAN61@GMAIL.COM>
2021-09-13 15:33:26 +0400

    exercise_10_lesson6_version2

--- ani.ter-markosyan/repo/Lesson_6/exercise_10/exercise_10.py
+++ ani.ter-markosyan/repo/Lesson_6/exercise_10/exercise_10.py
@@ -1,21 +1,24 @@
-#!usr/bin/env python3
+#!/usr/bin/env python3
 
 from math import sqrt
 
 perimeter = 0
 x_coordinate = float(input("Enter x part of the coordinate: "))
 y_coordinate = float(input("Enter y part of the coordinate: "))
-x = x_coordinate
-y = y_coordinate
-x_coordinate1 = input("Enter x part of the coordinate(blank to quit: ")
+x_coordinates_value = x_coordinate
+y_coordinates_value = y_coordinate
+x_coordinate1 = input("Enter x part of the coordinate(blank to quit): ")
+
 while x_coordinate1 != "":
     x_coordinate2 = float(x_coordinate1)
     y_coordinate2 = float(input("Enter y part of the coordinate: "))
-    distance = sqrt((x - x_coordinate2) ** 2 + (y - y_coordinate2) ** 2)
+    distance = sqrt((x_coordinates_value - x_coordinate2) ** 2 + (y_coordinates_value - y_coordinate2) ** 2)
     perimeter += distance
-    x = x_coordinate2
-    y = y_coordinate2
+    x_coordinates_value = x_coordinate2
+    y_coordinates_value = y_coordinate2
     x_coordinate1 = input("Enter x part of the coordinate(blank to quit): ")
-distance = sqrt((x- x_coordinate) ** 2 + (y - y_coordinate) ** 2)
+
+distance = sqrt((x_coordinates_value- x_coordinate) ** 2 + (y_coordinates_value - y_coordinate) ** 2)
 perimeter += distance
+
 print ("The perimeter of the poligon is", perimeter)
