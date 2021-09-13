60911674d0d813798ffba082a0a89cdd387f7bac
serine-galstyan <serine.galstyan98@gmail.com>
2021-09-13 22:30:15 +0400

    Exercise_12 Version3

--- serine.galstyan/repo/Lesson_3/exercise_12/exercise_12.py
+++ serine.galstyan/repo/Lesson_3/exercise_12/exercise_12.py
@@ -1,4 +1,4 @@
-#!/usr/bin/evn python3
+#!/usr/bin/env python3
 
 liter_or_less = int(input("Enter the number of containers that holding 1 liter or less: "))
 more_than_liter = int(input("Enter the number of containers that holding more than 1 liter: "))
