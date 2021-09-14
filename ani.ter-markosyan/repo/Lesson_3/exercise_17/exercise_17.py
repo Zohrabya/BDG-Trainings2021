c4a1dfd30a14a20c63ec78f05c53270e469224d8
ani2025 <ANIHARUTYUNYAN61@GMAIL.COM>
2021-09-15 03:10:57 +0400

    exercise_17_version5

--- ani.ter-markosyan/repo/Lesson_3/exercise_17/exercise_17.py
+++ ani.ter-markosyan/repo/Lesson_3/exercise_17/exercise_17.py
@@ -1,25 +1,23 @@
-#!usr/bin/evn python3
+#!/usr/bin/env python3
 
 number1, number2, number3 = (input("Input three different integers: ").split())
-sum = int(number1) + int (number2) + int(number3)
-average = round(int (number1) + int(number2) + int(number3) / 3, 2)
-product = int(number1) * int(number2) * int(number3)
-if number1 > number2 and number1 > number3:
-    if number2 < number3:
-        largest = number1
-        smallest = number2
-    else:
-        smallest = number3
-elif number2 > number1 and number2 > number3:
-    if number1 < number3:
-        largest = number2
-        smallest = number1
-    else:
-        smallest = number3
-else:
+number1 = int(number1)
+number2 = int(number2)
+number3 = int(number3)
+sum = number1 + number2+ number3
+average = round(sum / 3, 2)
+product = number1 * number2 * number3
+
+largest = number1
+if number2 > largest:
+    largest = number2
+if  number3 > largest:
     largest = number3
-    if number1 < number2:
-        smallest = number1
-    else:
-        smallest = number2
+
+smallest = number1
+if number2 < smallest:
+    smallest = number2
+if number3 < smallest:
+    smallest = number3
+    
 print("Sum is", sum), print("Average is", average), print("Product is", product), print("Smallest is", smallest), print("Largest is", largest)
