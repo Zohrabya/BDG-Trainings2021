64c0a4728c25f486e6a75ebb1c746984b3248f9d
ani2025 <ANIHARUTYUNYAN61@GMAIL.COM>
2021-09-15 01:10:09 +0400

    exercise_08_lesson6_version3

--- ani.ter-markosyan/repo/Lesson_6/exercise_08/exercise_08.py
+++ ani.ter-markosyan/repo/Lesson_6/exercise_08/exercise_08.py
@@ -1,9 +1,11 @@
-#!usr/bin/evn python3
+#!/usr/bin/env python3
 
-print ("Original_prices", "\t", "Dicount", "\t","Prices_after_discount")
+print ("Original_prices", "\t", "Discount", "\t","Prices_after_discount")
+
 discount = 0.6
-list_1 = [4.95, 9.95, 14.95, 19.95, 24.95]
-for item in (list_1):
+original_prices = [4.95, 9.95, 14.95, 19.95, 24.95]
+
+for item in (original_prices):
     discount_list = discount * item
     new_prices = item - discount * item
-    print (item, 3 * "\t",format(discount_list, ".2f"), 3 * "\t", format(new_prices, ".2f"))
+    print(item, 3 * "\t", format(discount_list, ".2f"), 3 * "\t", format(new_prices, ".2f"))
