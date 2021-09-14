025bd5f82f3a627ecf5b15f3b3c51f9279877f05
ani2025 <ANIHARUTYUNYAN61@GMAIL.COM>
2021-09-15 00:39:27 +0400

    exercise_12_lesson6_version2

--- ani.ter-markosyan/repo/Lesson_6/exercise_12/exercise_12.py
+++ ani.ter-markosyan/repo/Lesson_6/exercise_12/exercise_12.py
@@ -1,23 +1,20 @@
-#!usr/bin/evn python3
+#!/usr/bin/env python3
 
 list = []
-numbers = int
+numbers = input("Enter the numbers(blank to exit): ")
+list.append(int(numbers))
+
 while numbers != "":
-    numbers = input("Enter the numbers(blank to exit): ")
+    numbers = input("Enter the numbers(blank to exit): ")  
     if numbers != "":
         list.append(int(numbers))
+        
 for item in list:
     if item < 0:
-        print(item)
+        print(item, end = " ")
 for item in list:
     if item == 0:
-        print(item)
+        print(item, end = " ")
 for item in list:
     if item > 0:
-        print(item)
-
-
-       
-
-
-
+        print(item, end = " ")
