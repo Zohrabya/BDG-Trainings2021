24edd5945edb48bfacdb4a31f66ce0da2e9467a8
ani2025 <ANIHARUTYUNYAN61@GMAIL.COM>
2021-09-15 01:05:51 +0400

    exercise_05_lesson6_version3

--- ani.ter-markosyan/repo/Lesson_6/exercise_05/exercise_05.py
+++ ani.ter-markosyan/repo/Lesson_6/exercise_05/exercise_05.py
@@ -1,7 +1,9 @@
-#!usr/bin/evn python3
+#!/usr/bin/env python3
 
 height = int(input("Enter the height of a diamond: "))
+
 for i in range (0, height):
     print(" " * (height - i) + "*" * (2 * i + 1))
+    
 for i in range(-height + 1, 0):
     print (" " * (height + 1 + i) + "*" * (-2 * i - 1))
