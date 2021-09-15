b24550a449f209a504f95a49a08c307ed3a4cc7f
ani2025 <ANIHARUTYUNYAN61@GMAIL.COM>
2021-09-16 01:31:12 +0400

    exercise_18_lesson6_version2

--- ani.ter-markosyan/repo/Lesson_6/exercise_18/exercise_18.py
+++ ani.ter-markosyan/repo/Lesson_6/exercise_18/exercise_18.py
@@ -1,13 +1,13 @@
-#!usr/bin/env python3
+#!/usr/bin/env python3
 
-list_1 = []
-item = int
-while item != "":
-    item = input("Enter the elements of list: ")
-    if item != "":
-     list_1.append(int(item))
-if len(list_1) >= 0 and list_1 == sorted(list_1) or list_1 == sorted(list_1, reverse = True):
+list_of_numbers = []
+elements = input("Enter the elements of list: ").split()
+
+for item in elements:
+    item = int(item)
+    list_of_numbers.append(item)
+
+if len(list_of_numbers) >= 0 and list_of_numbers== sorted(list_of_numbers) or list_of_numbers == sorted(list_of_numbers, reverse = True):
     print("The list is sorted")   
 else:
     print ("The list is not sorted")
-    
