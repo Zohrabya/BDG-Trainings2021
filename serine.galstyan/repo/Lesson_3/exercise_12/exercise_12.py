899006aaf2b29ad881488176149bd9c4148352d9
serine-galstyan <serine.galstyan98@gmail.com>
2021-09-16 21:27:08 +0400

    Exercise_12 Version5

--- serine.galstyan/repo/Lesson_3/exercise_12/exercise_12.py
+++ serine.galstyan/repo/Lesson_3/exercise_12/exercise_12.py
@@ -2,7 +2,7 @@
 
 liter_or_less = int(input("Enter the number of containers that holding 1 liter or less: "))
 more_than_liter = int(input("Enter the number of containers that holding more than 1 liter: "))
-LIGHTWEIGHT_CONTAINER_DEPOSIT = 0.10
+LESS_CONTAINER_DEPOSIT = 0.10
 HEAVY_CONTAINER_DEPOSIT = 0.25
-total = (LIGHTWEIGHT_CONTAINER_DEPOSIT * liter_or_less) + (HEAVY_CONTAINER_DEPOSIT * more_than_liter)
+total = (LESS_CONTAINER_DEPOSIT * liter_or_less) + (HEAVY_CONTAINER_DEPOSIT * more_than_liter)
 print("The refund for returning containers is $" + str(round(total, 2)) + ".")
