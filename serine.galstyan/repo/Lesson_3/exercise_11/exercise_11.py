3d8a2d9f56c6bcb15ba4163cbcba808efa605067
serine-galstyan <serine.galstyan98@gmail.com>
2021-09-16 21:10:17 +0400

    Exercise_11 Version4

--- serine.galstyan/repo/Lesson_3/exercise_11/exercise_11.py
+++ serine.galstyan/repo/Lesson_3/exercise_11/exercise_11.py
@@ -2,6 +2,6 @@
 
 width = float(input("Enter the width(in feet): "))
 length = float(input("Enter the length(in feet): "))
-feets_in_an_acre = 43560
-field_area = width * length / feets_in_an_acre
-print("The area of the field is", round(field_area, 4), "acres.", sep=" ")
+FEETS_IN_ACRE = 43560
+field_area = width * length / FEETS_IN_ACRE
+print("The area of the field is", round(field_area, 4), "acres.")
