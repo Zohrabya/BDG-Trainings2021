5491ee8bf28d9252320975f2587bfef59cde0d83
Arsen1978 <jobforars@gmail.com>
2021-09-16 20:18:32 +0400

    Version2

--- arsen.musheghyan/repo/Lesson_3/exercise_02/exercise_02.py
+++ arsen.musheghyan/repo/Lesson_3/exercise_02/exercise_02.py
@@ -1,8 +1,6 @@
-print ("Exercise N 2,a")
+#!/usr/bin/env python3
 print ("*******************""\n" "* 		  *""\n""* 		  *","\n""*******************")
-print ("Exercise N 2,b")
 print ("*******************");"\n";print ("* 		  *");"\n";print ("* 		  *");"\n";print ("*******************")
-print ("Exercise N 2,c")
 print ("*******************")
 print ("*                 *")
 print ("*                 *")
