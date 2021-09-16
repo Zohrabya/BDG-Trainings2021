bb193d62c0bb3692ea7de92a44886327c9efd17b
serine-galstyan <serine.galstyan98@gmail.com>
2021-09-16 21:29:59 +0400

    Exercise_16 Version4

--- serine.galstyan/repo/Lesson_3/exercise_16/exercise_16.py
+++ serine.galstyan/repo/Lesson_3/exercise_16/exercise_16.py
@@ -3,8 +3,8 @@
 first_num = int(input("Enter first integer: "))
 second_num = int(input("Enter second integer: "))
 if first_num > second_num:
-    print(first_num, "is larger", sep=" ")
+    print(first_num, "is larger")
 elif first_num == second_num:
     print("These numbers are equal.")
 else:
-    print(second_num, "is larger", sep=" ")
+    print(second_num, "is larger")
