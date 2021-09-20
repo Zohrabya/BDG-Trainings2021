02ddeb22e24ffb63bc339f71c7bfb9867ecdb7b9
diananavasardyan <diana.navasardyan@gmail.com>
2021-09-21 02:07:08 +0400

    Exercise_08_Lesson_6 Version2

--- diana.navasardyan/repo/Lesson_6/exercise_08/exercise_08.py
+++ diana.navasardyan/repo/Lesson_6/exercise_08/exercise_08.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-SALE = 60
+SALE = 0.6
 prices_before_sale = [4.95, 9.95, 14.95, 19.95, 24.95]
 
 for i in prices_before_sale:
-    print(f'Old price: {i}$. Discount amount: {i * SALE/100:.2f}$. New price: {(i * ((100 - SALE)/100)):.2f}$.')
+    print(f'Old price: {i}$. Discount amount: {i * SALE:.2f}$. New price: {(i * (1-SALE)):.2f}$.')
