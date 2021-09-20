ddba9c2f97a175ecf04b6bb5c00997be957b4b32
diananavasardyan <diana.navasardyan@gmail.com>
2021-09-21 01:30:30 +0400

    Exercise_11 Version3

--- diana.navasardyan/repo/Lesson_3/exercise_11/exercise_11.py
+++ diana.navasardyan/repo/Lesson_3/exercise_11/exercise_11.py
@@ -3,5 +3,5 @@
 length_of_field = int(input("Enter the length(in feet): "))
 width_of_field = int(input("Enter the width(in feet): "))
 ACRES = 43560
-area_of_field = (length_of_field + width_of_field) / ACRES
-print("The area of the field is", area_of_field )
+area_of_field = round(length_of_field + width_of_field / ACRES, 2)
+print("The area of the field is", area_of_field)
