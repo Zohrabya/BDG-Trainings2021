cfa4e100b85410f2a31a8728d3e840d04c31976c
diananavasardyan <diana.navasardyan@gmail.com>
2021-09-20 23:30:36 +0400

    Exercise_09 Version4

--- diana.navasardyan/repo/Lesson_03/exercis_09/exercise_09.py
+++ diana.navasardyan/repo/Lesson_03/exercis_09/exercise_09.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 meal_price = float(input("Please enter the price of the meal: "))
-Percentage_tip = float(input("Please enter a percentage tip: "))
+percentage_tip = float(input("Please enter a percentage tip: "))
 
-meal_price_percentage_tip = meal_price * Percentage_tip / 100
+meal_price_percentage_tip = meal_price * percentage_tip / 100
 print ("Meal price after applying persent: ", meal_price_percentage_tip)
 print ("Total amount: ", meal_price + meal_price_percentage_tip)
 
