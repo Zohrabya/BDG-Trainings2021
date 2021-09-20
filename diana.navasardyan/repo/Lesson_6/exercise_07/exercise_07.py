1ea975232a7043a139e41c1680b8180306638b4d
diananavasardyan <diana.navasardyan@gmail.com>
2021-09-21 02:02:52 +0400

    Exercise_07_Lesson_6 Version2

--- diana.navasardyan/repo/Lesson_6/exercise_07/exercise_07.py
+++ diana.navasardyan/repo/Lesson_6/exercise_07/exercise_07.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-num_1 = int(input(" Please enter a number (Hint:Enter '0' to calculate the average): "))
+num_1 = int(input(" Please enter a number: "))
 num_1_list = []
 
 while num_1 != 0:
     num_1_list.append(num_1)
-    num_1 = int(input("Please enter another number: "))
-num_average = sum(num_1_list) / len(num_1_list)
+    num_1 = int(input("Please enter another number (Hint:Enter '0' to calculate the average): "))
+num_average = round(sum(num_1_list) / len(num_1_list), 2)
 print(num_average)
