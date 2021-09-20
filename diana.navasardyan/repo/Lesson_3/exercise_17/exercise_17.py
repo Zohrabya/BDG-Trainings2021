03e5079eb44795977609688ad5b1de624ea0b586
diananavasardyan <diana.navasardyan@gmail.com>
2021-09-21 03:49:58 +0400

    Exercise_17 Version3

--- diana.navasardyan/repo/Lesson_3/exercise_17/exercise_17.py
+++ diana.navasardyan/repo/Lesson_3/exercise_17/exercise_17.py
@@ -1,28 +1,23 @@
 #!/usr/bin/env python3
 
-number_number = input("Enter a three integers: ")
-number1 = str(number_number[0])
-number2 = str(number_number[1])
-number3 = str(number_number[2])
+number1, number2, number3 = input("Enter a three integers: ")
 
-sum_numbers = int(number1)+ int(number2) + int(number3)
+sum_numbers = int(number1) + int(number2) + int(number3)
 
-averege_numbers = sum_numbers / 3
+averege_numbers = int(sum_numbers) / 3
 
 product_numbers = int(number1) * int(number2) * int(number3)
 #largest_number
-if number2 < number1 > number3:
-    largest = number1    
-elif number1 < number2 > number3:
-    largest = number2
-else:
+largest = number1
+if number2 > number1:
+    largest = number2   
+if number3 > number2:
     largest = number3
-#smallest_number
-if number2 > number1 < number3:
-    smallest = number1    
-elif number1 > number2 < number3:
-    smallest = number2
-else:
-    smallest = number3
 
+#smallest_number
+smallest = number1
+if number2 < number1:
+    smallest = number2    
+if number3 < number2:
+    smallest = number3
 print(f"Input three deferent integers: {number1} {number2} {number3} \nSum is {sum_numbers} \nAverage is {averege_numbers:.2f} \nProduct is {product_numbers} \nSmallest is {smallest} \nLargest is {largest}")
