5625e1647b81f52e04b5c649739cb23ddd32fdb4
diananavasardyan <diana.navasardyan@gmail.com>
2021-09-21 05:51:01 +0400

    Exercise_20 Version4

--- diana.navasardyan/repo/Lesson_03/exercise_20/exercise_20.py
+++ diana.navasardyan/repo/Lesson_03/exercise_20/exercise_20.py
@@ -4,23 +4,23 @@ number1, number2, number3, number4, number5 = input("Enter a five integers: ")
 
 #largest
 largest = number1
-if number3 < number2 > number4 and number1 < number2 > number5:
+if number2 > largest:
     largest = number2
-if number4 < number3 > number5 and number3 > number2:
+if number3 > largest:
     largest = number3
-if number3 < number4 > number5 and number4 > number2:
+if number4 > largest:
     largest = number4
-if number3 < number5 > number4 and number5 > number2:
+if number5 > largest:
     largest = number5
 
 #smallest
 smallest = number1
-if number1 > number2 < number4 and number4 > number2 < number5:
+if  number2 < smallest:
     smallest = number2
-if number4 > number3 < number2 and number4 > number3 < number5:
+if number3 < smallest:
     smallest = number3
-if number2 > number4 < number3 and number4 < number5:
+if number4 < smallest:
     smallest = number4
-if number2 > number5 < number3 and number5 < number4:
+if number5 < smallest:
     smallest = number5
-print(f"The largest number is {largest}: \nThe smallest number is {smallest}: ")
+print(f"The largest number is {largest}\nThe smallest number is {smallest}")
