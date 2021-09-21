6058948a4f22e88060a92f6e6436104683694749
diananavasardyan <diana.navasardyan@gmail.com>
2021-09-21 05:55:33 +0400

    Exercise_17 Version4

--- diana.navasardyan/repo/Lesson_3/exercise_17/exercise_17.py
+++ diana.navasardyan/repo/Lesson_3/exercise_17/exercise_17.py
@@ -9,15 +9,15 @@ averege_numbers = int(sum_numbers) / 3
 product_numbers = int(number1) * int(number2) * int(number3)
 #largest_number
 largest = number1
-if number2 > number1:
+if number2 > largest:
     largest = number2   
-if number3 > number2:
+if number3 > largest:
     largest = number3
 
 #smallest_number
 smallest = number1
-if number2 < number1:
+if number2 < smallest:
     smallest = number2    
-if number3 < number2:
+if number3 < smallest:
     smallest = number3
 print(f"Input three deferent integers: {number1} {number2} {number3} \nSum is {sum_numbers} \nAverage is {averege_numbers:.2f} \nProduct is {product_numbers} \nSmallest is {smallest} \nLargest is {largest}")
