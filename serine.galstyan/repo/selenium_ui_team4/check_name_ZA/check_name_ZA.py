1cea248d46743addc24fc7f72eb3b83eba814b5a
serine-galstyan <serine.galstyan98@gmail.com>
2021-10-17 19:39:47 +0400

    serine.galstyan_check_name_ZA

---
+++ serine.galstyan/repo/selenium_ui_team4/check_name_ZA/check_name_ZA.py
@@ -0,0 +1,28 @@
+# This test case was created by Serine Galstyan
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+import time
+
+chrome_user_options = Options()
+chrome_user_options.add_argument("--start-maximized")
+
+chrome_driver = webdriver.Chrome(chrome_options = chrome_user_options, executable_path=r"C:/Users/Orchid/OneDrive/Desktop/chrome/chromedriver_win32 (1)/chromedriver.exe")
+chrome_driver.get("https://www.saucedemo.com/")
+time.sleep(2)
+chrome_driver.find_element(By.XPATH, "//input[@id='user-name']").send_keys('standard_user')
+chrome_driver.find_element(By.XPATH, "//input[@id='password']").send_keys('secret_sauce')
+chrome_driver.find_element(By.XPATH, "//input[@id='login-button']").click()
+time.sleep(2)
+chrome_driver.find_element(By.XPATH, "//select[@class='product_sort_container']").click()
+chrome_driver.find_element(By.XPATH, "//option[@value='za']").click()
+first_item_title = chrome_driver.find_element(By.XPATH, "//div[contains(text(),'Test.allTheThings() T-Shirt (Red)')]").text
+second_item_title = chrome_driver.find_element(By.XPATH, "//div[contains(text(),'Sauce Labs Onesie')]").text
+if first_item_title > second_item_title:
+    print("Test case PASSED.")
+else:
+    print("Test case FAILED.")
+time.sleep(2)
+chrome_driver.quit()
