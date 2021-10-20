db0687af43e7347f9f0cf09914fb7b1235da3aec
diananavasardyan <diana.navasardyan@gmail.com>
2021-10-21 02:22:43 +0400

    login_page_problem_user Version1

---
+++ diana.navasardyan/selenium_ui_team3/login_page_problem_user/login_page_problem_user.py
@@ -0,0 +1,20 @@
+#This test case created by Diana Navasardyan
+
+#!/usr/bin/env python3
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+chrome_driver = webdriver.Chrome(executable_path=r"C:\Users\Acer\Desktop\Webdriver\chromedriver.exe")
+chrome_driver.get("https://www.saucedemo.com/")
+username_form = username_form = chrome_driver.find_element_by_id("user-name")
+login = chrome_driver.find_element_by_xpath('//*[@id="login_credentials"]').text.split()[5]
+username_form.send_keys(login)
+password_form = chrome_driver.find_element_by_id("password")
+password = chrome_driver.find_element_by_xpath('//*[@class="login_password"]').text.split()[4]
+password_form.send_keys(password)
+login_button = chrome_driver.find_element_by_id("login-button").click()
+
+if chrome_driver.find_element_by_xpath('//*[@id="item_4_title_link"]'):
+    print("test case PASSED")
+else:
+    print("test case FAILED")
