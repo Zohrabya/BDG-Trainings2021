79da2a722aaec95aaeb455277f80a6a30c80a512
diananavasardyan <diana.navasardyan@gmail.com>
2021-10-21 01:25:26 +0400

    login_page_valid Version2

--- diana.navasardyan/selenium_ui_team3/login_page_valid/login_page_valid.py
+++ diana.navasardyan/selenium_ui_team3/login_page_valid/login_page_valid.py
@@ -3,12 +3,16 @@
 #!/usr/bin/env python3
 
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 chrome_driver = webdriver.Chrome(executable_path=r"C:\Users\Acer\Desktop\Webdriver\chromedriver.exe")
 chrome_driver.get("https://www.saucedemo.com/")
-username_form = chrome_driver.find_element_by_id("user-name")
-username_form.send_keys("standard_user")
+username_form = username_form = chrome_driver.find_element_by_id("user-name")
+login = chrome_driver.find_element_by_xpath('//*[@id="login_credentials"]').text.split()[3]
+username_form.send_keys(login)
 password_form = chrome_driver.find_element_by_id("password")
-password_form.send_keys("secret_sauce")
+password = chrome_driver.find_element_by_xpath('//*[@class="login_password"]').text.split()[4]
+password_form.send_keys(password)
 login_button = chrome_driver.find_element_by_id("login-button").click()
 cur_url = chrome_driver.current_url
 expected_url = "https://www.saucedemo.com/inventory.html"
