08ee082f76baf5be2d122558c613b0b147597be9
serine-galstyan <serine.galstyan98@gmail.com>
2021-10-22 00:22:23 +0400

    Team_4 login with username and empty password

---
+++ serine.galstyan/selenium_ui_team4/login_with_username_and_empty_password/login_with_username_and_empty_password.py
@@ -0,0 +1,28 @@
+# This test case was created by Serine Galstyan
+
+#!/usr/bin/env python3
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import time
+
+chrome_user_options = Options()
+chrome_user_options.add_argument("--start-maximized")
+chrome_user_options.add_argument("--log-level=3")
+
+chrome_driver = webdriver.Chrome(chrome_options = chrome_user_options, executable_path=r"C:/Users/Orchid/OneDrive/Desktop/chrome/chromedriver_win32 (1)/chromedriver.exe")
+chrome_driver.get("https://www.saucedemo.com/")
+time.sleep(2)
+username_input = chrome_driver.find_element(By.XPATH, "//input[@id='user-name']").send_keys('standard_user')
+chrome_driver.find_element(By.XPATH, "//input[@id='login-button']").click()
+time.sleep(2)
+error_message = chrome_driver.find_element(By.XPATH, "//div[@id='login_button_container']/div/form/div[3]/h3").text
+time.sleep(2)
+expected_message = "Epic sadface: Password is required"
+if error_message == expected_message:
+    print("Test case PASSED.")
+else:
+    print("Test case FAILED.")
+time.sleep(2)
+chrome_driver.quit()
