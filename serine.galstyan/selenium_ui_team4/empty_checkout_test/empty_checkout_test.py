459935e054de0e8a7bc5466553925ceb63c2540f
serine-galstyan <serine.galstyan98@gmail.com>
2021-10-22 01:03:47 +0400

    Team_4 empty checkout test

---
+++ serine.galstyan/selenium_ui_team4/empty_checkout_test/empty_checkout_test.py
@@ -0,0 +1,34 @@
+# This test case was created by Serine Galstyan
+
+#!/usr/bin/env python3
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import time
+
+chrome_user_options = Options()
+chrome_user_options.add_argument("--start-maximized")
+chrome_user_options.add_argument("--log-level=3")
+
+chrome_driver = webdriver.Chrome(chrome_options = chrome_user_options, executable_path=r"C:/Users/Orchid/OneDrive/Desktop/chrome/chromedriver_win32 (1)/chromedriver.exe")
+chrome_driver.get("https://www.saucedemo.com/")
+time.sleep(2)
+chrome_driver.find_element(By.XPATH, "//input[@id='user-name']").send_keys('standard_user')
+chrome_driver.find_element(By.XPATH, "//input[@id='password']").send_keys('secret_sauce')
+chrome_driver.find_element(By.XPATH, "//input[@id='login-button']").click()
+time.sleep(2)
+chrome_driver.find_element(By.XPATH, "//a[@class='shopping_cart_link']").click()
+chrome_driver.find_element(By.XPATH, "//button[@id='checkout']").click()
+time.sleep(2)
+chrome_driver.find_element(By.XPATH, "//input[@id='first-name']").send_keys('Serine')
+chrome_driver.find_element(By.XPATH, "//input[@id='last-name']").send_keys('Galstyan')
+chrome_driver.find_element(By.XPATH, "//input[@id='postal-code']").send_keys('12345')
+chrome_driver.find_element(By.XPATH, "//input[@id='continue']").click()
+chrome_driver.find_element(By.XPATH, "//button[@id='finish']").click()
+if chrome_driver.find_element(By.XPATH, "//span[text()='Checkout: Complete!']") == None:
+    print("Test case PASSED.")
+else:
+    print("Test case FAILED.")
+time.sleep(2)
+chrome_driver.quit()
