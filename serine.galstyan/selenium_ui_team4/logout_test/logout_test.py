7199546c6d3cc3236120d0ba797791e4eaebb246
serine-galstyan <serine.galstyan98@gmail.com>
2021-10-22 00:31:53 +0400

    Team_4 logout test

---
+++ serine.galstyan/selenium_ui_team4/logout_test/logout_test.py
@@ -0,0 +1,29 @@
+# This test case was created by Serine Galstyan
+
+#!/usr/bin/env python3
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import time
+
+chrome_user_options = Options()
+chrome_user_options.add_argument("--start-maximized")
+chrome_user_options.add_argument("--log-level=3")
+
+chrome_driver = webdriver.Chrome(chrome_options = chrome_user_options, executable_path=r"C:/Users/Orchid/OneDrive/Desktop/chrome/chromedriver_win32 (1)/chromedriver.exe")
+chrome_driver.get("https://www.saucedemo.com/")
+time.sleep(2)
+chrome_driver.find_element(By.XPATH, "//input[@id='user-name']").send_keys('standard_user')
+chrome_driver.find_element(By.XPATH, "//input[@id='password']").send_keys('secret_sauce')
+chrome_driver.find_element(By.XPATH, "//input[@id='login-button']").click()
+time.sleep(2)
+chrome_driver.find_element(By.XPATH, "//button[@id='react-burger-menu-btn']").click()
+time.sleep(1)
+chrome_driver.find_element(By.XPATH, "//a[@id='logout_sidebar_link']").click()
+if chrome_driver.find_element(By.XPATH, "//input[@id='user-name']")!= None:
+    print("Test case PASSED.")
+else:
+    print("Test case FAILED.")
+time.sleep(2)
+chrome_driver.quit()
