a60c89aea2c5b7aae849edfe1e580a5167c7c810
annievoskanyan <you@example.com>
2021-11-05 14:36:53 +0400

    Team1 Footer links tests using Pytest Version3

--- ani.voskanyan/selenium_ui_team1/pytest_tests/footer_links_test.py
+++ ani.voskanyan/selenium_ui_team1/pytest_tests/footer_links_test.py
@@ -1,5 +1,6 @@
 #The test case was Written by Ani Voskanyan
 #!/usr/bin/env python3
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -8,8 +9,9 @@ from selenium.webdriver.support import expected_conditions as EC
 browser = webdriver.Chrome()
 browser.implicitly_wait(3)
 
-
-def login():
+#Test case: Test if there are working Twitter, Facebook, LinkedIn links on the footer 
+@pytest.mark.parametrize("link_name_input", ["Twitter", "Facebook", "LinkedIn"])
+def test_twitter_link(link_name_input):
     link = "https://www.saucedemo.com/"
     browser.get(link)
     username_input_field = browser.find_element_by_css_selector('input[placeholder = "Username"]')
@@ -18,49 +20,16 @@ def login():
     password_input_field.send_keys('secret_sauce')
     browser.find_element_by_class_name('submit-button').click()
     first_window = browser.window_handles[0]
-    return first_window
-    
-
-def logout():
-    browser.switch_to.window(login())
+    link_button = browser.find_element_by_link_text(link_name_input)
+    browser.execute_script("return arguments[0].scrollIntoView(true);", link_button)
+    link_button.click()
+    link_window = browser.window_handles[1]
+    browser.switch_to.window(link_window)
+    WebDriverWait(browser, 10).until(EC.title_contains(link_name_input))
+    assert link_name_input.lower() in browser.title.lower() 
+    browser.switch_to.window(first_window)
     browser.execute_script("window.scrollTo(0, -document.body.scrollHeight);")
     sidebar_button = browser.find_element(By.ID, "react-burger-menu-btn")
     sidebar_button.click()
     browser.find_element_by_id("logout_sidebar_link").click()
-
-#Test case N1: Test if there is working Twitter link on the footer 
-def test_twitter_link():
-    login()
-    twitter_button = browser.find_element_by_link_text("Twitter")
-    browser.execute_script("return arguments[0].scrollIntoView(true);", twitter_button)
-    twitter_button.click()
-    twitter_window = browser.window_handles[1]
-    browser.switch_to.window(twitter_window)
-    WebDriverWait(browser, 10).until(EC.title_contains("Twitter"))
-    assert "twitter" in browser.title.lower() 
-    logout()
-    
-    #Test case N2: Test if there is working Facebook link on the footer 
-def test_facebook_link():
-    login()
-    facebook_button = browser.find_element_by_link_text("Facebook")
-    browser.execute_script("return arguments[0].scrollIntoView(true);", facebook_button)
-    facebook_button.click()
-    facebook_window = browser.window_handles[2]
-    browser.switch_to.window(facebook_window)
-    WebDriverWait(browser, 10).until(EC.title_contains("Facebook"))
-    assert "facebook" in browser.title.lower()
-    logout()
-        
-    #Test case N3: Test if there is working LinkedIn link on the footer 
-def test_linkedin_link():
-    login()
-    linkedin_button = browser.find_element_by_link_text("LinkedIn")
-    browser.execute_script("return arguments[0].scrollIntoView(true);", linkedin_button)
-    linkedin_button.click()
-    linkedin_window = browser.window_handles[3]
-    browser.switch_to.window(linkedin_window)
-    WebDriverWait(browser, 10).until(EC.title_contains("LinkedIn"))
-    assert "linkedin" in browser.title.lower() 
-    logout()
     
